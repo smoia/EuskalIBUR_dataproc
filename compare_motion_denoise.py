b809495a5cdea7d6a532ba0f290736c0726f9ce6
smoia <s.moia@bcbl.eu>
2019-09-23 15:41:38 +0200

    Finished algorithm for compare_motion_denoise

--- compare_motion_denoise.py
+++ compare_motion_denoise.py
@@ -6,29 +6,66 @@ import pandas
 
 from numpy import genfromtxt
 
+
+def compute_slope(x,y):
+    m = ( (x*y).mean - x.mean*y.mean ) / ( x**2.mean - x.mean**2 )
+    return m
+
+
 cwd = os.getcwd()
 
 os.chdir('/media')
 # os.chdir('/media/nemo/ANVILData/gdrive/PJMASK')
 
 sub_list = ['007', '003', '002']
+ftype_list = ['echo-2', 'optcom', 'meica']
 
-# #!# PSEUDOCODE
-sub_table = pd. genemptydf
+sub_table = pd.DataFrame()
+slope_table = pd.DataFrame()
 
 for sub in sub_list:
     for ses in range(1, 10):
-        for mottype in ['dvars_pre', 'fd', 'dvars']:
-            if mottype == 'dvars':
-                for ftype in ['echo-2', 'optcom', 'meica']:
-                    colname = f'{sub}_{ses:02d}_{mottype}_{ftype}'
-                    filename = f'{mottype}_{ftype}_sub-{sub}_ses-{ses}.1D'
-                    sub_table[colname] = np.genfromtxt(filename)
+        slopes = np.zeroes([30])
+        
+        for mottype in ['dvars_pre', 'fd']:
+            col_name = f'{sub}_{ses:02d}_{mottype}'
+            filename = f'{mottype}_sub-{sub}_ses-{ses}.1D'
+            sub_table[col_name] = np.genfromtxt(filename)
 
+            avg_col_name = f'{sub}_avg_{mottype}'
+            if ses == 1:
+                sub_table[avg_col_name] = sub_table[col_name]
             else:
-                colname = f'{sub}_{ses:02d}_{mottype}'
-                filename = f'{mottype}_sub-{sub}_ses-{ses}.1D'
-                sub_table[colname] = np.genfromtxt(filename)
+                sub_table[avg_col_name] = sub_table[avg_col_name] + sub_table[col_name]
+
+        # loop for ftype_list
+        for i in range(3):
+            col_name = f'{sub}_{ses:02d}_dvars_{ftype_list[i]}'
+            filename = f'dvars_{ftype_list[i]}_sub-{sub}_ses-{ses}.1D'
+            sub_table[col_name] = np.genfromtxt(filename)
+
+            avg_col_name = f'{sub}_avg_dvars_{ftype_list[i]}'
+            if ses == 1:
+                sub_table[avg_col_name] = sub_table[col_name]
+            else:
+                sub_table[avg_col_name] = sub_table[avg_col_name] + sub_table[col_name]
+
+            delta_col_name = f'{sub}_{ses:02d}_delta_dvars'
+            sub_table[delta_col_name] = ( sub_table[col_name] - sub_table[f'{sub}_{ses:02d}_dvars_pre'] ) / sub_table[f'{sub}_{ses:02d}_fd']
+
+            slopes[ses*(i+1)-1] = compute_slope(sub_table[delta_col_name], sub_table[f'{sub}_{ses:02d}_fd'])
+
+        for i in range(3):
+            slope_table[f'{sub}_{ses:02d}_{ftype_list[i]}'] = slopes[i*10:(i*10+10)]
+
+    slopes = np.zeroes([30])
+    for i in range(3):
+        col_name = f'{sub}_avg_dvars_{ftype_list[i]}'
+        delta_col_name = f'{sub}_avg_delta_dvars'
+        sub_table[delta_col_name] = ( sub_table[col_name] - sub_table[f'{sub}_avg_dvars_pre'] ) / sub_table[f'{sub}_avg_fd']
+
+        slopes[ses*(i+1)-1] = compute_slope(sub_table[delta_col_name], sub_table[f'{sub}_svg_fd'])
+        slope_table[f'{sub}_avg_{ftype_list[i]}'] = slopes[i*10:(i*10+10)]
 
 
 

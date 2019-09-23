cb5a9875e89fc0fd7cf6256abaca5b6c96b26bec
smoia <s.moia@bcbl.eu>
2019-09-23 12:45:51 +0200

    continued scripting compare motion denoise

--- 51.compare_motion_denoise.sh
+++ 51.compare_motion_denoise.sh
@@ -23,16 +23,16 @@ do
 	for ses in $( seq -f %02g 1 9 )
 	do
 		flpr=sub-${sub}_ses-${ses}
-		cp ${wdr}/preproc/sub-${sub}/ses-${ses}/func_preproc/${flpr}_task-breathhold_echo-1_bold_dvars.par sub-${sub}/dvars_pre_ses-${ses}.1D
-		# cp ${wdr}/preproc/sub-${sub}/ses-${ses}/func_preproc/${flpr}_task-breathhold_echo-1_bold_dvars_pre.par sub-${sub}/dvars_pre_ses-${ses}.1D
-		cp ${wdr}/preproc/sub-${sub}/ses-${ses}/func_preproc/${flpr}_task-breathhold_echo-1_bold_fd.par sub-${sub}/fd_ses-${ses}.1D
+		cp ${wdr}/preproc/sub-${sub}/ses-${ses}/func_preproc/${flpr}_task-breathhold_echo-1_bold_dvars.par sub-${sub}/dvars_pre_sub-${sub}_ses-${ses}.1D
+		# cp ${wdr}/preproc/sub-${sub}/ses-${ses}/func_preproc/${flpr}_task-breathhold_echo-1_bold_dvars_pre.par sub-${sub}/dvars_pre_sub-${sub}_ses-${ses}.1D
+		cp ${wdr}/preproc/sub-${sub}/ses-${ses}/func_preproc/${flpr}_task-breathhold_echo-1_bold_fd.par sub-${sub}/fd_sub-${sub}_ses-${ses}.1D
 
 		fsl_motion_outliers -i ${wdr}/preproc/sub-${sub}/ses-${ses}/00.${flpr}_task-breathhold_optcom_bold_native_preprocessed \
-		-o tmp_out -s dvars_optcom_ses-${ses}.1D --dvars --nomoco
+		-o tmp_out -s dvars_optcom_sub-${sub}_ses-${ses}.1D --dvars --nomoco
 		fsl_motion_outliers -i ${wdr}/preproc/sub-${sub}/ses-${ses}/00.${flpr}_task-breathhold_echo-2_bold_native_preprocessed \
-		-o tmp_out -s dvars_echo-2_ses-${ses}.1D --dvars --nomoco
+		-o tmp_out -s dvars_echo-2_sub-${sub}_ses-${ses}.1D --dvars --nomoco
 		fsl_motion_outliers -i ${wdr}/preproc/sub-${sub}/ses-${ses}/${flpr}_task-breathhold_meica_bold_bet \
-		-o tmp_out -s dvars_meica_ses-${ses}.1D --dvars --nomoco
+		-o tmp_out -s dvars_meica_sub-${sub}_ses-${ses}.1D --dvars --nomoco
 	done
 done
 
--- compare_motion_denoise.py
+++ compare_motion_denoise.py
@@ -18,11 +18,17 @@ sub_table = pd. genemptydf
 
 for sub in sub_list:
     for ses in range(1, 10):
-        for 
-        for ftype in ['echo-2','optcom','meica']:
-            colname = f'{sub}_{ses:02d}_{ftype}'            
-            sub_table[f'{sub}_']
+        for mottype in ['dvars_pre', 'fd', 'dvars']:
+            if mottype == 'dvars':
+                for ftype in ['echo-2', 'optcom', 'meica']:
+                    colname = f'{sub}_{ses:02d}_{mottype}_{ftype}'
+                    filename = f'{mottype}_{ftype}_sub-{sub}_ses-{ses}.1D'
+                    sub_table[colname] = np.genfromtxt(filename)
 
+            else:
+                colname = f'{sub}_{ses:02d}_{mottype}'
+                filename = f'{mottype}_sub-{sub}_ses-{ses}.1D'
+                sub_table[colname] = np.genfromtxt(filename)
 
 
 

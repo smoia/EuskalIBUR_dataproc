2a8979bfa6e50ae0efe9b353ad8077eb7ef6cd49
smoia <s.moia@bcbl.eu>
2019-09-27 15:49:23 +0200

    Corrected filepaths and added final rm

--- 51.compare_motion_denoise.sh
+++ 51.compare_motion_denoise.sh
@@ -27,15 +27,17 @@ do
 		cp ${wdr}/sub-${sub}/ses-${ses}/func_preproc/${flpr}_task-breathhold_echo-1_bold_dvars_pre.par sub-${sub}/dvars_pre_sub-${sub}_ses-${ses}.1D
 		cp ${wdr}/sub-${sub}/ses-${ses}/func_preproc/${flpr}_task-breathhold_echo-1_bold_fd.par sub-${sub}/fd_sub-${sub}_ses-${ses}.1D
 
-		fsl_motion_outliers -i ${wdr}/sub-${sub}/ses-${ses}/00.${flpr}_task-breathhold_optcom_bold_native_preprocessed \
+		fsl_motion_outliers -i ${wdr}/sub-${sub}/ses-${ses}/func_preproc/00.${flpr}_task-breathhold_optcom_bold_native_preprocessed \
 		-o tmp_out -s dvars_optcom_sub-${sub}_ses-${ses}.1D --dvars --nomoco
-		fsl_motion_outliers -i ${wdr}/sub-${sub}/ses-${ses}/00.${flpr}_task-breathhold_echo-2_bold_native_preprocessed \
+		fsl_motion_outliers -i ${wdr}/sub-${sub}/ses-${ses}/func_preproc/00.${flpr}_task-breathhold_echo-2_bold_native_preprocessed \
 		-o tmp_out -s dvars_echo-2_sub-${sub}_ses-${ses}.1D --dvars --nomoco
-		fsl_motion_outliers -i ${wdr}/sub-${sub}/ses-${ses}/${flpr}_task-breathhold_meica_bold_bet \
+		fsl_motion_outliers -i ${wdr}/sub-${sub}/ses-${ses}/func_preproc/${flpr}_task-breathhold_meica_bold_bet \
 		-o tmp_out -s dvars_meica_sub-${sub}_ses-${ses}.1D --dvars --nomoco
 	done
 done
 
 # add call to python!
+
+rm tmp*
 
 cd ${cwd}

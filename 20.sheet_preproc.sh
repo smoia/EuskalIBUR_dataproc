8fd693b3bb0e67d68cdd549ac7f7b92f56534239
smoia <s.moia@bcbl.eu>
2019-09-28 00:38:25 +0200

    Rename variables and files

--- 20.sheet_preproc.sh
+++ 20.sheet_preproc.sh
@@ -15,21 +15,27 @@ do
 	for ses in $( seq -f %02g 1 9 )
 	do
 		echo "Denoising sub ${sub} ses ${ses}"
-		func=sub-${sub}/ses-${ses}/func_preproc/00.sub-${sub}_ses-${ses}_task-breathhold_optcom_bold_native_preprocessed.nii.gz
-		fsl_regfilt -i ${func} \
-		-d sub-${sub}/ses-${ses}/func_preproc/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
-		-f "$( cat sub-${sub}_ses-${ses}_rejected.1D )" \
-		-o sub-${sub}/ses-${ses}/func_preproc/sub-${sub}_ses-${ses}_task-breathhold_meica_bold_bet
-		fsl_regfilt -i ${func} \
-		-d sub-${sub}/ses-${ses}/func_preproc/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
-		-f "$( cat sub-${sub}_ses-${ses}_vascular.1D )" \
-		-o sub-${sub}/ses-${ses}/func_preproc/sub-${sub}_ses-${ses}_task-breathhold_meica_bold_bet_vascular
-		fsl_regfilt -i ${func} \
-		-d sub-${sub}/ses-${ses}/func_preproc/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
-		-f "$( cat sub-${sub}_ses-${ses}_networks.1D )" \
-		-o sub-${sub}/ses-${ses}/func_preproc/sub-${sub}_ses-${ses}_task-breathhold_meica_bold_bet_network
+		fdir=sub-${sub}/ses-${ses}/func_preproc
+		func=${fdir}/00.sub-${sub}_ses-${ses}_task-breathhold_optcom_bold_native_preprocessed
+		bold=sub-${sub}_ses-${ses}_task-breathhold_meica_bold
 
-		exit
+		fsl_regfilt -i ${func} \
+		-d ${fdir}/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
+		-f "$( cat sub-${sub}_ses-${ses}_rejected.1D )" \
+		-o ${fdir}/${bold}_bet
+		fsl_regfilt -i ${func} \
+		-d ${fdir}/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
+		-f "$( cat sub-${sub}_ses-${ses}_vascular.1D )" \
+		-o ${fdir}/${bold}_bet_vascular
+		fsl_regfilt -i ${func} \
+		-d ${fdir}/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
+		-f "$( cat sub-${sub}_ses-${ses}_networks.1D )" \
+		-o ${fdir}/${bold}_bet_network
+
+		./11.func_spc.sh ${bold}_bet ${fdir}
+
+		immv ${fdir}/${bold}_bet ${fdir}/00.${bold}_native_preprocessed
+		immv ${fdir}/${bold}_bet_SPC ${fdir}/01.${bold}_native_SPC_preprocessed
 	done
 done
 

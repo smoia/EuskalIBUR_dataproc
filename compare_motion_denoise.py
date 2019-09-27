Eliminated average column, bug fixes

compare_motion_denoise.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 def compute_slope(x, y):
-    m = ((x*y).mean - x.mean*y.mean) / ((x**2).mean() - (x.mean())**2)
+    m = ((x*y).mean() - x.mean()*y.mean()) / ((x**2).mean() - (x.mean())**2)
     return m
 
 
@@ -24,8 +24,8 @@ sub_table = pd.DataFrame()
 slope_table = pd.DataFrame()
 
 for sub in sub_list:
+    slopes = np.zeros([30])
     for ses in range(1, 10):
-        slopes = np.zeros([30])
 
         for mottype in ['dvars_pre', 'fd']:
             col_name = f'{sub}_{ses:02d}_{mottype}'
@@ -44,37 +44,17 @@ for sub in sub_list:
             filename = f'sub-{sub}/dvars_{ftype_list[i]}_sub-{sub}_ses-{ses:02d}.1D'
             sub_table[col_name] = np.genfromtxt(filename)
 
-            avg_col_name = f'{sub}_avg_dvars_{ftype_list[i]}'
-            if ses == 1:
-                sub_table[avg_col_name] = sub_table[col_name]
-            else:
-                sub_table[avg_col_name] = sub_table[avg_col_name] + sub_table[col_name]
-
-            delta_col_name = f'{sub}_{ses:02d}_delta_dvars'
+            delta_col_name = f'{sub}_{ses:02d}_delta_dvars_{ftype_list[i]}'
             sub_table[delta_col_name] = ((sub_table[col_name] -
                                          sub_table[f'{sub}_{ses:02d}_dvars_pre'])
                                          / sub_table[f'{sub}_{ses:02d}_fd'])
             sub_table[delta_col_name][0] = 0
 
-            slopes[ses*(i+1)-1] = compute_slope(np.array(sub_table[delta_col_name]),
-                                                np.array(sub_table[f'{sub}_{ses:02d}_fd']))
+            slopes[i*10+ses-1] = compute_slope(np.array(sub_table[delta_col_name]),
+                                               np.array(sub_table[f'{sub}_{ses:02d}_fd']))
 
-        for i in range(3):
-            slope_table[f'{sub}_{ses:02d}_{ftype_list[i]}'] = slopes[i*10:(i*10+10)]
-
-    slopes = np.zeros([30])
     for i in range(3):
-        col_name = f'{sub}_avg_dvars_{ftype_list[i]}'
-        delta_col_name = f'{sub}_avg_delta_dvars'
-        sub_table[delta_col_name] = ((sub_table[col_name] -
-                                     sub_table[f'{sub}_avg_dvars_pre']) /
-                                     sub_table[f'{sub}_avg_fd'])
-        sub_table[delta_col_name][0] = 0
-
-        slopes[ses*(i+1)-1] = compute_slope(np.array(sub_table[delta_col_name]),
-                                            np.array(sub_table[f'{sub}_svg_fd']))
-        slope_table[f'{sub}_avg_{ftype_list[i]}'] = slopes[i*10:(i*10+10)]
-
+        slope_table[f'{sub}_{ftype_list[i]}'] = slopes[i*10:(i*10+10)]
 
 sub_table.to_csv('sub_table.csv', compression='gzip')
 slope_table.to_csv('slope_table.csv', compression='gzip')
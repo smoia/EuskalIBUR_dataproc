2ce07e787bb9c7ffd3de5a88abfadfa3155afdb9
smoia <s.moia@bcbl.eu>
2019-09-27 15:44:49 +0200

    Corrected main floder

--- 51.compare_motion_denoise.sh
+++ 51.compare_motion_denoise.sh
@@ -24,14 +24,14 @@ do
 	for ses in $( seq -f %02g 1 9 )
 	do
 		flpr=sub-${sub}_ses-${ses}
-		cp ${wdr}/preproc/sub-${sub}/ses-${ses}/func_preproc/${flpr}_task-breathhold_echo-1_bold_dvars_pre.par sub-${sub}/dvars_pre_sub-${sub}_ses-${ses}.1D
-		cp ${wdr}/preproc/sub-${sub}/ses-${ses}/func_preproc/${flpr}_task-breathhold_echo-1_bold_fd.par sub-${sub}/fd_sub-${sub}_ses-${ses}.1D
+		cp ${wdr}/sub-${sub}/ses-${ses}/func_preproc/${flpr}_task-breathhold_echo-1_bold_dvars_pre.par sub-${sub}/dvars_pre_sub-${sub}_ses-${ses}.1D
+		cp ${wdr}/sub-${sub}/ses-${ses}/func_preproc/${flpr}_task-breathhold_echo-1_bold_fd.par sub-${sub}/fd_sub-${sub}_ses-${ses}.1D
 
-		fsl_motion_outliers -i ${wdr}/preproc/sub-${sub}/ses-${ses}/00.${flpr}_task-breathhold_optcom_bold_native_preprocessed \
+		fsl_motion_outliers -i ${wdr}/sub-${sub}/ses-${ses}/00.${flpr}_task-breathhold_optcom_bold_native_preprocessed \
 		-o tmp_out -s dvars_optcom_sub-${sub}_ses-${ses}.1D --dvars --nomoco
-		fsl_motion_outliers -i ${wdr}/preproc/sub-${sub}/ses-${ses}/00.${flpr}_task-breathhold_echo-2_bold_native_preprocessed \
+		fsl_motion_outliers -i ${wdr}/sub-${sub}/ses-${ses}/00.${flpr}_task-breathhold_echo-2_bold_native_preprocessed \
 		-o tmp_out -s dvars_echo-2_sub-${sub}_ses-${ses}.1D --dvars --nomoco
-		fsl_motion_outliers -i ${wdr}/preproc/sub-${sub}/ses-${ses}/${flpr}_task-breathhold_meica_bold_bet \
+		fsl_motion_outliers -i ${wdr}/sub-${sub}/ses-${ses}/${flpr}_task-breathhold_meica_bold_bet \
 		-o tmp_out -s dvars_meica_sub-${sub}_ses-${ses}.1D --dvars --nomoco
 	done
 done

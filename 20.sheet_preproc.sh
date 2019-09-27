4cf86972ea7700bb4e17d6eab4c3717a5e2a695c
smoia <s.moia@bcbl.eu>
2019-09-28 00:28:32 +0200

    Changed input of fslregfilt

--- 20.sheet_preproc.sh
+++ 20.sheet_preproc.sh
@@ -15,18 +15,21 @@ do
 	for ses in $( seq -f %02g 1 9 )
 	do
 		echo "Denoising sub ${sub} ses ${ses}"
-		fsl_regfilt -i sub-${sub}/ses-${ses}/func_preproc/sub-${sub}_ses-${ses}_task-breathhold_optcom_bold_bet \
+		func=sub-${sub}/ses-${ses}/func_preproc/00.sub-${sub}_ses-${ses}_task-breathhold_optcom_bold_native_preprocessed.nii.gz
+		fsl_regfilt -i ${func} \
 		-d sub-${sub}/ses-${ses}/func_preproc/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
 		-f "$( cat sub-${sub}_ses-${ses}_rejected.1D )" \
 		-o sub-${sub}/ses-${ses}/func_preproc/sub-${sub}_ses-${ses}_task-breathhold_meica_bold_bet
-		fsl_regfilt -i sub-${sub}/ses-${ses}/func_preproc/sub-${sub}_ses-${ses}_task-breathhold_optcom_bold_bet \
+		fsl_regfilt -i ${func} \
 		-d sub-${sub}/ses-${ses}/func_preproc/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
 		-f "$( cat sub-${sub}_ses-${ses}_vascular.1D )" \
 		-o sub-${sub}/ses-${ses}/func_preproc/sub-${sub}_ses-${ses}_task-breathhold_meica_bold_bet_vascular
-		fsl_regfilt -i sub-${sub}/ses-${ses}/func_preproc/sub-${sub}_ses-${ses}_task-breathhold_optcom_bold_bet \
+		fsl_regfilt -i ${func} \
 		-d sub-${sub}/ses-${ses}/func_preproc/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
 		-f "$( cat sub-${sub}_ses-${ses}_network.1D )" \
 		-o sub-${sub}/ses-${ses}/func_preproc/sub-${sub}_ses-${ses}_task-breathhold_meica_bold_bet_network
+
+		exit
 	done
 done
 

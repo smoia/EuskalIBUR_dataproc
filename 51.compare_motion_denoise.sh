e8e885413a4ee752d7cae3cc9c08fc3eb01d53b3
smoia <s.moia@bcbl.eu>
2019-09-27 15:37:40 +0200

    Corrected dvars file, loop

--- 51.compare_motion_denoise.sh
+++ 51.compare_motion_denoise.sh
@@ -16,14 +16,13 @@ mkdir ME_Denoising
 
 cd ME_Denoising
 
-for sub in 002 003 007
+for sub in 007 003 002
 do
 	mkdir sub-${sub}
 	for ses in $( seq -f %02g 1 9 )
 	do
 		flpr=sub-${sub}_ses-${ses}
-		cp ${wdr}/preproc/sub-${sub}/ses-${ses}/func_preproc/${flpr}_task-breathhold_echo-1_bold_dvars.par sub-${sub}/dvars_pre_sub-${sub}_ses-${ses}.1D
-		# cp ${wdr}/preproc/sub-${sub}/ses-${ses}/func_preproc/${flpr}_task-breathhold_echo-1_bold_dvars_pre.par sub-${sub}/dvars_pre_sub-${sub}_ses-${ses}.1D
+		cp ${wdr}/preproc/sub-${sub}/ses-${ses}/func_preproc/${flpr}_task-breathhold_echo-1_bold_dvars_pre.par sub-${sub}/dvars_pre_sub-${sub}_ses-${ses}.1D
 		cp ${wdr}/preproc/sub-${sub}/ses-${ses}/func_preproc/${flpr}_task-breathhold_echo-1_bold_fd.par sub-${sub}/fd_sub-${sub}_ses-${ses}.1D
 
 		fsl_motion_outliers -i ${wdr}/preproc/sub-${sub}/ses-${ses}/00.${flpr}_task-breathhold_optcom_bold_native_preprocessed \

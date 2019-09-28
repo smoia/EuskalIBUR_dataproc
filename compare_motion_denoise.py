b3324871ae9e96054dadee6e695e7a6b34b516e6
smoia <s.moia@bcbl.eu>
2019-09-28 02:19:33 +0200

    Rename variables

--- compare_motion_denoise.py
+++ compare_motion_denoise.py
@@ -32,50 +32,26 @@ for sub in sub_list:
             filename = f'sub-{sub}/{mottype}_sub-{sub}_ses-{ses:02d}.1D'
             sub_table[col_name] = np.genfromtxt(filename)
 
-            avg_col_name = f'{sub}_avg_{mottype}'
-            if ses == 1:
-                sub_table[avg_col_name] = sub_table[col_name]
-            else:
-                sub_table[avg_col_name] = sub_table[avg_col_name] + sub_table[col_name]
-
         # loop for ftype_list
-        # for i in range(3):
-        for i in range(2):
-            col_name = f'{sub}_{ses:02d}_dvars_{ftype_list[i]}'
+        dvars_pre = f'{sub}_{ses:02d}_dvars_pre'
+        for i in range(3):
+            dvars_type = f'{sub}_{ses:02d}_dvars_{ftype_list[i]}'
             filename = f'sub-{sub}/dvars_{ftype_list[i]}_sub-{sub}_ses-{ses:02d}.1D'
-            sub_table[col_name] = np.genfromtxt(filename)
+            sub_table[dvars_type] = np.genfromtxt(filename)
 
-            delta_col_name = f'{sub}_{ses:02d}_delta_dvars_{ftype_list[i]}'
-            sub_table[delta_col_name] = ((sub_table[col_name] -
-                                         sub_table[f'{sub}_{ses:02d}_dvars_pre'])
-                                         / sub_table[f'{sub}_{ses:02d}_fd'])
-            sub_table[delta_col_name][0] = 0
+            delta_dvars = f'{sub}_{ses:02d}_delta_dvars_{ftype_list[i]}'
+            dvars_fd = f'{sub}_{ses:02d}_delta_dvars_{ftype_list[i]}_over_fd'
+            sub_table[delta_dvars] = sub_table[dvars_pre] - sub_table[dvars_type]
+            sub_table[dvars_fd] = sub_table[delta_dvars] / sub_table[f'{sub}_{ses:02d}_fd']
+            sub_table[delta_dvars][0] = 0
 
-            slopes[i*10+ses-1] = compute_slope(np.array(sub_table[delta_col_name]),
+            slopes[i*10+ses-1] = compute_slope(np.array(sub_table[delta_dvars]),
                                                np.array(sub_table[f'{sub}_{ses:02d}_fd']))
 
-    # for i in range(3):
-    for i in range(2):
+    for i in range(3):
         slope_table[f'{sub}_{ftype_list[i]}'] = slopes[i*10:(i*10+10)]
 
 sub_table.to_csv('sub_table.csv')
 slope_table.to_csv('slope_table.csv')
-
-    # Need to make graphs
-
-    # PSEUDOCODE
-
-    # Graph one: slopes
-    # For each session, for each ftype
-        # Create a fake point that is (10,10*m)
-        # Plot a line in blue?
-    # Average: repeat session but plot in red
-
-    # Graph two: violin plot
-    # For each ftype
-        # Append all the subjects and sessions
-        # If needed, tidy the matrix
-        # Violin plot
-
 
 os.chdir(cwd)

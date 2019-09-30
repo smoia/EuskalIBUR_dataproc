c59c2d8b19c7cb1a876a18c75ea68d9cdf16bb60
smoia <s.moia@bcbl.eu>
2019-09-30 17:11:35 +0200

    Generalised table creation with all echoes rather than just echo 2

--- compare_motion_denoise.py
+++ compare_motion_denoise.py
@@ -5,6 +5,7 @@ import matplotlib
 import pandas as pd
 import numpy as np
 
+NTE = 5
 
 def compute_slope(x, y):
     m = ((x*y).mean() - x.mean()*y.mean()) / ((x**2).mean() - (x.mean())**2)
@@ -18,13 +19,18 @@ os.chdir('/data')
 os.chdir('ME_Denoising')
 
 sub_list = ['007', '003', '002']
-ftype_list = ['echo-2', 'optcom', 'meica']
+ftype_list = ['optcom', 'meica']
+
+for e in range(1, NTE):
+    ftype_list.append(f'echo-{e+1}')
+    ftype_list.append(f'meica_echo-{e+1}')
+
 
 sub_table = pd.DataFrame()
 slope_table = pd.DataFrame()
 
 for sub in sub_list:
-    slopes = np.zeros([30])
+    slopes = np.zeros([len(ftype_list)*10])
     for ses in range(1, 10):
 
         for mottype in ['dvars_pre', 'fd']:
@@ -34,22 +40,24 @@ for sub in sub_list:
 
         # loop for ftype_list
         dvars_pre = f'{sub}_{ses:02d}_dvars_pre'
-        for i in range(3):
+        for i in range(len(ftype_list)):
             dvars_type = f'{sub}_{ses:02d}_dvars_{ftype_list[i]}'
             filename = f'sub-{sub}/dvars_{ftype_list[i]}_sub-{sub}_ses-{ses:02d}.1D'
             sub_table[dvars_type] = np.genfromtxt(filename)
 
             delta_dvars = f'{sub}_{ses:02d}_delta_dvars_{ftype_list[i]}'
-            dvars_norm = f'{sub}_{ses:02d}_delta_dvars_{ftype_list[i]}_normalised'
+            dvars_norm = f'{sub}_{ses:02d}_norm_dvars_{ftype_list[i]}'
             sub_table[delta_dvars] = sub_table[dvars_pre] - sub_table[dvars_type]
             sub_table[dvars_norm] = sub_table[delta_dvars] / sub_table[dvars_type]
             sub_table[dvars_norm][0] = 0
 
-            slopes[i*10+ses-1] = compute_slope(np.array(sub_table[delta_dvars]),
-                                               np.array(sub_table[f'{sub}_{ses:02d}_fd']))
+            slopes[i*10+ses-1] = compute_slope(np.array(sub_table[delta_dvars][1:]),
+                                               np.array(sub_table[f'{sub}_{ses:02d}_fd'][1:]))
 
-    for i in range(3):
+    for i in range(len(ftype_list)):
         slope_table[f'{sub}_{ftype_list[i]}'] = slopes[i*10:(i*10+10)]
+
+sub_table.drop(sub_table.index[0])
 
 sub_table.to_csv('sub_table.csv')
 slope_table.to_csv('slope_table.csv')

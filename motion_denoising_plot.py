4c5d6dcdf71ec886fd334275d8c879e198bd3e7b
smoia <s.moia@bcbl.eu>
2019-09-30 17:12:26 +0200

    Added plot for normalised DVARS

--- motion_denoising_plot.py
+++ motion_denoising_plot.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import os
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
@@ -19,79 +18,81 @@ data = pd.read_csv('sub_table.csv')
 colours = ['#1f77b4ff', '#ff7f0eff', '#2ca02cff', '#d62728ff']
 ftype_list = ['pre', 'echo-2', 'optcom', 'meica']
 sub_list = ['007', '003', '002']
+dvars_list = ['norm', 'simple']
+
 
 for sub in sub_list:
-    plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
-    plt.title(f'DVARS vs FD sub {sub}')
+    for dvars_type in dvars_list:
+        plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
+        plot_title = f'DVARS vs FD sub {sub}'
+        if dvars_type == 'norm':
+            plot_title = f'NORM {plot_title}'
 
-    for ses in range(1,10):
-        x_col=f'{sub}_{ses:02g}_fd'
-        # loop for ftype
-        for i in range(4):
-            y_col=f'{sub}_{ses:02g}_dvars_{ftype_list[i]}'
-            sns.regplot(x=data[x_col], y=data[y_col], fit_reg=True, label=ftype_list[i], color=colours[i])
-    
-    plt.legend()
-    plt.xlabel('FD')
-    plt.xlim(-1, 5)
-    plt.ylabel('DVARS')
-    plt.ylim(-80, 300)
-    plt.savefig(f'{sub}_DVARS_vs_FD.png', dpi=SET_DPI)
-    plt.clf()
-    plt.close()
+        plt.title(plot_title)
 
-    plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
-    plt.title(f'NORM DVARS vs FD sub {sub}')
+        for ses in range(1, 10):
+            x_col = f'{sub}_{ses:02g}_fd'
+            # loop for ftype
+            for i in range(4):
+                if dvars_type == 'simple':
+                    y_col = f'{sub}_{ses:02g}_dvars_{ftype_list[i]}'
+                else:
+                    y_col = f'{sub}_{ses:02g}_{dvars_type}_dvars_{ftype_list[i]}'
 
-    for ses in range(1,10):
-        x_col=f'{sub}_{ses:02g}_fd'
-        # loop for ftype
-        for i in range(4):
-            y_col=f'{sub}_{ses:02g}_dvars_{ftype_list[i]}'
-            sns.regplot(x=data[x_col], y=data[y_col], fit_reg=True, label=ftype_list[i], color=colours[i])
-    
-    plt.legend()
-    plt.xlabel('FD')
-    plt.xlim(-1, 5)
-    plt.ylabel('DVARS')
-    plt.ylim(-80, 300)
-    plt.savefig(f'{sub}_NORM_DVARS_vs_FD.png', dpi=SET_DPI)
-    plt.clf()
-    plt.close()
+                sns.regplot(x=data[x_col], y=data[y_col], fit_reg=True,
+                            label=ftype_list[i], color=colours[i])
+
+        plt.legend()
+        plt.xlabel('FD')
+        plt.xlim(-1, 5)
+        plot_ylabel = 'DVARS'
+        if dvars_type == 'norm':
+            plot_ylabel = f'NORM {plot_ylabel}'
+
+        plt.ylabel(plot_ylabel)
+        plt.ylim(-80, 300)
+        if dvars_type == 'simple':
+            fig_name = f'{sub}_DVARS_vs_FD.png'
+        else:
+            fig_name = f'{sub}_{dvars_type}_DVARS_vs_FD.png'
+
+        plt.savefig(fig_name, dpi=SET_DPI)
+        plt.clf()
+        plt.close()
 
 plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
-plt.title('DVARS vs FD allsubs')
+plot_title = f'DVARS vs FD sub {sub}'
+if dvars_type == 'norm':
+    plot_title = f'NORM {plot_title}'
+
+plt.title(plot_title)
 for sub in sub_list:
-    for ses in range(1,10):
-        x_col=f'{sub}_{ses:02g}_fd'
+    for ses in range(1, 10):
+        x_col = f'{sub}_{ses:02g}_fd'
         # loop for ftype
         for i in range(4):
-            y_col=f'{sub}_{ses:02g}_dvars_{ftype_list[i]}'
-            sns.regplot(x=data[x_col], y=data[y_col], scatter=False, fit_reg=True, label=ftype_list[i], color=colours[i])
+            if dvars_type == 'simple':
+                y_col = f'{sub}_{ses:02g}_dvars_{ftype_list[i]}'
+            else:
+                y_col = f'{sub}_{ses:02g}_{dvars_type}_dvars_{ftype_list[i]}'
+
+            sns.regplot(x=data[x_col], y=data[y_col], scatter=False,
+                        fit_reg=True, label=ftype_list[i], color=colours[i])
 
 plt.legend()
 plt.xlabel('FD')
-plt.ylabel('DVARS')
-plt.savefig(f'Allsubs_DVARS_vs_FD.png', dpi=SET_DPI)
+plot_ylabel = 'DVARS'
+if dvars_type == 'norm':
+    plot_ylabel = f'NORM {plot_ylabel}'
+
+plt.ylabel(plot_ylabel)
+if dvars_type == 'simple':
+    fig_name = f'{sub}_DVARS_vs_FD.png'
+else:
+    fig_name = f'{sub}_{dvars_type}_DVARS_vs_FD.png'
+
+plt.savefig(fig_name, dpi=SET_DPI)
 plt.clf()
 plt.close()
-
-plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
-plt.title('NORM DVARS vs FD allsubs')
-for sub in sub_list:
-    for ses in range(1,10):
-        x_col=f'{sub}_{ses:02g}_fd'
-        # loop for ftype
-        for i in range(4):
-            y_col=f'{sub}_{ses:02d}_delta_dvars_{ftype_list[i]}_normalised'
-            sns.regplot(x=data[x_col], y=data[y_col], scatter=False, fit_reg=True, label=ftype_list[i], color=colours[i])
-
-plt.legend()
-plt.xlabel('FD')
-plt.ylabel('NORM DVARS')
-plt.savefig(f'Allsubs_NORM_DVARS_vs_FD.png', dpi=SET_DPI)
-plt.clf()
-plt.close()
-
 
 os.chdir(cwd)

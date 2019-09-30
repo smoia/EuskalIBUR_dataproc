e9e7b0bd705e2b16484c4438db6d4f198869cd93
smoia <s.moia@bcbl.eu>
2019-09-30 19:26:30 +0200

    Started plotting CVR response

--- motion_denoising_plot.py
+++ motion_denoising_plot.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import os
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
 SET_DPI = 100
 FIGSIZE = (18, 10)
-
+BH_LEN = 39  # in TRs
 
 cwd = os.getcwd()
 
@@ -20,7 +21,7 @@ ftype_list = ['pre', 'echo-2', 'optcom', 'meica']
 sub_list = ['007', '003', '002']
 dvars_list = ['norm', 'simple']
 
-
+# 01. Make scatterplots of DVARS vs FD
 for sub in sub_list:
     for dvars_type in dvars_list:
         plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
@@ -94,5 +95,57 @@ else:
 plt.savefig(fig_name, dpi=SET_DPI)
 plt.clf()
 plt.close()
+
+# 02. Make timeseries plots skipping "pre"
+colours = ['#1f77b4ff', '#ff7f0eff', '#2ca02cff', '#d62728ff']
+ftype_list = ['pre', 'echo-2', 'optcom', 'meica']
+
+for sub in sub_list:
+    for i in range(1, len(ftype_list)):
+        bh_responses = []
+        for ses in range(1, 10):
+            avg_gm = np.genfromtxt(f'sub-{sub}_ses-{ses}_GM_{ftype_list[i]}_avg.1D')
+            for bh in range(8):
+                bh_responses.append(avg_gm[BH_LEN*i:BH_LEN*(i+1)])
+
+        
+
+
+
+
+
+plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
+plt.title(plot_title)
+for sub in sub_list:
+    for ses in range(1, 10):
+        x_col = f'{sub}_{ses:02g}_fd'
+        # loop for ftype
+        for i in range(4):
+            if dvars_type == 'simple':
+                y_col = f'{sub}_{ses:02g}_dvars_{ftype_list[i]}'
+            else:
+                y_col = f'{sub}_{ses:02g}_{dvars_type}_dvars_{ftype_list[i]}'
+
+            sns.regplot(x=data[x_col], y=data[y_col], scatter=False,
+                        fit_reg=True, label=ftype_list[i], color=colours[i])
+
+plt.legend()
+plt.xlabel('FD')
+plot_ylabel = 'DVARS'
+if dvars_type == 'norm':
+    plot_ylabel = f'NORM {plot_ylabel}'
+
+plt.ylabel(plot_ylabel)
+if dvars_type == 'simple':
+    fig_name = f'{sub}_DVARS_vs_FD.png'
+else:
+    fig_name = f'{sub}_{dvars_type}_DVARS_vs_FD.png'
+
+plt.savefig(fig_name, dpi=SET_DPI)
+plt.clf()
+plt.close()
+
+
+
 
 os.chdir(cwd)

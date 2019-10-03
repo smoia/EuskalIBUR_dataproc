23e4f0f0dc56257078215d45befe4f118d813808
smoia <s.moia@bcbl.eu>
2019-10-03 12:55:15 +0200

    Added resting state clearing, added modularisation

--- funcclearspace.sh
+++ funcclearspace.sh
@@ -10,6 +10,8 @@
 sub=$1
 ses=$2
 wdr=${3:-/data}
+part=${4:-all}
+# part can be anat, rest, task, all
 
 flpr=sub-${sub}_ses-${ses}
 
@@ -50,53 +52,114 @@ echo "***    Cleaning ${flpr}   ***"
 echo "************************************"
 echo "************************************"
 echo ""
+echo "Selected to clean: ${part}"
+echo ""
 
-echo "************************************"
-echo "*** Anat clean ${anat1}"
-echo "************************************"
-echo "************************************"
+if [[ ${part} == "anat" || ${part} == "all" ]]
+then
 
-./98.anat_delete.sh ${adir} ${anat1}
-./98.anat_delete.sh ${adir} ${anat2}
+	echo "************************************"
+	echo "*** Anat clean ${anat1}"
+	echo "************************************"
+	echo "************************************"
 
-for f in breathhold TASK1 TASK2 TASK3
-do 
-	for d in AP PA
-	do
+	./98.anat_delete.sh ${adir} ${anat1}
+	./98.anat_delete.sh ${adir} ${anat2}
+fi
+
+if [[ ${part} == "task" || ${part} == "all" ]]
+then
+
+	for f in breathhold  # TASK1 TASK2 TASK3
+	do 
+		for d in AP PA
+		do
+
+			echo "************************************"
+			echo "*** Fmap clean ${f} PE ${d}"
+			echo "************************************"
+			echo "************************************"
+
+			func=${flpr}_acq-${f}_dir-${d}_epi
+			./99.fmap_delete.sh ${fmap} ${func}
+		done
+
+		for e in $( seq 1 ${nTE} )
+		do
+
+			echo "************************************"
+			echo "*** Func clean ${f} echo ${e}"
+			echo "************************************"
+			echo "************************************"
+
+			sbrf=${flpr}_task-${f}_rec-magnitude_echo-${e}_sbref
+			bold=${flpr}_task-${f}_echo-${e}_bold
+			./97.func_delete.sh ${fdir} ${sbrf}
+			./97.func_delete.sh ${fdir} ${bold}
+
+		done
 
 		echo "************************************"
-		echo "*** Fmap clean ${f} PE ${d}"
+		echo "*** Func clean ${f} optcom"
 		echo "************************************"
 		echo "************************************"
 
-		func=${flpr}_acq-${f}_dir-${d}_epi
-		./99.fmap_delete.sh ${fmap} ${func}
+		./97.func_delete.sh ${fdir} ${flpr}_task-${f}_optcom_bold
+
+		echo "************************************"
+		echo "*** MEICA clean ${f}"
+		echo "************************************"
+		echo "************************************"
+
+		./96.meica_delete.sh ${fdir} ${flpr}_task-${f}_echo-1_bold_RPI_bet
+
 	done
 
-	for e in $( seq 1 ${nTE} )
-	do
+fi
+
+# Rest
+
+if [[ ${part} == "rest" || ${part} == "all" ]]
+then
+
+	for r in $( seq -f %02g 1 4 )
+	do 
+		for d in AP PA
+		do
+
+			echo "************************************"
+			echo "*** Fmap clean rest run ${r} PE ${d}"
+			echo "************************************"
+			echo "************************************"
+
+			func=${flpr}_acq-rest_dir-${d}_run-${r}_epi
+			./99.fmap_delete.sh ${fmap} ${func}
+		done
+
+		for e in $( seq 1 ${nTE} )
+		do
+
+			echo "************************************"
+			echo "*** Func clean rest run ${r} echo ${e}"
+			echo "************************************"
+			echo "************************************"
+
+			sbrf=${flpr}_task-rest_rec-magnitude_run-${run}_echo-${e}_sbref
+			bold=${flpr}_task-rest_run-${run}_echo-${e}_bold
+			./97.func_delete.sh ${fdir} ${sbrf}
+			./97.func_delete.sh ${fdir} ${bold}
+
+		done
 
 		echo "************************************"
-		echo "*** Func clean ${f} echo ${e}"
+		echo "*** MEICA clean rest run ${r}"
 		echo "************************************"
 		echo "************************************"
 
-		sbrf=${flpr}_task-${f}_rec-magnitude_echo-${e}_sbref
-		bold=${flpr}_task-${f}_echo-${e}_bold
-		./97.func_delete.sh ${fdir} ${sbrf}
-		./97.func_delete.sh ${fdir} ${bold}
+		./96.meica_delete.sh ${fdir} ${flpr}_task-rest_run-${run}_echo-1_bold_RPI_bet
 
 	done
 
-	echo "************************************"
-	echo "*** MEICA clean ${f}"
-	echo "************************************"
-	echo "************************************"
-
-	./96.meica_delete.sh ${fdir} ${flpr}_task-${f}_echo-1_bold_RPI_bet
-
-done
-
-# Needs RS
+fi
 
 cd ${cwd}

d86951d1571516705d2414de527fb531799ae953
smoia <s.moia@bcbl.eu>
2019-10-04 00:33:33 +0200

    Wrote motion denoise pipeline

---
+++ pipeline_motiondenoise.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+wdr=${1:-/data}
+
+### Main ###
+
+./20.sheet_preproc.sh
+
+for sub in 007 003 002
+do
+	for ses in $( seq -f %02g 1 9 )
+	do
+		./31.reg_manual_meica.sh ${sub} ${ses}
+		./32.compute_motion_outliers.sh ${sub} ${ses}
+	done
+done
+
+./51.compare_motion_denoise.sh
+
+.71.plot_motion_denoise.sh

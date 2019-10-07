f1ed9294c2ea86efcffed0d699d934a58a533232
smoia <s.moia@bcbl.eu>
2019-10-07 18:52:32 +0200

    Now creating folder and copying file

--- biopac_decimate.py
+++ biopac_decimate.py
@@ -4,8 +4,9 @@ import os
 import biopac_preproc as bio
 import matplotlib.pyplot as plt
 
+from shutil import copyfile
 
-wdir = '/data'
+wdir = '/bcbl/home/public/PJMASK_2/PHYSIO/BIDS_BH'
 SET_DPI = 100
 
 for sub in ['002', '003', '007']:
@@ -16,9 +17,16 @@ for sub in ['002', '003', '007']:
             ch = 4
 
         os.chdir(wdir)
-        path = f'sub-{sub}/ses-{ses:02g}/func_phys'
+        path = f'sub-{sub}/ses-{ses:02g}/func'
         filename = f'sub-{sub}_ses-{ses:02g}_task-breathhold_physio'
         os.chdir(path)
+        if not os.path.exists('../func_phys'):
+            os.mkdir('../func_phys')
+
+        os.chdir('../func_phys')
+        print(f'Copying file {filename}')
+        copyfile(f'../func/{filename}.tsv.gz', f'./{filename}.tsv.gz')
+        print(f'Decimating {filename}')
         co, pidx = bio.partone(filename, ch)
 
         plt.figure(figsize=(18, 10), dpi=SET_DPI)

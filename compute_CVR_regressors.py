941104da1a8a255bec444cb56b1bf4bd07f22194
smoia <s.moia@bcbl.eu>
2019-10-09 17:45:40 +0200

    Split biopac_preproc_token into three

---
+++ compute_CVR_regressors.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import os
+import biopac_preproc as bio
+import numpy as np
+
+wdir = '/data'
+SET_DPI = 100
+
+cwd = os.getcwd()
+
+os.chdir(wdir)
+for sub in ['002', '003', '007']:
+    for ses in range(1, 11):
+        filename = f'sub-{sub}/ses-{ses:02g}/func_phys/sub-{sub}_ses-{ses:02g}_task-breathhold_physio'
+        npidx = np.genfromtxt(f'{filename}_manualpeaks.1D').astype('int')
+        co = np.genfromtxt(f'{filename}_co_orig.1D')
+
+        ftype_list = ['optcom', 'echo-2', 'meica', 'vessels', 'networks']
+        for ftype in ftype_list:
+            GM_name = f'CVR/sub-{sub}_ses-{ses}_GM_{ftype}_avg'
+            bio.parttwo(co, npidx, filename, GM_name)
+
+os.chdir(cwd)

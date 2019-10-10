d6800f1573a693b969a54bf2bda30e798b4714a3
smoia <s.moia@bcbl.eu>
2019-10-10 19:49:06 +0200

    Added comments

--- pipeline_cvr_maps.sh
+++ pipeline_cvr_maps.sh
@@ -24,16 +24,43 @@ echo "************************************"
 # saving the current wokdir
 cwd=$(pwd)
 
+echo "************************************"
+echo "***    CVR mapping ${flpr}    ***"
+echo "************************************"
+echo "************************************"
+echo ""
+echo ""
+echo "************************************"
+echo "*** Processing xslx sheet"
+echo "************************************"
+echo "************************************"
+
 ./20.sheet_preproc.sh
 
 for sub in 007 003 002
 do
 	for ses in $( seq -f %02g 1 9 )
 	do
+
+		echo "************************************"
+		echo "*** Denoising sub ${sub} ses ${ses}"
+		echo "************************************"
+		echo "************************************"
 		./30.reg_manual_meica.sh ${sub} ${ses}
+
+		echo "************************************"
+		echo "*** Preparing CVR sub ${sub} ses ${ses}"
+		echo "************************************"
+		echo "************************************"
 		./21.prepare_CVR_mapping.sh ${sub} ${ses}
 	done
 done
+
+echo "************************************"
+echo "*** Compute CVR regressors"
+echo "************************************"
+echo "************************************"
+
 
 ./24.compute_CVR_regressors.sh
 
@@ -43,12 +70,27 @@ do
 	do
 		for ses in $( seq -f %02g 1 9 )
 		do
+					echo "************************************"
+					echo "*** CVR map sub ${sub} ses ${ses} ${ftype}"
+					echo "************************************"
+					echo "************************************"
+
 			./31.cvr_map.sh ${sub} ${ses} ${ftype} 
 		done
+
+		echo "************************************"
+		echo "*** General CVR sub ${sub} ${ftype}"
+		echo "************************************"
+		echo "************************************"
 
 		./50.generalcvrmaps.sh ${sub} ${ftype}
 	done
 done
+
+echo "************************************"
+echo "*** Plot CVRs"
+echo "************************************"
+echo "************************************"
 
 ./70.plot_cvr_maps.sh
 

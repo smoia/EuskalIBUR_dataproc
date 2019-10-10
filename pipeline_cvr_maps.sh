7454aabcffd059bd997799f5eb58ed333e310b54
smoia <s.moia@bcbl.eu>
2019-10-10 18:39:27 +0200

    Corrected pipeline with new enumeration

--- pipeline_cvr_maps.sh
+++ pipeline_cvr_maps.sh
@@ -24,10 +24,13 @@ echo "************************************"
 # saving the current wokdir
 cwd=$(pwd)
 
+./20.sheet_preproc.sh
+
 for sub in 007 003 002
 do
 	for ses in $( seq -f %02g 1 9 )
 	do
+		./30.reg_manual_meica.sh ${sub} ${ses}
 		./21.prepare_CVR_mapping.sh ${sub} ${ses}
 	done
 done
@@ -40,7 +43,7 @@ do
 	do
 		for ses in $( seq -f %02g 1 9 )
 		do
-			./30.cvr_map.sh ${sub} ${ses} ${ftype} 
+			./31.cvr_map.sh ${sub} ${ses} ${ftype} 
 		done
 
 		./50.generalcvrmaps.sh ${sub} ${ftype}

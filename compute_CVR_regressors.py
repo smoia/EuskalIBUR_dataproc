5a7af4c5d8d3e942cb7860dc3586fed2ec1bc2e9
smoia <s.moia@bcbl.eu>
2019-10-11 09:56:45 +0200

    Corrected indentation

--- compute_CVR_regressors.py
+++ compute_CVR_regressors.py
@@ -11,10 +11,10 @@ cwd = os.getcwd()
 
 os.chdir(wdir)
 for sub in ['007', '003', '002']:
-	if sub == '002':
-		lastses = 10
-	else:
-		lastses = 9
+    if sub == '002':
+        lastses = 10
+    else:
+        lastses = 9
 
     for ses in range(1, 11):
         filename = f'sub-{sub}/ses-{ses:02g}/func_phys/sub-{sub}_ses-{ses:02g}_task-breathhold_physio'

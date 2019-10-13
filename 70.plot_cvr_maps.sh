a0bc8ff6571186fa288d809e03f53da58987d279
smoia <s.moia@bcbl.eu>
2019-10-14 01:39:56 +0200

    Changed path

--- 70.plot_cvr_maps.sh
+++ 70.plot_cvr_maps.sh
@@ -6,7 +6,7 @@ wdr=${1:-/data}
 
 cwd=$( pwd )
 
-cd ${wdr}
+cd ${wdr}/CVR
 
 for sub in 007 003 002
 do
@@ -22,46 +22,46 @@ do
 
 			echo "sub ${sub} ses ${ses} ftype ${ftype}"
 			echo "cvr"
-			fsleyes render -of sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr --size 1400 500 --scene lightbox --sliceSpacing 18 --zrange 21 131 \
+			fsleyes render -of ${wdir}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr --size 1400 500 --scene lightbox --sliceSpacing 18 --zrange 21 131 \
 			--ncols 6 --nrows 1 --hideCursor --bgColour 0.0 0.0 0.0 --fgColour 1.0 1.0 1.0 --showColourBar --colourBarLocation top \
 			--colourBarLabelSide top-left --colourBarSize 50 --labelSize 11 \
-			--performance 3 /media/nemo/ANVILData/gdrive/PJMASK/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr.nii.gz \
+			--performance 3 ${wdr}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr.nii.gz \
 			--name "cvr original" --overlayType volume --alpha 100.0 --cmap red-yellow --negativeCmap blue-lightblue --useNegativeCmap \
 			--displayRange 0.0 0.6 --clippingRange 0.0 10.0 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 \
 			--smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
-			fsleyes render -of sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_masked --size 1400 500 --scene lightbox --sliceSpacing 18 --zrange 21 131 \
+			fsleyes render -of ${wdir}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_masked --size 1400 500 --scene lightbox --sliceSpacing 18 --zrange 21 131 \
 			--ncols 6 --nrows 1 --hideCursor --bgColour 0.0 0.0 0.0 --fgColour 1.0 1.0 1.0 --showColourBar --colourBarLocation top \
 			--colourBarLabelSide top-left --colourBarSize 50 --labelSize 11 \
-			--performance 3 /media/nemo/ANVILData/gdrive/PJMASK/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_masked.nii.gz \
+			--performance 3 ${wdr}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_masked.nii.gz \
 			--name "cvr masked" --overlayType volume --alpha 100.0 --cmap red-yellow --negativeCmap blue-lightblue --useNegativeCmap \
 			--displayRange 0.0 0.6 --clippingRange 0.0 10.0 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 \
 			--smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
-			fsleyes render -of sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_corrected --size 1400 500 --scene lightbox --sliceSpacing 18 --zrange 21 131 \
+			fsleyes render -of ${wdir}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_corrected --size 1400 500 --scene lightbox --sliceSpacing 18 --zrange 21 131 \
 			--ncols 6 --nrows 1 --hideCursor --bgColour 0.0 0.0 0.0 --fgColour 1.0 1.0 1.0 --showColourBar --colourBarLocation top \
 			--colourBarLabelSide top-left --colourBarSize 50 --labelSize 11 \
-			--performance 3 /media/nemo/ANVILData/gdrive/PJMASK/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_corrected.nii.gz \
+			--performance 3 ${wdr}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_corrected.nii.gz \
 			--name "cvr corrected" --overlayType volume --alpha 100.0 --cmap red-yellow --negativeCmap blue-lightblue --useNegativeCmap \
 			--displayRange 0.0 0.6 --clippingRange 0.0 10.0 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 \
 			--smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
-			fsleyes render -of sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_tmap --size 1400 500 --scene lightbox --sliceSpacing 18 --zrange 21 131 \
+			fsleyes render -of ${wdir}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_tmap --size 1400 500 --scene lightbox --sliceSpacing 18 --zrange 21 131 \
 			--ncols 6 --nrows 1 --hideCursor --bgColour 0.0 0.0 0.0 --fgColour 1.0 1.0 1.0 --showColourBar --colourBarLocation top \
 			--colourBarLabelSide top-left --colourBarSize 50 --labelSize 11 \
-			--performance 3 /media/nemo/ANVILData/gdrive/PJMASK/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_tmap.nii.gz \
+			--performance 3 ${wdr}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_tmap.nii.gz \
 			--name "tmap" --overlayType volume --alpha 100.0 --cmap red-yellow --negativeCmap blue-lightblue --useNegativeCmap \
 			--displayRange ${tval} 50.0 --clippingRange ${tval} 100.0 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 \
 			--smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
 			echo "lag"
-			fsleyes render -of sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_lag --size 1400 500 --scene lightbox --sliceSpacing 18 --zrange 21 131 \
+			fsleyes render -of ${wdir}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_lag --size 1400 500 --scene lightbox --sliceSpacing 18 --zrange 21 131 \
 			--ncols 6 --nrows 1 --hideCursor --bgColour 0.0 0.0 0.0 --fgColour 1.0 1.0 1.0 --showColourBar --colourBarLocation top \
 			--colourBarLabelSide top-left --colourBarSize 50 --labelSize 11 \
-			--performance 3 /media/nemo/ANVILData/gdrive/PJMASK/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_lag.nii.gz \
+			--performance 3 ${wdr}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_lag.nii.gz \
 			--name "cvr lag original" --overlayType volume --alpha 100.0 --cmap brain_colours_actc_iso --invert \
 			--displayRange -9 9 --clippingRange -9 9 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 \
 			--smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
-			fsleyes render -of sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_lag_corrected --size 1400 500 --scene lightbox --sliceSpacing 18 --zrange 21 131 \
+			fsleyes render -of ${wdir}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_lag_corrected --size 1400 500 --scene lightbox --sliceSpacing 18 --zrange 21 131 \
 			--ncols 6 --nrows 1 --hideCursor --bgColour 0.0 0.0 0.0 --fgColour 1.0 1.0 1.0 --showColourBar --colourBarLocation top \
 			--colourBarLabelSide top-left --colourBarSize 50 --labelSize 11 \
-			--performance 3 /media/nemo/ANVILData/gdrive/PJMASK/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_lag_corrected.nii.gz \
+			--performance 3 ${wdr}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_lag_corrected.nii.gz \
 			--name "cvr lag corrected" --overlayType volume --alpha 100.0 --cmap brain_colours_actc_iso --invert \
 			--displayRange -9 9 --clippingRange -9 9 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 \
 			--smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0

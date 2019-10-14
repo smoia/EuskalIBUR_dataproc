02120e9a5a2b04229edfbacc671438fc60fc5c45
smoia <s.moia@bcbl.eu>
2019-10-14 10:16:31 +0200

    Make ICC tables and longitudinal graphs

---
+++ compute_CVR_ICCs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import ICC
+import pandas as pd
+import numpy as np
+
+ftype = ['echo-2', 'optcom', 'meica', 'vessels', 'networks']
+val = ['cvrvals', 'lagvals']
+
+cwd = os.getcwd()
+
+os.chdir('/home/nemo/Documenti/Archive/Data/gdrive/PJMASK/CVR/00.Reliability')
+# os.chdir('/data/CVR/00.Reliability')
+
+for j in val:
+    iccs = np.empty((4, 5))
+    for i in range(5):
+        print(f'\n\n {ftype[i]} {j}: ')
+        iccs[0, i], iccs[1:, i] = ICC.compute_ICC_1(f'{ftype[i]}_{j}')
+
+    df = pd.DataFrame(iccs, columns=ftype)
+    df.to_csv(f'ICC_table_{j}')
+
+
+os.chdir(cwd)

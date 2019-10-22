a50118b6ca4a71ed8e8ab9e122ad68aa68439a94
smoia <s.moia@bcbl.eu>
2019-10-22 11:32:05 +0200

    Took away networks

--- 30.reg_manual_meica.sh
+++ 30.reg_manual_meica.sh
@@ -18,7 +18,7 @@ fdir=sub-${sub}/ses-${ses}/func_preproc
 func=${fdir}/00.sub-${sub}_ses-${ses}_task-breathhold_optcom_bold_native_preprocessed
 bold=sub-${sub}_ses-${ses}_task-breathhold_meica_bold
 bves=sub-${sub}_ses-${ses}_task-breathhold_vessels_bold
-bnet=sub-${sub}_ses-${ses}_task-breathhold_networks_bold
+# bnet=sub-${sub}_ses-${ses}_task-breathhold_networks_bold
 
 fsl_regfilt -i ${func} \
 -d ${fdir}/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
@@ -28,21 +28,21 @@ fsl_regfilt -i ${func} \
 -d ${fdir}/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
 -f "$( cat decomp/sub-${sub}_ses-${ses}_vessels.1D )" \
 -o ${fdir}/${bves}_bet
-fsl_regfilt -i ${func} \
--d ${fdir}/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
--f "$( cat decomp/sub-${sub}_ses-${ses}_networks.1D )" \
--o ${fdir}/${bnet}_bet
+# fsl_regfilt -i ${func} \
+# -d ${fdir}/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
+# -f "$( cat decomp/sub-${sub}_ses-${ses}_networks.1D )" \
+# -o ${fdir}/${bnet}_bet
 
 ${cwd}/11.func_spc.sh ${bold}_bet ${fdir}
 ${cwd}/11.func_spc.sh ${bves}_bet ${fdir}
-${cwd}/11.func_spc.sh ${bnet}_bet ${fdir}
+# ${cwd}/11.func_spc.sh ${bnet}_bet ${fdir}
 
 immv ${fdir}/${bold}_bet ${fdir}/00.${bold}_native_preprocessed
 immv ${fdir}/${bold}_bet_SPC ${fdir}/01.${bold}_native_SPC_preprocessed
 immv ${fdir}/${bves}_bet ${fdir}/00.${bves}_native_preprocessed
 immv ${fdir}/${bves}_bet_SPC ${fdir}/01.${bves}_native_SPC_preprocessed
-immv ${fdir}/${bnet}_bet ${fdir}/00.${bnet}_native_preprocessed
-immv ${fdir}/${bnet}_bet_SPC ${fdir}/01.${bnet}_native_SPC_preprocessed
+# immv ${fdir}/${bnet}_bet ${fdir}/00.${bnet}_native_preprocessed
+# immv ${fdir}/${bnet}_bet_SPC ${fdir}/01.${bnet}_native_SPC_preprocessed
 
 
 for e in $( seq 1 ${nTE} )

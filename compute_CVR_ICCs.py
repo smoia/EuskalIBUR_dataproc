c9e11154a8768d7a103c363610c0c02adec5230c
smoia <s.moia@bcbl.eu>
2019-10-23 17:27:26 +0200

    Added ICC plotting

--- compute_CVR_ICCs.py
+++ compute_CVR_ICCs.py
@@ -4,9 +4,17 @@ import os
 import ICC
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 FTYPE_LIST = ['echo-2', 'optcom', 'meica', 'vessels']  #, 'networks']
 VALUE_LIST = ['cvrvals', 'lagvals']
+SUB_LIST = ['002', '003', '007']
+COLOURS = ['#1f77b4ff', '#ff7f0eff', '#2ca02cff', '#d62728ff', '#ac45a8ff']
+
+SET_DPI = 100
+FIGSIZE = (18, 10)
+
 
 cwd = os.getcwd()
 
@@ -15,7 +23,7 @@ os.chdir('/bcbl/home/public/PJMASK_2/preproc/CVR/00.Reliability')
 # os.chdir('/data/CVR/00.Reliability')
 
 for val in VALUE_LIST:
-    iccs = np.empty((4, 5))
+    iccs = np.empty((4, 4))
     for i in range(len(FTYPE_LIST)):
         print(f'\n\n {FTYPE_LIST[i]} {val}: ')
         fname = f'{FTYPE_LIST}_{val}_alltypes_mask'
@@ -23,5 +31,20 @@ for val in VALUE_LIST:
 
     df = pd.DataFrame(iccs, columns=FTYPE_LIST)
     df.to_csv(f'ICC_table_{val}')
+
+    df_tidy = pd.melt(df[1:], var_name='type', value_name='ICC')
+    df_tidy['sub'] = np.tile(np.array(range(len(SUB_LIST))), 4)
+
+    plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
+    sns.lineplot(x="ses", y="cvr", hue="vox",
+                 data=df_tidy,
+                 palette=COLOURS, alpha=.3).legend_.remove()
+    sns.scatterplot(x="ses", y="cvr", hue="vox",
+                    data=df_tidy,
+                    palette=COLOURS, marker='.', edgecolor=None).legend_.remove()
+    plt.title(f'Spatial ICC, {val}')
+    plt.savefig(f'Spatial_ICC_plot_{val}.png', dpi=SET_DPI)
+    plt.clf()
+    plt.close()
 
 os.chdir(cwd)

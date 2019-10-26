4a85a5144916b367b1343eeaf241ce0e3934ad5f
smoia <s.moia@bcbl.eu>
2019-10-26 14:21:30 +0200

    Extended generalisation

--- compute_CVR_ICCs.py
+++ compute_CVR_ICCs.py
@@ -22,18 +22,21 @@ cwd = os.getcwd()
 # os.chdir('/home/nemo/Documenti/Archive/Data/gdrive/PJMASK/CVR/00.Reliability')
 os.chdir('/data/CVR/00.Reliability')
 
+n_ftypes = len(FTYPE_LIST)
+n_subs = len(SUB_LIST)
+
 for val in VALUE_LIST:
-    iccs = np.empty((4, 4))
-    for i in range(len(FTYPE_LIST)):
+    iccs = np.empty((n_subs+1, n_ftypes))
+    for i in range(n_ftypes):
         print(f'\n\n {FTYPE_LIST[i]} {val}: ')
-        fname = f'{FTYPE_LIST}_{val}_alltypes_mask'
+        fname = f'{FTYPE_LIST[i]}_{val}_alltypes_mask'
         iccs[0, i], iccs[1:, i] = ICC.compute_spatial_ICC_1(fname)
 
     df = pd.DataFrame(iccs, columns=FTYPE_LIST)
     df.to_csv(f'ICC_table_{val}_alltypes_mask')
 
     df_tidy = pd.melt(df[1:], var_name='type', value_name='ICC')
-    df_tidy['sub'] = np.tile(np.array(range(len(SUB_LIST))), 4)
+    df_tidy['sub'] = np.tile(np.array(range(n_subs)), 4)
 
     plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
     sns.pointplot(x='type', y='ICC', hue='sub', data=df_tidy,
@@ -44,17 +47,17 @@ for val in VALUE_LIST:
     plt.close()
 
 for val in VALUE_LIST:
-    iccs = np.empty((4, 4))
-    for i in range(len(FTYPE_LIST)):
+    iccs = np.empty((n_subs+1, n_ftypes))
+    for i in range(n_ftypes):
         print(f'\n\n {FTYPE_LIST[i]} {val}: ')
-        fname = f'{FTYPE_LIST}_{val}_GM_mask'
+        fname = f'{FTYPE_LIST[i]}_{val}_GM_mask'
         iccs[0, i], iccs[1:, i] = ICC.compute_spatial_ICC_1(fname)
 
     df = pd.DataFrame(iccs, columns=FTYPE_LIST)
     df.to_csv(f'ICC_table_{val}_GM_mask')
 
     df_tidy = pd.melt(df[1:], var_name='type', value_name='ICC')
-    df_tidy['sub'] = np.tile(np.array(range(len(SUB_LIST))), 4)
+    df_tidy['sub'] = np.tile(np.array(range(n_subs)), 4)
 
     plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
     sns.pointplot(x='type', y='ICC', hue='sub', data=df_tidy,
@@ -65,8 +68,8 @@ for val in VALUE_LIST:
     plt.close()
 
 for val in VALUE_LIST:
-    iccs = np.empty((4, 4))
-    for i in range(len(FTYPE_LIST)):
+    iccs = np.empty((n_subs+1, n_ftypes))
+    for i in range(n_ftypes):
         print(f'\n\n {FTYPE_LIST[i]} {val}: ')
         fname = f'{FTYPE_LIST}_{val}'
         iccs[0, i], iccs[1:, i] = ICC.compute_spatial_ICC_1(fname)
@@ -75,7 +78,7 @@ for val in VALUE_LIST:
     df.to_csv(f'ICC_table_{val}')
 
     df_tidy = pd.melt(df[1:], var_name='type', value_name='ICC')
-    df_tidy['sub'] = np.tile(np.array(range(len(SUB_LIST))), 4)
+    df_tidy['sub'] = np.tile(np.array(range(n_subs)), 4)
 
     plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
     sns.pointplot(x='type', y='ICC', hue='sub', data=df_tidy,

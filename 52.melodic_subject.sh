53164056af4348ba790faa3822c9bad914ffef43
smoia <s.moia@bcbl.eu>
2019-10-26 17:10:29 +0200

    Corrected name and position of files

--- 52.melodic_subject.sh
+++ 52.melodic_subject.sh
@@ -26,17 +26,17 @@ then
 fi
 
 echo "Copying session 01"
-imcp sub-${sub}/ses-01/sub-${sub}_ses-01_task-rest_run-01_optcom_bold_native_preprocessed DMN/sub-${sub}/ses-01_rest_run-01_${ftype}
+imcp sub-${sub}/ses-01/func_preproc/00.sub-${sub}_ses-01_task-rest_run-01_optcom_bold_native_preprocessed DMN/sub-${sub}/ses-01_rest_run-01_${ftype}
 
 lastses=9
 
-reffile=sub-${sub}/ses-01/func_preproc/sub-${sub}_ses-01_task-breathhold_rec-magnitude_echo-1_sbref_cr_brain
+reffile=sub-${sub}/ses-01/func_preproc/00.sub-${sub}_ses-01_task-breathhold_rec-magnitude_echo-1_sbref_cr_brain
 
 imcp ${wdr}/${reffile}_mask DMN/sub-${sub}/mask_${ftype}
 
 for ses in $( seq -f %02g 2 ${lastses} )
 do
-	infile=sub-${sub}/ses-${ses}/func_preproc/sub-${sub}_ses-${ses}_task-rest_run-01_optcom_bold_native_preprocessed
+	infile=sub-${sub}/ses-${ses}/func_preproc/00.sub-${sub}_ses-${ses}_task-rest_run-01_optcom_bold_native_preprocessed
 	matfile=sub-${sub}/ses-${ses}/reg/sub-${sub}_ses-${ses}_task-breathhold_rec-magnitude_echo-1_sbref_cr_brain2ses-01
 
 	echo "Flirting session ${ses} to session 01"

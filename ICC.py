Corrected axis of ICC computation

ICC.py
@@ -8,16 +8,24 @@ SET_DPI = 100
 FIGSIZE = (18, 10)
 
 
+def compute_MS(table):
+    """
+    Computes MSs necessary for ICC
+    """
+    n, k = table.shape
+    SS_tot = table.var(ddof=1) * (n * k - 1)
+    MS_r = np.var(table.mean(axis=1), ddof=1) * k
+    MS_w = np.sum(np.var(table, axis=1, ddof=1)) / n
+    MS_c = np.var(np.mean(table, axis=0), ddof=1) * n
+    MS_e = (SS_tot - MS_r * (n - 1) - MS_c * (k - 1)) / ((n - 1) * (k - 1))
+    return n, k, MS_r, MS_w, MS_c, MS_e
+
 def compute_ICC_1(table):
     """
     Computes standard ICC(1,1)
     """
-    n_vxs, n_sess = table.shape
-
-    MS_r = np.var(table.mean(0), ddof=1) * n_sess
-    MS_w = np.sum(table.var(0, ddof=1)) / n_vxs
-
-    ICC_1 = (MS_r - MS_w) / (MS_r + (n_sess - 1) * MS_w)
+    n, k, MS_r, MS_w, MS_c, MS_e = compute_MS(table)
+    ICC_1 = (MS_r - MS_w) / (MS_r + (k - 1) * MS_w)
 
     return ICC_1
 
@@ -26,14 +34,8 @@ def compute_ICC_C_1(table):
     """
     Computes standard ICC(2,1)
     """
-    n_vxs, n_sess = table.shape
-
-    MS_r = np.var(table.mean(0), ddof=1) * n_sess
-    MS_e = (table.var(ddof=1)*(n_vxs*n_sess - 1) - MS_r*(n_vxs-1)
-            - np.var(table.mean(0), ddof=1)*n_vxs*(n_sess-1)) / (
-            (n_vxs-1)*(n_sess-1));
-
-    ICC_C_1 = (MS_r - MS_e) / (MS_r + (n_sess - 1) * MS_e)
+    n, k, MS_r, MS_w, MS_c, MS_e = compute_MS(table)
+    ICC_C_1 = (MS_r - MS_e) / (MS_r + (k - 1) * MS_e)
 
     return ICC_C_1
 
@@ -41,19 +43,13 @@ def compute_ICC_A_1(table):
     """
     Computes standard ICC(3,1)
     """
-    n_vxs, n_sess = table.shape
-
-    MS_r = np.var(table.mean(0), ddof=1) * n_sess
-    MS_e = (table.var(ddof=1)*(n_vxs*n_sess - 1) - MS_r*(n_vxs-1)
-            - np.var(table.mean(0), ddof=1)*n_vxs*(n_sess-1)) / (
-            (n_vxs-1)*(n_sess-1));
-
-    ICC_A_1 = (MS_r - MS_e) / (MS_r + (n_sess - 1) * MS_e
-               + n_sess*(np.var(table.mean(0), ddof=1)*n_vxs - MS_e)/n)
+    n, k, MS_r, MS_w, MS_c, MS_e = compute_MS(table)
+    ICC_A_1 = (MS_r - MS_e) / ((MS_r + (k - 1) * MS_e
+                               + k * (MS_c - MS_e) - MS_e) / n)
 
     return ICC_A_1
 
-def compute_spatial_ICC_1(val, sub_list=['002', '003', '007'], n_sess=9, spatial_ICC=True):
+def compute_spatial_ICC_1(val, sub_list=['002', '003', '007'], k=9, spatial_ICC=True):
     """
     Computes standard ICC(1,1) for the average of a table,
     and a spatial ICC(1,1) for each subject
@@ -65,7 +61,7 @@ def compute_spatial_ICC_1(val, sub_list=['002', '003', '007'], n_sess=9, spatial
                                     delimiter=',', skip_header=1))
 
     n_subs = len(sub_list)
-    meanvals = np.zeros((n_subs, n_sess))
+    meanvals = np.zeros((n_subs, k))
     if spatial_ICC:
         s_ICC = []
     else:
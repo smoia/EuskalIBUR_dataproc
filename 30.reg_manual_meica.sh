7dc3c4e230fe2451244b826baf58e206ff27d40a
smoia <s.moia@bcbl.eu>
2019-10-30 15:13:21 +0100

    Switched to 3dTproject and ortogonalised components

--- 30.reg_manual_meica.sh
+++ 30.reg_manual_meica.sh
@@ -12,24 +12,66 @@ cwd=$( pwd )
 
 cd ${wdr} || exit
 
+if [[ ! -d "decomp" ]]
+then
+	cd ${cwd}
+	./20.sheet_preproc.sh
+	cd ${wdr}
+fi
+
 echo "Denoising sub ${sub} ses ${ses} optcom"
 fdir=sub-${sub}/ses-${ses}/func_preproc
-
-func=${fdir}/00.sub-${sub}_ses-${ses}_task-breathhold_optcom_bold_native_preprocessed
-bold=sub-${sub}_ses-${ses}_task-breathhold_meica_bold
-bves=sub-${sub}_ses-${ses}_task-breathhold_vessels_bold
+prfx=sub-${sub}_ses-${ses}
+meica_mix=${wdr}/${fdir}/${prfx}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D
+func=${fdir}/00.${prfx}_task-breathhold_optcom_bold_native_preprocessed
+bold=${prfx}_task-breathhold_meica_bold
+bves=${prfx}_task-breathhold_vessels_bold
 # bnet=sub-${sub}_ses-${ses}_task-breathhold_networks_bold
 
-fsl_regfilt -i ${func} \
--d ${fdir}/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
--f "$( cat decomp/sub-${sub}_ses-${ses}_rejected.1D )" \
--o ${fdir}/${bold}_bet
-fsl_regfilt -i ${func} \
--d ${fdir}/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
--f "$( cat decomp/sub-${sub}_ses-${ses}_vessels.1D )" \
--o ${fdir}/${bves}_bet
+cd decomp
+
+# Removing component by ortogonalisation
+acc=$( cat ${prfx}_accepted.1D )
+rej=$( cat ${prfx}_rejected.1D )
+ves=$( cat ${prfx}_vessels.1D )
+net=$( cat ${prfx}_networks.1D )
+
+1dcat "$meica_mix[$acc$net$ves]" > tmp.${prfx}_meica_good.1D
+1dcat "$meica_mix[$rej]" > tmp.${prfx}_rej.tr.1D
+1dtranspose tmp.${prfx}_rej.tr.1D > tmp.${prfx}_meica_rej.1D
+
+3dTproject -ort tmp.${prfx}_meica_good.1D -polort -1 -prefix tmp.${prfx}_tr.1D -input tmp.${prfx}_meica_rej.1D -overwrite
+1dtranspose tmp.${prfx}_tr.1D > ${prfx}_rejected_ort.1D
+
+1dcat "$meica_mix[$acc$net]" > tmp.${prfx}_meica_good.1D
+1dcat "$meica_mix[$rej$ves]" > tmp.${prfx}_rej.tr.1D
+1dtranspose tmp.${prfx}_rej.tr.1D > tmp.${prfx}_meica_rej.1D
+
+3dTproject -ort tmp.${prfx}_meica_good.1D -polort -1 -prefix tmp.${prfx}_tr.1D -input tmp.${prfx}_meica_rej.1D -overwrite
+1dtranspose tmp.${prfx}_tr.1D > ${prfx}_vessels_ort.1D
+
+rm tmp.${prfx}*
+
+cd ..
+
+3dTproject -input ${func}.nii.gz \
+-ort decomp/${prfx}_rejected_ort.1D \
+-polort -1 -prefix ${fdir}/${bold}_bet.nii.gz
+3dTproject -input ${func}.nii.gz \
+-ort decomp/${prfx}_vessels_ort.1D \
+-polort -1 -prefix ${fdir}/${bves}_bet.nii.gz
+
+# Uncomment here for regfilt
 # fsl_regfilt -i ${func} \
-# -d ${fdir}/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
+# -d ${meica_mix} \
+# -f "$( cat decomp/sub-${sub}_ses-${ses}_rejected.1D )" \
+# -o ${fdir}/${bold}_bet
+# fsl_regfilt -i ${func} \
+# -d ${meica_mix} \
+# -f "$( cat decomp/sub-${sub}_ses-${ses}_vessels.1D )" \
+# -o ${fdir}/${bves}_bet
+# fsl_regfilt -i ${func} \
+# -d ${meica_mix} \
 # -f "$( cat decomp/sub-${sub}_ses-${ses}_networks.1D )" \
 # -o ${fdir}/${bnet}_bet
 
@@ -45,29 +87,34 @@ immv ${fdir}/${bves}_bet_SPC ${fdir}/01.${bves}_native_SPC_preprocessed
 # immv ${fdir}/${bnet}_bet_SPC ${fdir}/01.${bnet}_native_SPC_preprocessed
 
 
-for e in 2  # $( seq 1 ${nTE} )
-do
-	echo "Denoising sub ${sub} ses ${ses} echo ${e}"
-	func=${fdir}/00.sub-${sub}_ses-${ses}_task-breathhold_echo-${e}_bold_native_preprocessed
-	bold=sub-${sub}_ses-${ses}_task-breathhold_meica_echo-${e}_bold
+# for e in 2  # $( seq 1 ${nTE} )
+# do
+# 	echo "Denoising sub ${sub} ses ${ses} echo ${e}"
+# 	func=${fdir}/00.sub-${sub}_ses-${ses}_task-breathhold_echo-${e}_bold_native_preprocessed
+# 	bold=sub-${sub}_ses-${ses}_task-breathhold_meica_echo-${e}_bold
 
-	fsl_regfilt -i ${func} \
-	-d ${fdir}/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
-	-f "$( cat decomp/sub-${sub}_ses-${ses}_rejected.1D )" \
-	-o ${fdir}/${bold}_bet
-	# fsl_regfilt -i ${func} \
-	# -d ${fdir}/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
-	# -f "$( cat decomp/sub-${sub}_ses-${ses}_vascular.1D )" \
-	# -o ${fdir}/${bold}_bet
-	# fsl_regfilt -i ${func} \
-	# -d ${fdir}/sub-${sub}_ses-${ses}_task-breathhold_echo-1_bold_RPI_bet_meica/meica_mix.1D \
-	# -f "$( cat decomp/sub-${sub}_ses-${ses}_networks.1D )" \
-	# -o ${fdir}/${bold}_bet
+# 	3dTproject -input ${func}.nii.gz \
+# 	-ort decomp/${prfx}_rejected_ort.1D \
+# 	-polort -1 -prefix ${fdir}/${bold}_bet.nii.gz
 
-	${cwd}/11.func_spc.sh ${bold}_bet ${fdir}
+# 	# Uncomment here for regfilt
+# 	# fsl_regfilt -i ${func} \
+# 	# -d ${meica_mix} \
+# 	# -f "$( cat decomp/sub-${sub}_ses-${ses}_rejected.1D )" \
+# 	# -o ${fdir}/${bold}_bet
+# 	# fsl_regfilt -i ${func} \
+# 	# -d ${meica_mix} \
+# 	# -f "$( cat decomp/sub-${sub}_ses-${ses}_vascular.1D )" \
+# 	# -o ${fdir}/${bold}_bet
+# 	# fsl_regfilt -i ${func} \
+# 	# -d ${meica_mix} \
+# 	# -f "$( cat decomp/sub-${sub}_ses-${ses}_networks.1D )" \
+# 	# -o ${fdir}/${bold}_bet
 
-	immv ${fdir}/${bold}_bet ${fdir}/04.${bold}_native_preprocessed
-	immv ${fdir}/${bold}_bet_SPC ${fdir}/05.${bold}_native_SPC_preprocessed
-done
+# 	${cwd}/11.func_spc.sh ${bold}_bet ${fdir}
+
+# 	immv ${fdir}/${bold}_bet ${fdir}/04.${bold}_native_preprocessed
+# 	immv ${fdir}/${bold}_bet_SPC ${fdir}/05.${bold}_native_SPC_preprocessed
+# done
 
 cd ${cwd}

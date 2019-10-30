49dc3bf212e781634b04d5302f47f95e2586c63e
smoia <s.moia@bcbl.eu>
2019-10-30 14:19:40 +0100

    Changed component retrieval

--- sheet_preproc.py
+++ sheet_preproc.py
@@ -20,23 +20,20 @@ os.chdir('decomp')
 
 for sub in sub_list:
     sub_table = pd.read_excel(xl, sub)
-    sub_table.index += 1
+    # Uncomment this for fsl_regfilt
+    # sub_table.index += 1
 
     for ses in range(1, 10):
         col = f'ses-{ses:02d}'
         net = sub_table.index[sub_table[col] == 'N'].tolist()
         vas = sub_table.index[sub_table[col] == 'V'].tolist()
-        acc = net + vas + sub_table.index[sub_table[col] == 'A'].tolist()
-        acc = [x - 1 for x in acc]
+        acc = sub_table.index[sub_table[col] == 'A'].tolist()
         rej = sub_table.index[sub_table[col] == 'R'].tolist()
-        rej = [x - 1 for x in rej]
-        vas = rej + sub_table.index[sub_table[col] == 'V'].tolist()
-        # net = vas + sub_table.index[sub_table[col] == 'N'].tolist()
 
         px = f'sub-{sub}_ses-{ses:02d}'
         savetxt(f'{px}_accepted.1D', acc, fmt='%d', delimiter=',', newline=',')
         savetxt(f'{px}_rejected.1D', rej, fmt='%d', delimiter=',', newline=',')
         savetxt(f'{px}_vessels.1D', vas, fmt='%d', delimiter=',', newline=',')
-        # savetxt(f'{px}_networks.1D', net, fmt='%d', delimiter=',', newline=',')
+        savetxt(f'{px}_networks.1D', net, fmt='%d', delimiter=',', newline=',')
 
 os.chdir(cwd)

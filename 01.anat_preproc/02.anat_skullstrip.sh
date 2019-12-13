9ac4f4b38ba9da68ed4dbe702a598831414e6adc
smoia <s.moia@bcbl.eu>
2019-12-13 02:16:54 +0100

    padded and added transformation

--- 01.anat_preproc/02.anat_skullstrip.sh
+++ 01.anat_preproc/02.anat_skullstrip.sh
@@ -34,8 +34,8 @@ then
 	# If no mask is specified, then creates it.
 	echo "Skull Stripping ${anat}"
 	3dSkullStrip -input ${anat_in}.nii.gz \
-	-prefix ${anat}_brain.nii.gz \
-	-orig_vol -overwrite
+				 -prefix ${anat}_brain.nii.gz \
+				 -orig_vol -overwrite
 	fslmaths ${anat}_brain -bin ${anat}_brain_mask
 	mask=${anat}_brain_mask
 else
@@ -55,8 +55,8 @@ then
 	# If a reference is specified, coreg the mask to the reference
 	echo "Flirting ${mask} into ${aref}"
 	flirt -in ${mask} -ref ${aref} -cost normmi -searchcost normmi \
-	-init ../reg/${anat}2${aref}_fsl.mat -o ${aref}_brain_mask \
-	-applyxfm -interp nearestneighbour
+		  -init ../reg/${anat}2${aref}_fsl.mat -o ${aref}_brain_mask \
+		  -applyxfm -interp nearestneighbour
 fi
 	
 if [[ "${c3dsrc}" != "none" ]]
@@ -64,8 +64,15 @@ then
 	# If a source for c3d is specified,
 	# translate fsl transformation into ants with the right images.
 	echo "Moving from FSL to ants in brain extracted images"
-	c3d_affine_tool -ref ${anat}_brain -src ${c3dsrc}_brain \
-	../reg/${c3dsrc}2${anat}_fsl.mat -fsl2ras -oitk ../reg/${c3dsrc}2${anat}0GenericAffine.mat
+	c3d_affine_tool -ref ${anat}_brain -src ${c3dsrc}_brain ../reg/${c3dsrc}2${anat}_fsl.mat \
+				    -fsl2ras -oitk ../reg/${c3dsrc}2${anat}0GenericAffine.mat
+	# Also transform both skullstripped and not!
+	antsApplyTransforms -d 3 -i ${c3dsrc}_brain.nii.gz \
+						-r ${anat}_brain.nii.gz -o ../reg/${c3dsrc}_brain2${anat}_brain.nii.gz \
+						-n Linear -t ../reg/${c3dsrc}2${anat}0GenericAffine.mat/${c3dsrc}2${anat}0GenericAffine.mat
+	antsApplyTransforms -d 3 -i ${c3dsrc}.nii.gz \
+						-r ${anat}.nii.gz -o ../reg/${c3dsrc}2${anat}.nii.gz \
+						-n Linear -t ../reg/${c3dsrc}2${anat}0GenericAffine.mat
 fi
 
 
--- 01.anat_preproc/04.anat_normalize.sh
+++ 01.anat_preproc/04.anat_normalize.sh
@@ -36,25 +36,25 @@ fi
 
 echo "Normalizing ${anat} to ${std}"
 antsRegistration -d 3 -r [../reg/${std}.nii.gz,${anat_in}.nii.gz,1] \
--o [../reg/${anat}2std,../reg/${anat}2std.nii.gz,../reg/std2${anat}.nii.gz] \
--x [../reg/${std}_mask.nii.gz, ${anat_in}_mask.nii.gz] \
--n Linear -u 0 -w [0.005,0.995] \
--t Rigid[0.1] \
--m MI[../reg/${std}.nii.gz,${anat_in}.nii.gz,1,48,Regular,0.1] \
--c [1000x500x250x100,1e-6,10] \
--f 8x4x2x1 \
--s 3x2x1x0vox \
--t Affine[0.1] \
--m MI[../reg/${std}.nii.gz,${anat_in}.nii.gz,1,48,Regular,0.1] \
--c [1000x500x250x100,1e-6,10] \
--f 8x4x2x1 \
--s 3x2x1x0vox \
--t SyN[0.1,3,0] \
--m CC[../reg/${std}.nii.gz,${anat_in}.nii.gz,1,5] \
--c [100x70x50x20,1e-6,10] \
--f 8x4x2x1 \
--s 3x2x1x0vox \
--z 1 -v 1
+				 -o [../reg/${anat}2std,../reg/${anat}2std.nii.gz,../reg/std2${anat}.nii.gz] \
+				 -x [../reg/${std}_mask.nii.gz, ${anat_in}_mask.nii.gz] \
+				 -n Linear -u 0 -w [0.005,0.995] \
+				 -t Rigid[0.1] \
+				 -m MI[../reg/${std}.nii.gz,${anat_in}.nii.gz,1,48,Regular,0.1] \
+				 -c [1000x500x250x100,1e-6,10] \
+				 -f 8x4x2x1 \
+				 -s 3x2x1x0vox \
+				 -t Affine[0.1] \
+				 -m MI[../reg/${std}.nii.gz,${anat_in}.nii.gz,1,48,Regular,0.1] \
+				 -c [1000x500x250x100,1e-6,10] \
+				 -f 8x4x2x1 \
+				 -s 3x2x1x0vox \
+				 -t SyN[0.1,3,0] \
+				 -m CC[../reg/${std}.nii.gz,${anat_in}.nii.gz,1,5] \
+				 -c [100x70x50x20,1e-6,10] \
+				 -f 8x4x2x1 \
+				 -s 3x2x1x0vox \
+				 -z 1 -v 1
 
 ## 02. Registration to downsampled MNI
 cd ../reg || exit
@@ -67,10 +67,8 @@ fi
 
 echo "Registering ${anat} to resampled standard"
 antsApplyTransforms -d 3 -i ${adir}/${anat_in}.nii.gz \
--r ${std}_resamp_${mmres}mm.nii.gz -o ${anat}2std_resamp_${mmres}mm.nii.gz \
--n Linear -t ${anat}2std0GenericAffine.mat -t ${anat}2std1Warp.nii.gz
-# WarpImageMultiTransform 3 ${adir}/${anat_in}.nii.gz \
-# ${anat}2std_resamp_${mmres}mm.nii.gz -R ${std}_resamp_${mmres}mm.nii.gz 
+					-r ${std}_resamp_${mmres}mm.nii.gz -o ${anat}2std_resamp_${mmres}mm.nii.gz \
+					-n Linear -t ${anat}2std0GenericAffine.mat -t ${anat}2std1Warp.nii.gz
 
 
 cd ${cwd}

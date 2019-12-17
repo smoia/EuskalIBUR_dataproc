10bdf3ef69274b9a859f6500d6572bd8f59f91d5
smoia <s.moia@bcbl.eu>
2019-12-17 03:00:37 +0100

    Started rethinking general cvr maps

--- 05.second_level_analysis/01.generalcvrmaps.sh
+++ 05.second_level_analysis/01.generalcvrmaps.sh
@@ -9,67 +9,38 @@
 
 sub=$1
 ftype=$2
+lastses=${3:-5}
 wdr=${3:-/data}
 
 ### Main ###
 cwd=$( pwd )
-cd ${wdr} || exit
+cd ${wdr}/CVR || exit
 
 echo "Creating folders"
-if [ ! -d CVR/00.Reliability ]
+if [ ! -d 00.Reliability ]
 then
-	mkdir CVR/00.Reliability
-fi
-if [ ! -d CVR/00.Reliability/sub-${sub} ]
-then
-	mkdir CVR/00.Reliability/sub-${sub}
+	mkdir 00.Reliability
 fi
 
-echo "Copying session 01"
-imcp CVR/sub-${sub}_ses-01_${ftype}_map_cvr/sub-${sub}_ses-01_${ftype}_cvr_idx_mask CVR/00.Reliability/sub-${sub}/sub-${sub}_ses-01_${ftype}_cvr_idx_mask
-imcp CVR/sub-${sub}_ses-01_${ftype}_map_cvr/sub-${sub}_ses-01_${ftype}_cvr CVR/00.Reliability/sub-${sub}/sub-${sub}_ses-01_${ftype}_cvr
-imcp CVR/sub-${sub}_ses-01_${ftype}_map_cvr/sub-${sub}_ses-01_${ftype}_cvr_lag CVR/00.Reliability/sub-${sub}/sub-${sub}_ses-01_${ftype}_cvr_lag
-imcp CVR/sub-${sub}_ses-01_${ftype}_map_cvr/sub-${sub}_ses-01_${ftype}_tmap CVR/00.Reliability/sub-${sub}/sub-${sub}_ses-01_${ftype}_tmap
-
-echo "Initialising common mask"
-imcp CVR/00.Reliability/sub-${sub}/sub-${sub}_ses-01_${ftype}_cvr_idx_mask CVR/00.Reliability/sub-${sub}_${ftype}_allses_mask
-# coreg
-# if [[ ${sub} == "002" ]]
-# then
-# 	lastses=9
-# else
-# 	lastses=10
-# fi
-
-lastses=9
-
-reffile=sub-${sub}/ses-01/func_preproc/sub-${sub}_ses-01_task-breathhold_rec-magnitude_echo-1_sbref_cr_brain
-for ses in $( seq -f %02g 2 ${lastses} )
+for ses in $( seq -f %02g 1 ${lastses} )
 do
-	infile=sub-${sub}/ses-${ses}/func_preproc/sub-${sub}_ses-${ses}_task-breathhold_rec-magnitude_echo-1_sbref_cr_brain
-	outfile=sub-${sub}/ses-${ses}/reg/sub-${sub}_ses-${ses}_task-breathhold_rec-magnitude_echo-1_sbref_cr_brain2ses-01
-
-	echo "Flirting session ${ses} to session 01"
-	flirt -in ${infile} -ref ${reffile} -out ${outfile} -omat ${outfile}.mat -cost normcorr -searchcost normcorr
-	flirt -in CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_idx_mask -ref ${reffile} \
-	-out CVR/00.Reliability/sub-${sub}/sub-${sub}_ses-${ses}_${ftype}_cvr_idx_mask -interp nearestneighbour \
-	-applyxfm -init ${outfile}.mat
-	flirt -in CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr -ref ${reffile} \
-	-out CVR/00.Reliability/sub-${sub}/sub-${sub}_ses-${ses}_${ftype}_cvr -interp nearestneighbour \
-	-applyxfm -init ${outfile}.mat
-	flirt -in CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_lag -ref ${reffile} \
-	-out CVR/00.Reliability/sub-${sub}/sub-${sub}_ses-${ses}_${ftype}_cvr_lag -interp nearestneighbour \
-	-applyxfm -init ${outfile}.mat
-	flirt -in CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_tmap -ref ${reffile} \
-	-out CVR/00.Reliability/sub-${sub}/sub-${sub}_ses-${ses}_${ftype}_tmap -interp nearestneighbour \
-	-applyxfm -init ${outfile}.mat
-
-	echo "Updating common mask"
-	fslmaths CVR/00.Reliability/sub-${sub}_${ftype}_allses_mask -mas CVR/00.Reliability/sub-${sub}/sub-${sub}_ses-${ses}_${ftype}_cvr_idx_mask \
-	CVR/00.Reliability/sub-${sub}_${ftype}_allses_mask
+	echo "Copying session ${ses}"
+	imcp sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr 00.Reliability/sub-${sub}_ses-${ses}_${ftype}_cvr
+	imcp sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_simple 00.Reliability/sub-${sub}_ses-${ses}_${ftype}_cvr_simple
+	imcp sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_lag 00.Reliability/sub-${sub}_ses-${ses}_${ftype}_cvr_lag
+	imcp sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_tmap 00.Reliability/sub-${sub}_ses-${ses}_${ftype}_tmap
 done
 
-cd ${wdr}/CVR/00.Reliability
+echo "Initialising common mask"
+imcp sub-${sub}_ses-01_${ftype}_map_cvr/sub-${sub}_ses-01_${ftype}_cvr_idx_mask 00.Reliability/sub-${sub}_${ftype}_allses_mask
+for ses in $( seq -f %02g 2 ${lastses} )
+do
+	echo "Updating common mask"
+	fslmaths 00.Reliability/sub-${sub}_${ftype}_allses_mask -mas sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_idx_mask \
+	00.Reliability/sub-${sub}_${ftype}_allses_mask
+done
+
+cd 00.Reliability
 echo "Initialising csv files"
 
 for fname in tvals cvrvals lagvals
@@ -86,7 +57,7 @@ do
 	do
 		echo "Extracting voxel informations in session ${ses} for ${fname}"
 		echo "ses-${ses}" > tmp.sub-${sub}_${ses}_${ftype}_${fname}.csv
-		fslmeants -i sub-${sub}/sub-${sub}_ses-${ses}_${ftype}_${fvol} -m sub-${sub}_${ftype}_allses_mask --showall --transpose \
+		fslmeants -i sub-${sub}_ses-${ses}_${ftype}_${fvol} -m sub-${sub}_${ftype}_allses_mask --showall --transpose \
 		| csvtool -t SPACE col 4 - >> tmp.sub-${sub}_${ses}_${ftype}_${fname}.csv
 	done
 

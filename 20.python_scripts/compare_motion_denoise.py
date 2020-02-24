9ac87fcd09a6c96ad7c7b50d30db71a2ff03281a
smoia <s.moia@bcbl.eu>
2020-02-24 23:25:17 +0100

    Changed production of tables for R script

--- 20.python_scripts/compare_motion_denoise.py
+++ 20.python_scripts/compare_motion_denoise.py
@@ -5,13 +5,8 @@ import pandas as pd
 import numpy as np
 
 NTE = 5
-LAST_SES = 10  #10
+LAST_SES = 10  # 10
 SUB_LIST = ['001', '002', '003', '004', '007', '008', '009']
-
-
-def compute_slope(x, y):
-    m = ((x*y).mean() - x.mean()*y.mean()) / ((x**2).mean() - (x.mean())**2)
-    return m
 
 
 cwd = os.getcwd()
@@ -28,10 +23,9 @@ ftype_list = ['echo-2', 'optcom', 'meica-aggr', 'meica-orth', 'meica-cons',
 LAST_SES += 1
 
 sub_table = pd.DataFrame()
-slope_table = pd.DataFrame()
+sub_long_table = pd.DataFrame(columns=['dvars', 'sub', 'ses', 'ftype', 'fd'])
 
 for sub in SUB_LIST:
-    slopes = np.zeros([len(ftype_list) * LAST_SES])
     for ses in range(1, LAST_SES):
         for mot_type in ['dvars_pre', 'fd']:
             col_name = f'{sub}_{ses:02d}_{mot_type}'
@@ -48,15 +42,27 @@ for sub in SUB_LIST:
             delta_dvars = f'{sub}_{ses:02d}_delta_dvars_{ftype}'
             sub_table[delta_dvars] = sub_table[dvars_pre] - sub_table[dvars_type]
 
-            slopes[i*LAST_SES+ses-1] = compute_slope(np.array(sub_table[delta_dvars][1:]),
-                                                     np.array(sub_table[f'{sub}_{ses:02d}_fd'][1:]))
+ftype_list = ['pre', 'echo-2', 'optcom', 'meica-aggr', 'meica-orth',
+              'meica-cons', 'meica-mvar']
 
-    for i, ftype in enumerate(ftype_list):
-        slope_table[f'{sub}_{ftype}'] = slopes[i*LAST_SES:(i*LAST_SES+LAST_SES)]
+for sub in SUB_LIST:
+    for ses in range(1, LAST_SES):
+        fd_col = np.genfromtxt(f'sub-{sub}/fd_sub-{sub}_ses-{ses:02d}.1D')
 
-sub_table.drop(sub_table.index[0])
+        for ftype in ftype_list:
+            tmp_df = pd.DataFrame()
+            tmp_df['dvars'] = np.genfromtxt(f'sub-{sub}/dvars_{ftype}_sub-{sub}_ses-{ses:02d}.1D')
+            tmp_df['sub'] = sub
+            tmp_df['ses'] = f'{ses:02d}'
+            tmp_df['ftype'] = ftype
+            tmp_df['fd'] = fd_col
+            tmp_df = tmp_df.drop(tmp_df.index[0])
+
+            sub_long_table = sub_long_table.append(tmp_df, ignore_index=True)
+
+sub_table = sub_table.drop(sub_table.index[0])
 
 sub_table.to_csv('sub_table.csv')
-slope_table.to_csv('slope_table.csv')
+sub_long_table.to_csv('sub_long_table.csv')
 
 os.chdir(cwd)

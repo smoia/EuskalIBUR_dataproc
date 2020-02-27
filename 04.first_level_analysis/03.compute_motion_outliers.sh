4c89a8be76c1da064de5ebcd5832e6de4edeb3aa
smoia <s.moia@bcbl.eu>
2020-02-27 12:35:30 +0100

    Now extracting DVARS with AFNI

--- 04.first_level_analysis/03.compute_motion_outliers.sh
+++ 04.first_level_analysis/03.compute_motion_outliers.sh
@@ -10,8 +10,6 @@ sub=$1
 ses=$2
 wdr=${3:-/data}
 
-nTE=${4:-5}
-
 ### Main ###
 cwd=$( pwd )
 cd ${wdr} || exit
@@ -23,8 +21,8 @@ cd ME_Denoising
 if [[ ! -d "sub-${sub}" ]]; then mkdir sub-${sub}; fi
 
 flpr=sub-${sub}_ses-${ses}
-mref=sub-${sub}_sbref
 fdir=${wdr}/sub-${sub}/ses-${ses}/func_preproc
+mask=${wdr}/sub-${sub}/ses-01/reg/sub-${sub}_sbref_brain_mask
 
 # # 01. Register GM to MREF if necessary
 # if [[ ! -e sub-${sub}_GM.nii.gz ]]
@@ -40,31 +38,10 @@ fdir=${wdr}/sub-${sub}/ses-${ses}/func_preproc
 # 						-t [${wdr}/sub-${sub}/ses-${ses}/reg/${aref}2${anat}0GenericAffine.mat,1]
 # fi
 
-# # 02. Quick denoise for OC, E2, 4D denoise?
-# echo ${flpr}
+echo "Computing DVARS Pre-motcor and collecting FD"
+3dTto1D -input ${fdir}/${flpr}_task-breathhold_echo-2_bold_cr.nii.gz -mask ${mask}.nii.gz -method dvars -prefix sub-${sub}/dvars_pre_${flpr}.1D
 
-# for den in optcom echo-2
-# do
-# 	func=${fdir}/00.${flpr}_task-breathhold_${den}_bold_native_preprocessed
-# 	3dDeconvolve -input ${func}.nii.gz \
-# 				 -polort 2 -float \
-# 				 -ortvec ${fdir}/${flpr}_task-breathhold_echo-1_bold_mcf_demean.par motdemean \
-# 				 -ortvec ${fdir}/${flpr}_task-breathhold_echo-1_bold_mcf_deriv1.par motderiv1 \
-# 				 -x1D tmp.${flpr}_nuisreg_mat.1D \
-# 				 -x1D_stop
-# 	fslmaths ${func} -Tmean tmp.${flpr}_avg
-# 	3dTproject -polort 0 -input ${func}.nii.gz -mask ${wdr}/sub-${sub}/ses-${ses}/reg/${mref}_brain_mask.nii.gz \
-# 			   -ort tmp.${flpr}_nuisreg_mat.1D -prefix tmp.${flpr}_prj.nii.gz \
-# 			   -overwrite
-# 	fslmaths tmp.${flpr}_prj -add tmp.${flpr}_avg tmp.${flpr}_den
-
-# 	echo "Computing DVARS and average GM ${den}"
-# 	fsl_motion_outliers -i tmp.${flpr}_den -o tmp.${flpr}_out -s sub-${sub}/dvars_${den}_${flpr}.1D --dvars --nomoco
-# 	fslmeants -i tmp.${flpr}_den -m sub-${sub}_GM > sub-${sub}/avg_GM_${den}_${flpr}.1D
-# done
-
-echo "Collecting DVARS Pre-motcor and FD"
-cp ${fdir}/${flpr}_task-breathhold_echo-1_bold_dvars_pre.par sub-${sub}/dvars_pre_${flpr}.1D
+# cp ${fdir}/${flpr}_task-breathhold_echo-1_bold_dvars_pre.par sub-${sub}/dvars_pre_${flpr}.1D
 cp ${fdir}/${flpr}_task-breathhold_echo-1_bold_fd.par sub-${sub}/fd_${flpr}.1D
 
 # for type in meica  #vessels networks

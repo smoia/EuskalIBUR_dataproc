b692abd00e98ed1a1c69374fc7888fa4566f683c
smoia <s.moia@bcbl.eu>
2020-03-01 23:51:19 +0100

    Manual loop

--- 98.hcp/run_cvr_reliability.sh
+++ 98.hcp/run_cvr_reliability.sh
@@ -15,7 +15,4 @@ wdir=/bcbl/home/public/PJMASK_2/EuskalIBUR_dataproc
 
 cd ${wdir}
 
-for ftype in meica-aggr meica-orth meica-cons meica-mvar echo-2
-do
-	singularity exec -e --no-home -B /bcbl/home/public/PJMASK_2/preproc:/data -B /bcbl/home/public/PJMASK_2/EuskalIBUR_dataproc:/scripts -B /bcbl/home/public/PJMASK_2/tmp:/tmp euskalibur.sif 05.second_level_analysis/09.cvr_reliability.sh ${ftype}
-done
+singularity exec -e --no-home -B /bcbl/home/public/PJMASK_2/preproc:/data -B /bcbl/home/public/PJMASK_2/EuskalIBUR_dataproc:/scripts -B /bcbl/home/public/PJMASK_2/tmp:/tmp euskalibur.sif 05.second_level_analysis/09.cvr_reliability.sh $1

b93586248cfac86544394dfbaa2e2f2eff90c6ec
smoia <s.moia@bcbl.eu>
2020-03-02 17:18:23 +0100

    added jobs dependencies

--- 98.hcp/batch_pipeline.sh
+++ 98.hcp/batch_pipeline.sh
@@ -20,17 +20,19 @@ then
 	mkdir ../LogFiles
 fi
 
-# for sub in 001 002 003 004 007 008 009
-# do
-# 	for ses in $(seq -f %02g 1 10)
-# 	do
-# 		qsub -q long.q -N "s_${sub}_${ses}_EuskalIBUR" -o ${wdr}/../LogFiles/${sub}_${ses}_pipe -e ${wdr}/../LogFiles/${sub}_${ses}_pipe ${wdr}/98.hcp/run_subject_pipeline.sh ${sub} ${ses}
-# 	done
-# done
+joblist=""
+for sub in 001 002 003 004 007 008 009
+do
+	for ses in $(seq -f %02g 1 10)
+	do
+		qsub -q long.q -N "s_${sub}_${ses}_EuskalIBUR" -o ${wdr}/../LogFiles/${sub}_${ses}_pipe -e ${wdr}/../LogFiles/${sub}_${ses}_pipe ${wdr}/98.hcp/run_subject_pipeline.sh ${sub} ${ses}
+		joblist=${joblist}s_${sub}_${ses}_EuskalIBUR,
+	done
+done
+joblist=${joblist::-1}
 
-# qsub -q short.q -N "optcom_EuskalIBUR" -o ${wdr}/../LogFiles/${ftype}_pipe -e ${wdr}/../LogFiles/${ftype}_pipe ${wdr}/98.hcp/run_cvr_reliability.sh ${ftype}
-
-qsub -q short.q -N "meica-aggr_EuskalIBUR" -o ${wdr}/../LogFiles/meica-aggr_pipe -e ${wdr}/../LogFiles/meica-aggr_pipe ${wdr}/98.hcp/run_cvr_reliability.sh meica-aggr
+qsub -q short.q -hold_jid "${joblist}" -N "optcom_EuskalIBUR" -o ${wdr}/../LogFiles/${ftype}_pipe -e ${wdr}/../LogFiles/${ftype}_pipe ${wdr}/98.hcp/run_cvr_reliability.sh ${ftype}
+qsub -q short.q -hold_jid "optcom_EuskalIBUR" -N "meica-aggr_EuskalIBUR" -o ${wdr}/../LogFiles/meica-aggr_pipe -e ${wdr}/../LogFiles/meica-aggr_pipe ${wdr}/98.hcp/run_cvr_reliability.sh meica-aggr
 qsub -q short.q -hold_jid "meica-aggr_EuskalIBUR" -N "meica-orth_EuskalIBUR" -o ${wdr}/../LogFiles/meica-orth_pipe -e ${wdr}/../LogFiles/meica-orth_pipe ${wdr}/98.hcp/run_cvr_reliability.sh meica-orth
 qsub -q short.q -hold_jid "meica-orth_EuskalIBUR" -N "meica-cons_EuskalIBUR" -o ${wdr}/../LogFiles/meica-cons_pipe -e ${wdr}/../LogFiles/meica-cons_pipe ${wdr}/98.hcp/run_cvr_reliability.sh meica-cons
 qsub -q short.q -hold_jid "meica-cons_EuskalIBUR" -N "meica-mvar_EuskalIBUR" -o ${wdr}/../LogFiles/meica-mvar_pipe -e ${wdr}/../LogFiles/meica-mvar_pipe ${wdr}/98.hcp/run_cvr_reliability.sh meica-mvar

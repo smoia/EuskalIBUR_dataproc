ac5db233dce1cf24ec7cd0e25e65a673b6f9bf9e
smoia <s.moia@bcbl.eu>
2020-03-16 17:23:26 +0100

    prepare for twosteps

--- 00.pipelines/pipeline_sim_cvr_motdenoise.sh
+++ 00.pipelines/pipeline_sim_cvr_motdenoise.sh
@@ -60,7 +60,7 @@ echo ""
 # 	/scripts/03.data_preproc/05.compute_CVR_regressors.sh ${sub} ${ses} ${ftype}
 # done
 
-for ftype in meica-aggr meica-orth meica-cons meica-mvar optcom echo-2  #  meica-recn vessels-preg
+for ftype in meica-aggr meica-orth meica-cons # meica-mvar optcom echo-2  #  meica-recn vessels-preg
 do
 	echo "************************************"
 	echo "*** CVR map sub ${sub} ses ${ses} ${ftype}"

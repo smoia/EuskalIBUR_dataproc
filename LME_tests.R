a23616f04b7f395615264cbd7a0d44e6707be88b
Stefano Moia <s.moia@bcbl.eu>
2020-04-03 10:55:41 +0200

    Print better

--- LME_tests.R
+++ LME_tests.R
@@ -21,28 +21,54 @@ anova_table_subset <- vector("list", length = n_comb)
 # Compute and output to file
 sink(file = "LME_models.txt", append = TRUE, type = c("output", "message"),
      split = FALSE)
+print("Full model")
+print(" ")
+print(model)
+print(" ")
+print(anova_table)
+print(" ")
+print("------------------")
+print(" ")
+print(" ")
 
 for(i in 1:n_comb) {
 print(combinations[1, i])
 print(combinations[2, i])
+print(" ")
 subset_data <- subset(data, ftype == combinations[1, i] | ftype == combinations[2, i])
 model_subset[[i]] <- lmer(dvars ~ fd + ftype + ((1+fd+ftype)|ses) + ((1+fd+ftype)|sub), subset_data)
+anova_table_subset[[i]] <- anova(model_subset[[i]])
+print(model_subset[[i]])
+print(" ")
+print(anova_table_subset[[i]])
+print("------------------")
+print(" ")
+print(" ")
+}
+sink()
+
+saveRDS(combinations, file = "lme_combinations.rds")
+saveRDS(model_subset, file = "lme_model_subset.rds")
+saveRDS(anova_table_subset, file = "lme_anova_table_subset.rds")
+
+sink(file = "LME_models.txt", append = TRUE, type = c("output", "message"),
+     split = FALSE)
+print("Full model")
+print(" ")
+print(model)
+print(" ")
+print(anova_table)
+print(" ")
+print("------------------")
+print(" ")
+print(" ")
+
+for(i in 1:n_comb) {
+print(combinations[1, i])
+print(combinations[2, i])
 anova_table_subset[[i]] <- anova(model_subset[[i]])
 print(model_subset[[i]])
 print(anova_table_subset[[i]])
 print("------------------")
 }
 sink()
-unlink("LME_models.txt")
-
-saveRDS(combinations, file = "lme_combinations.rds")
-saveRDS(model_subset, file = "lme_model_subset.rds")
-saveRDS(anova_table_subset, file = "lme_anova_table_subset.rds")
-
-
-# for(i in 1:n_comb) {
-# print(combinations[1, i])
-# print(combinations[2, i])
-# anova_table_subset[[i]] <- anova(model_subset[[i]])
-# print("------------------")
-# }

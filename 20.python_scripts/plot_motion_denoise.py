d5bd4a70d35fc1d952c982711a23be6fce48970a
Stefano Moia <s.moia@bcbl.eu>
2020-05-18 19:04:46 +0200

    Improve time plots, add distance to average

--- 20.python_scripts/plot_motion_denoise.py
+++ 20.python_scripts/plot_motion_denoise.py
@@ -92,157 +92,103 @@ def plot_DVARS_vs_FD(data, ftypes=FTYPE_LIST):
     plt.xlabel('FD')
     plot_ylabel = 'DVARS'
     plt.ylabel(plot_ylabel)
-    fig_name = f'allsubs_DVARS_vs_FD.png'
+    fig_name = 'allsubs_DVARS_vs_FD.png'
     plt.savefig(fig_name, dpi=SET_DPI)
     plt.clf()
     plt.close()
 
 
+# 02. Helping function for the next one
+def read_and_average(filename, ftypes=FTYPE_LIST):
+    # responses is ftype*ses*trials*BH_LEN
+    responses = np.empty((len(ftypes), (LAST_SES - 1), 8, BH_LEN))
+    for n, ftype in enumerate(ftypes):
+        for ses in range(1, LAST_SES):
+            data = np.genfromtxt(filename.format(ftype=ftype, ses=f'{ses:02g}'))
+            for bh in range(8):
+                responses[n, ses-1, bh, :] = data[12+BH_LEN*bh:12+BH_LEN*(bh+1)]
+
+    return responses.reshape((len(ftypes), (LAST_SES - 1)*8, BH_LEN), order='C')
+
+
 # 02. Make timeseries plots
-def plot_timeseries_and_BOLD_vs_FD(ftypes=FTYPE_LIST):
-    for sub in SUB_LIST:
-        bh_timeplot = plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
-        bh_timeplot.suptitle(f'BreathHold (BH) response, subject {sub}')
+def plot_timeseries_and_BOLD_vs_FD(sub, ftypes=FTYPE_LIST):
+    # Read and prepare files
+    fd_response = read_and_average(f'sub-{sub}/{{ftype}}_sub-{sub}_'
+                                   f'ses-{{ses}}.1D', ['fd', ])
+    dvars_responses = read_and_average(f'sub-{sub}/dvars_{{ftype}}_sub-'
+                                       f'{sub}_ses-{{ses}}.1D')
+    bold_responses = read_and_average(f'sub-{sub}/avg_GM_{{ftype}}_sub-'
+                                      f'{sub}_ses-{{ses}}.1D')
+    # Compute averages and standard deviations
+    avg_d = dvars_responses.mean(axis=1)
+    std_d = dvars_responses.std(axis=1)
+    avg_b = bold_responses.mean(axis=1)
+    std_b = bold_responses.std(axis=1)
+    # Compute plot data width for bold as max between distance of each ftype
+    delta_y = (((avg_b+std_b).max(axis=1)-(avg_b-std_b).min(axis=1)).max()
+               + 0.004)
 
-        gs = bh_timeplot.add_gridspec(nrows=(len(ftypes)+1), ncols=2)
-        for col in range(2):
-            bh_timesubplot = bh_timeplot.add_subplot(gs[len(ftypes), col])
-            fd_responses = np.empty((8*(LAST_SES - 1), BH_LEN))
-            for ses in range(1, LAST_SES):
-                fd = np.genfromtxt(f'sub-{sub}/fd_sub-{sub}_ses-{ses:02g}.1D')
-                for bh in range(8):
-                    fd_responses[(8*(ses-1)+bh), :] = fd[12+BH_LEN*bh:12+BH_LEN*(bh+1)]
+    # Compute trial distance from average
+    dist_avg = np.empty(bold_responses.shape[:2])
+    for n in range(bold_responses.shape[2]):
+        dist_avg[:, n] = np.abs(avg_b - np.squeeze(bold_responses[:, :(n+1), :].mean(axis=1))).mean(axis=1)
 
-            bh_timesubplot.plot(fd_responses.mean(axis=0))
+    # Create response plot
+    bh_plot = plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
+    bh_plot.suptitle(f'BreathHold response, subject {sub}')
 
-            bh_timesubplot.set_ylabel('FD')
-            bh_timesubplot.set_xlabel('TPs')
+    gs = bh_plot.add_gridspec(nrows=(len(ftypes)+1), ncols=3)
 
-        avg = np.empty((len(ftypes), BH_LEN))
-        std = np.empty((len(ftypes), BH_LEN))
+    # Add FD on both columns
+    for col in range(2):
+        bh_subplot = bh_plot.add_subplot(gs[len(ftypes), col])
 
-        for i, ftype in enumerate(ftypes):
-            dvars_responses = np.empty((8*(LAST_SES - 1), BH_LEN))
-            for ses in range(1, LAST_SES):
-                dvars = np.genfromtxt(f'sub-{sub}/dvars_{ftype}_sub-{sub}_ses-{ses:02d}.1D')
-                for bh in range(8):
-                    dvars_responses[(8*(ses-1)+bh), :] = dvars[12+BH_LEN*bh:12+BH_LEN*(bh+1)]
+        bh_subplot.plot(TIME, fd_response.mean(axis=(0, 1)))
 
-            avg[i, :] = dvars_responses.mean(axis=0)
-            std[i, :] = dvars_responses.std(axis=0)
+        bh_subplot.set_ylabel('FD')
+        bh_subplot.set_xlabel('TPs')
 
-        for i, ftype in enumerate(ftypes):
-            bh_timesubplot = bh_timeplot.add_subplot(gs[i, 0])
-            bh_timesubplot.plot(TIME, avg[i, :],
-                                label=FTYPE_DICT[ftype], color=COLOURS[i])
-            bh_timesubplot.fill_between(TIME, avg[i, :] - std[i, :],
-                                        avg[i, :] + std[i, :],
-                                        color=COLOURS[i], alpha=0.2)
+    for i, ftype in enumerate(ftypes):
+        # Add DVARS plots in first column
+        bh_subplot = bh_plot.add_subplot(gs[i, 0])
 
-            bh_timesubplot.set_ylim(0, (avg + std).max()+((avg + std).max()/10))
-            bh_timesubplot.set_ylabel('DVARS')
-            bh_timesubplot.axes.get_xaxis().set_ticks([])
+        bh_subplot.plot(TIME, avg_d[i, :],
+                        label=FTYPE_DICT[ftype], color=COLOURS[i])
+        bh_subplot.fill_between(TIME, avg_d[i, :] - std_d[i, :],
+                                avg_d[i, :] + std_d[i, :],
+                                color=COLOURS[i], alpha=0.2)
 
-        avg = np.empty((len(ftypes), BH_LEN))
-        std = np.empty((len(ftypes), BH_LEN))
-        max_delta_y = 0  # This is for visualisation purposes
+        bh_subplot.set_ylim(0, (avg_d + std_d).max()*11/10)
+        bh_subplot.set_ylabel('DVARS')
+        bh_subplot.axes.get_xaxis().set_ticks([])
 
-        for i, ftype in enumerate(ftypes):
-            bh_responses = np.empty((8*(LAST_SES - 1), BH_LEN))
-            for ses in range(1, LAST_SES):
-                avg_gm = np.genfromtxt(f'sub-{sub}/avg_GM_{ftype}_sub-{sub}_ses-{ses:02g}.1D')
-                for bh in range(8):
-                    bh_trial = avg_gm[12+BH_LEN*bh:12+BH_LEN*(bh+1)]
-                    bh_responses[(8*(ses-1)+bh), :] = (bh_trial - bh_trial.mean()) / bh_trial.mean()
+        # Add BOLD plots in second column
+        bh_subplot = bh_plot.add_subplot(gs[i, 1])
+        bh_subplot.plot(TIME, avg_b[i, :],
+                        label=FTYPE_DICT[ftype], color=COLOURS[i])
+        bh_subplot.fill_between(TIME, avg_b[i, :] - std_b[i, :],
+                                avg_b[i, :] + std_b[i, :],
+                                color=COLOURS[i], alpha=0.2)
 
-            avg[i, :] = bh_responses.mean(axis=0)
-            std[i, :] = bh_responses.std(axis=0)
+        min_y = (avg_b - std_b)[i, :].min() - 0.002
+        bh_subplot.set_ylim(min_y, min_y + delta_y)
+        bh_subplot.set_ylabel('% BOLD')
+        bh_subplot.axes.get_xaxis().set_ticks([])
 
-            delta_y = (avg + std)[i, :].max() - (avg - std)[i, :].min() + 0.004
-            if delta_y > max_delta_y:
-                max_delta_y = delta_y
+        # Add distance to average plot
+        bh_subplot = bh_plot.add_subplot(gs[i, 2])
+        bh_subplot.plot(dist_avg[i, :],
+                        label=FTYPE_DICT[ftype], color=COLOURS[i])
 
-        for i, ftype in enumerate(ftypes):
-            bh_timesubplot = bh_timeplot.add_subplot(gs[i, 1])
-            bh_timesubplot.plot(TIME, avg[i, :],
-                                label=FTYPE_DICT[ftype], color=COLOURS[i])
-            bh_timesubplot.fill_between(TIME, avg[i, :] - std[i, :],
-                                        avg[i, :] + std[i, :],
-                                        color=COLOURS[i], alpha=0.2)
+        bh_subplot.set_ylim(0, 1)
+        bh_subplot.set_ylabel('avg dist')
+        bh_subplot.axes.get_xaxis().set_ticks([])
+        bh_subplot.legend(loc=1, prop={'size': 8})
 
-            min_y = (avg - std)[i, :].min() - 0.002
-            bh_timesubplot.set_ylim(min_y, min_y + delta_y)
-            bh_timesubplot.set_ylabel('% BOLD')
-            bh_timesubplot.axes.get_xaxis().set_ticks([])
-            bh_timesubplot.legend(loc=1, prop={'size': 8})
+    bh_plot.savefig(f'{sub}_BOLD_time.png', dpi=SET_DPI)
 
-        bh_timeplot.savefig(f'{sub}_BOLD_time.png', dpi=SET_DPI)
-
-        plt.close('all')
-
-        # for ses in range(1, LAST_SES):
-        #     bh_timesesplot = plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
-        #     bh_timesesplot.suptitle(f'BreathHold (BH) response, subject {sub},'
-        #                             f'session {ses:02g}')
-        #     gs = bh_timesesplot.add_gridspec(nrows=(len(ftypes)+1), ncols=2)
-        #     for col in range(2):
-        #         bh_timesubplot = bh_timesesplot.add_subplot(gs[len(ftypes), col])
-        #         bh_timesubplot.plot(fd_responses[8*(ses-1):8*ses, :].mean(axis=0))
-        #         bh_timesubplot.set_ylabel('FD')
-        #         bh_timesubplot.set_xlabel('TPs')
-
-        #     avg = np.empty((len(ftypes), BH_LEN))
-        #     std = np.empty((len(ftypes), BH_LEN))
-
-        #     for i, ftype in enumerate(ftypes):
-        #         dvars_responses = np.empty((8*(LAST_SES - 1), BH_LEN))
-        #         for ses in range(1, LAST_SES):
-        #             dvars = np.genfromtxt(f'sub-{sub}/dvars_{ftype}_sub-{sub}_ses-{ses:02d}.1D')
-        #             for bh in range(8):
-        #                 dvars_responses[(8*(ses-1)+bh), :] = dvars[12+BH_LEN*bh:12+BH_LEN*(bh+1)]
-
-        #         avg[i, :] = dvars_responses.mean(axis=0)
-        #         std[i, :] = dvars_responses.std(axis=0)
-
-        #     for i, ftype in enumerate(ftypes):
-        #         bh_timesubplot = bh_timesesplot.add_subplot(gs[i, 0])
-        #         bh_timesubplot.plot(TIME, avg[i, :],
-        #                             label=FTYPE_DICT[ftype], color=COLOURS[i])
-        #         bh_timesubplot.fill_between(TIME, avg[i, :] - std[i, :],
-        #                                     avg[i, :] + std[i, :],
-        #                                     color=COLOURS[i], alpha=0.2)
-
-        #         bh_timesubplot.set_ylim(0, (avg + std).max()+((avg + std).max()/10))
-        #         bh_timesubplot.set_ylabel('DVARS')
-        #         bh_timesubplot.axes.get_xaxis().set_ticks([])
-
-        #     for i, ftype in enumerate(ftypes):
-        #         bh_responses = np.empty((8*(LAST_SES - 1), BH_LEN))
-        #         for ses in range(1, LAST_SES):
-        #             avg_gm = np.genfromtxt(f'sub-{sub}/avg_GM_{ftype}_sub-{sub}_ses-{ses:02g}.1D')
-        #             for bh in range(8):
-        #                 bh_trial = avg_gm[12+BH_LEN*bh:12+BH_LEN*(bh+1)]
-        #                 bh_responses[(8*(ses-1)+bh), :] = (bh_trial - bh_trial.mean()) / bh_trial.mean()
-
-        #         avg[i, :] = bh_responses.mean(axis=0)
-        #         std[i, :] = bh_responses.std(axis=0)
-
-        #     for i, ftype in enumerate(ftypes):
-        #         bh_timesubplot = bh_timesesplot.add_subplot(gs[i, 1])
-        #         bh_timesubplot.plot(TIME, avg[i, :],
-        #                             label=FTYPE_DICT[ftype], color=COLOURS[i])
-        #         bh_timesubplot.fill_between(TIME, avg[i, :] - std[i, :],
-        #                                     avg[i, :] + std[i, :],
-        #                                     color=COLOURS[i], alpha=0.2)
-
-        #         min_y = (avg - std)[i, :].min() - 0.002
-        #         bh_timesubplot.set_ylim(min_y, min_y + delta_y)
-        #         bh_timesubplot.set_ylabel('% BOLD')
-        #         bh_timesubplot.axes.get_xaxis().set_ticks([])
-        #         bh_timesubplot.legend(loc=1, prop={'size': 8})
-
-        #     bh_timesesplot.savefig(f'{sub}_BOLD_time_ses_{ses:02g}.png',
-        #                            dpi=SET_DPI)
+    plt.close('all')
 
 
 if __name__ == '__main__':
@@ -253,7 +199,8 @@ if __name__ == '__main__':
     data = pd.read_csv('sub_table.csv')
 
     plot_DVARS_vs_FD(data)
-    plot_timeseries_and_BOLD_vs_FD()
+    for sub in SUB_LIST:
+        plot_timeseries_and_BOLD_vs_FD(sub)
     # os.makedirs('tps')
     # os.chdir('tps')
     # plot_tps_BOLD_vs_FD()

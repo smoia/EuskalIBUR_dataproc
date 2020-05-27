e28c3809a7c3b7ededf8ba2c453a14290c4d40a8
Stefano Moia <s.moia@bcbl.eu>
2020-05-27 23:24:40 +0200

    Correct temp dir

--- 10.visualisation/06.plot_icc_maps.sh
+++ 10.visualisation/06.plot_icc_maps.sh
@@ -2,6 +2,7 @@
 
 wdr=${1:-/data}
 scriptdir=${2:-/scripts}
+tmp=${3:-/tmp}
 
 ### Main ###
 
@@ -14,7 +15,7 @@ then
 	rm -rf ${tmp}/tmp.06pim
 fi
 
-mkdir tmp.06pim
+mkdir ${tmp}/tmp.06pim
 
 for mtype in masked  # corrected masked
 do
@@ -48,22 +49,22 @@ do
 					--clippingRange 0 100 --gamma 0.0 --cmapResolution 256 --numSteps 100 --blendFactor 0.1 --smoothing 0 \
 					--resolution 100 --numInnerSteps 10 --volume 0
 			
-			convert ICC2_${map}_${ftype}.png -filter Point -resize 1035x370 +repage -crop 920x212+15+70 +repage tmp.06pim/ICC2_${map}_${ftype}_res.png
-			convert ICC2_${map}_${ftype}_noclip.png -filter Point -resize 1035x370 +repage -crop 920x212+15+70 +repage tmp.06pim/ICC2_${map}_${ftype}_noclip_res.png
+			convert ICC2_${map}_${ftype}.png -filter Point -resize 1035x370 +repage -crop 920x212+15+70 +repage ${tmp}/tmp.06pim/ICC2_${map}_${ftype}_res.png
+			convert ICC2_${map}_${ftype}_noclip.png -filter Point -resize 1035x370 +repage -crop 920x212+15+70 +repage ${tmp}/tmp.06pim/ICC2_${map}_${ftype}_noclip_res.png
 
-			appending="${appending} tmp.06pim/ICC2_${map}_${ftype}_res.png"
-			appending_noclip="${appending_noclip} tmp.06pim/ICC2_${map}_${ftype}_noclip_res.png"
+			appending="${appending} ${tmp}/tmp.06pim/ICC2_${map}_${ftype}_res.png"
+			appending_noclip="${appending_noclip} ${tmp}/tmp.06pim/ICC2_${map}_${ftype}_noclip_res.png"
 		done
 
-		appending="${appending} +repage tmp.06pim/ICC2_${map}_app.png"
-		appending_noclip="${appending_noclip} +repage tmp.06pim/ICC2_${map}_noclip_app.png"
+		appending="${appending} +repage ${tmp}/tmp.06pim/ICC2_${map}_app.png"
+		appending_noclip="${appending_noclip} +repage ${tmp}/tmp.06pim/ICC2_${map}_noclip_app.png"
 		${appending}
 		${appending_noclip}
 	done
-	composite tmp.06pim/ICC2_cvr_${mtype}_app.png ${scriptdir}/10.visualisation/canvas/ICC_canvas.png +repage tmp.06pim/ICC2_cvr_${mtype}.png
-	composite -geometry +990+0 tmp.06pim/ICC2_lag_${mtype}_noclip_app.png tmp.06pim/ICC2_cvr_${mtype}.png +repage ICC2_${mtype}.png
+	composite ${tmp}/tmp.06pim/ICC2_cvr_${mtype}_app.png ${scriptdir}/10.visualisation/canvas/ICC_canvas.png +repage ${tmp}/tmp.06pim/ICC2_cvr_${mtype}.png
+	composite -geometry +990+0 ${tmp}/tmp.06pim/ICC2_lag_${mtype}_noclip_app.png ${tmp}/tmp.06pim/ICC2_cvr_${mtype}.png +repage ICC2_${mtype}.png
 done
 
-rm -rf tmp.06pim
+rm -rf ${tmp}/tmp.06pim
 
 cd ${cwd}

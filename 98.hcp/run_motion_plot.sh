713cee87f5c93c6603fe4ba7efe3d022fc517fe2
Stefano Moia <s.moia@bcbl.eu>
2020-05-27 19:13:00 +0200

    Add log

--- 98.hcp/run_motion_plot.sh
+++ 98.hcp/run_motion_plot.sh
@@ -9,7 +9,25 @@ module load singularity/3.3.0
 ##---START OF SCRIPT----------------------------------------------------------------------------------------------------##
 ##########################################################################################################################
 
+logname=run_motion_plot_log
+
+######################################
+######### Script starts here #########
+######################################
+
+# Preparing log folder and log file, removing the previous one
+if [[ ! -d "${wdr}/log" ]]; then mkdir ${wdr}/log; fi
+if [[ -e "${wdr}/log/${logname}" ]]; then rm ${wdr}/log/${logname}; fi
+
+echo "************************************" >> ${wdr}/log/${logname}
+
+exec 3>&1 4>&2
+
+exec 1>${wdr}/log/${logname} 2>&1
+
 date
+echo "************************************"
+
 
 wdir=/bcbl/home/public/PJMASK_2/EuskalIBUR_dataproc
 
@@ -18,3 +36,14 @@ cd ${wdir}
 singularity exec -e --no-home -B /bcbl/home/public/PJMASK_2/preproc:/data -B /bcbl/home/public/PJMASK_2/EuskalIBUR_dataproc:/scripts -B /bcbl/home/public/PJMASK_2/tmp:/tmp euskalibur.sif 05.second_level_analysis/02.compare_motion_denoise.sh
 
 singularity exec -e --no-home -B /bcbl/home/public/PJMASK_2/preproc:/data -B /bcbl/home/public/PJMASK_2/EuskalIBUR_dataproc:/scripts -B /bcbl/home/public/PJMASK_2/tmp:/tmp euskalibur.sif 10.visualisation/02.plot_motion_denoise.sh
+
+echo ""
+echo ""
+echo "************************************"
+echo "************************************"
+echo "*** Pipeline Completed!"
+echo "************************************"
+echo "************************************"
+date
+
+cd ${cwd}

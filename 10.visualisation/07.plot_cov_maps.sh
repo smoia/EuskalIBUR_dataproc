e178fa895a148e9d7095f42a1c51bad22c5f6c50
Stefano Moia <s.moia@bcbl.eu>
2020-05-29 14:53:54 +0200

    Correct displayrange and figure

--- 10.visualisation/07.plot_cov_maps.sh
+++ 10.visualisation/07.plot_cov_maps.sh
@@ -37,34 +37,20 @@ do
 						--brightness 50.0 --contrast 50.0 --cmap greyscale --displayRange 0.0 8337.0 --clippingRange 0.0 8420.37 \
 						--gamma 0.0 --cmapResolution 256 --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 \
 						--numInnerSteps 10 --volume 0 CoV_${sub}_${map}_${ftype}.nii.gz --name "CoV_${map}" --overlayType volume \
-						--alpha 100.0 --brightness 50.0 --contrast 50.0 --cmap brain_colours_1hot_iso --displayRange 0.4 1.0 \
-						--clippingRange 0.4 100 --gamma 0.0 --cmapResolution 256 --numSteps 100 --blendFactor 0.1 --smoothing 0 \
-						--resolution 100 --numInnerSteps 10 --volume 0
-				fsleyes render -of CoV_${sub}_${map}_${ftype}_noclip --size 1400 500 --scene lightbox --displaySpace reg/MNI_T1_brain.nii.gz \
-						--zaxis 2 --sliceSpacing 21.4 --zrange 15.55 178.75 --ncols 6 --nrows 1 --hideCursor --showColourBar \
-						--colourBarLocation right --colourBarLabelSide bottom-right --colourBarSize 70.0 --labelSize 18 \
-						--performance 3 reg/MNI_T1_brain.nii.gz --name "MNI_T1_brain" --overlayType volume --alpha 100.0 \
-						--brightness 50.0 --contrast 50.0 --cmap greyscale --displayRange 0.0 8337.0 --clippingRange 0.0 8420.37 \
-						--gamma 0.0 --cmapResolution 256 --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 \
-						--numInnerSteps 10 --volume 0 CoV_${sub}_${map}_${ftype}.nii.gz --name "CoV_${map}" --overlayType volume \
-						--alpha 100.0 --brightness 50.0 --contrast 50.0 --cmap brain_colours_1hot_iso --displayRange 0 1.0 \
-						--clippingRange 0 100 --gamma 0.0 --cmapResolution 256 --numSteps 100 --blendFactor 0.1 --smoothing 0 \
-						--resolution 100 --numInnerSteps 10 --volume 0
+						--alpha 100.0 --brightness 50.0 --contrast 50.0 --cmap brain_colours_1hot_iso --negativeCmap blue-lightblue \
+						--useNegativeCmap --displayRange 0 5.0 --clippingRange 0 100000 --gamma 0.0 --cmapResolution 256 \
+						--numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --volume 0
 
 				convert CoV_${sub}_${map}_${ftype}.png -filter Point -resize 1035x370 +repage -crop 920x212+15+70 +repage ${tmp}/tmp.07pcm/CoV_${sub}_${map}_${ftype}_res.png
-				convert CoV_${sub}_${map}_${ftype}_noclip.png -filter Point -resize 1035x370 +repage -crop 920x212+15+70 +repage ${tmp}/tmp.07pcm/CoV_${sub}_${map}_${ftype}_noclip_res.png
 
 				appending="${appending} ${tmp}/tmp.07pcm/CoV_${sub}_${map}_${ftype}_res.png"
-				appending_noclip="${appending_noclip} ${tmp}/tmp.07pcm/CoV_${sub}_${map}_${ftype}_noclip_res.png"
 			done
 
 			appending="${appending} +repage ${tmp}/tmp.07pcm/CoV_${sub}_${map}_app.png"
-			appending_noclip="${appending_noclip} +repage ${tmp}/tmp.07pcm/CoV_${sub}_${map}_noclip_app.png"
 			${appending}
-			${appending_noclip}
 		done
 		composite ${tmp}/tmp.07pcm/CoV_${sub}_cvr_${mtype}_app.png ${scriptdir}/10.visualisation/canvas/CoV_canvas.png +repage ${tmp}/tmp.07pcm/CoV_${sub}_cvr_${mtype}.png
-		composite -geometry +990+0 ${tmp}/tmp.07pcm/CoV_${sub}_lag_${mtype}_noclip_app.png ${tmp}/tmp.07pcm/CoV_${sub}_cvr_${mtype}.png +repage CoV_${sub}_${mtype}.png
+		composite -geometry +990+0 ${tmp}/tmp.07pcm/CoV_${sub}_lag_${mtype}_app.png ${tmp}/tmp.07pcm/CoV_${sub}_cvr_${mtype}.png +repage CoV_${sub}_${mtype}.png
 	done
 done
 

de678a3adb0d464bbd00e0f3857184619258794b
Stefano Moia <s.moia@bcbl.eu>
2020-05-30 02:04:05 +0200

    Change script name, start writing third level analysis

---
+++ 06.third_level_analysis/01.cvr_post_icc_tests.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+wdr=${1:-/data}
+tmp=${2:-/tmp}
+scriptdir=${3:-/scripts}
+
+cwd=$( pwd )
+
+cd ${wdr}/CVR_reliability || exit
+
+if [ -d tests ]; then rm -rf tests; fi
+
+mkdir tests
+
+cd tests
+
+fslmaths ${scriptdir}/90.template/MNI152_T1_1mm_brain_resamp_2.5mm -bin mask
+
+for map in masked # corrected
+do
+	for inmap in cvr lag
+	do
+		inmap=${inmap}_${map}
+		for ftype in echo-2 optcom meica-aggr meica-cons meica-orth all-orth
+		do
+			# Extract ICC
+			fslmeants -i ICC2_${inmap}_${ftype}.nii.gz -m mask.nii.gz --showall --transpose -o ICC2_${inmap}_${ftype}.txt
+
+			# Extract CoV
+			for sub in 001 002 003 004 007 008 009
+			do
+				fslmeants -i CoV_${sub}_${inmap}_${ftype}.nii.gz -m mask.nii.gz --showall --transpose -o ICC2_${inmap}_${ftype}.txt
+			done
+		done
+	done
+done
+
+python3 /scripts/20.python_scripts/post_icc_tests.py
+
+cd ${cwd}

38d9b887997b7c3af8a12e9b812d4cb48e551ce1
Stefano Moia <s.moia@bcbl.eu>
2020-05-31 20:11:00 +0200

    Add right folders

--- 06.third_level_analysis/01.cvr_post_icc_tests.sh
+++ 06.third_level_analysis/01.cvr_post_icc_tests.sh
@@ -10,7 +10,7 @@ cd ${wdr}/CVR_reliability || exit
 
 if [ -d tests ]; then rm -rf tests; fi
 
-mkdir tests
+mkdir tests tests/val
 
 cd tests
 
@@ -24,17 +24,17 @@ do
 		for ftype in echo-2 optcom meica-aggr meica-cons meica-orth all-orth
 		do
 			# Extract ICC
-			fslmeants -i ICC2_${inmap}_${ftype}.nii.gz -m mask.nii.gz --showall --transpose -o ICC2_${inmap}_${ftype}.txt
+			fslmeants -i ICC2_${inmap}_${ftype}.nii.gz -m mask.nii.gz --showall --transpose > val/ICC2_${inmap}_${ftype}.txt
 
 			# Extract CoV
 			for sub in 001 002 003 004 007 008 009
 			do
-				fslmeants -i CoV_${sub}_${inmap}_${ftype}.nii.gz -m mask.nii.gz --showall --transpose -o ICC2_${inmap}_${ftype}.txt
+				fslmeants -i CoV_${sub}_${inmap}_${ftype}.nii.gz -m mask.nii.gz --showall --transpose > val/CoV_${inmap}_${ftype}.txt
 			done
 		done
 	done
 done
 
-python3 /scripts/20.python_scripts/post_icc_tests.py
+python3 ${scriptdir}/20.python_scripts/post_icc_tests.py
 
 cd ${cwd}

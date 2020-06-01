880f77701d6d965327044d8237d4d86591939347
Stefano Moia <s.moia@bcbl.eu>
2020-06-01 12:16:46 +0200

    Got folders right?

--- 06.third_level_analysis/01.cvr_post_icc_tests.sh
+++ 06.third_level_analysis/01.cvr_post_icc_tests.sh
@@ -12,8 +12,6 @@ if [ -d tests ]; then rm -rf tests; fi
 
 mkdir tests tests/val
 
-cd tests
-
 fslmaths ${scriptdir}/90.template/MNI152_T1_1mm_brain_resamp_2.5mm -bin mask
 
 for map in masked # corrected
@@ -24,16 +22,18 @@ do
 		for ftype in echo-2 optcom meica-aggr meica-cons meica-orth all-orth
 		do
 			# Extract ICC
-			fslmeants -i ICC2_${inmap}_${ftype}.nii.gz -m mask.nii.gz --showall --transpose > val/ICC2_${inmap}_${ftype}.txt
+			fslmeants -i ICC2_${inmap}_${ftype}.nii.gz -m mask.nii.gz --showall --transpose > tests/val/ICC2_${inmap}_${ftype}.txt
 
 			# Extract CoV
 			for sub in 001 002 003 004 007 008 009
 			do
-				fslmeants -i CoV_${sub}_${inmap}_${ftype}.nii.gz -m mask.nii.gz --showall --transpose > val/CoV_${inmap}_${ftype}.txt
+				fslmeants -i CoV_${sub}_${inmap}_${ftype}.nii.gz -m mask.nii.gz --showall --transpose > tests/val/CoV_${inmap}_${ftype}.txt
 			done
 		done
 	done
 done
+
+cd tests
 
 python3 ${scriptdir}/20.python_scripts/post_icc_tests.py
 

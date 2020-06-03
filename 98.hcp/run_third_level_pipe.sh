222119a8202916dd4678160c87f7574d1e996bbe
Stefano Moia <s.moia@bcbl.eu>
2020-06-03 19:26:04 +0200

    Add log name and batch

--- 98.hcp/run_third_level_pipe.sh
+++ 98.hcp/run_third_level_pipe.sh
@@ -15,6 +15,21 @@ wdir=/bcbl/home/public/PJMASK_2/EuskalIBUR_dataproc
 
 cd ${wdir}
 
+logname = third_level_pipe
+
+# Preparing log folder and log file, removing the previous one
+if [[ ! -d "${wdir}/log" ]]; then mkdir ${wdir}/log; fi
+if [[ -e "${wdir}/log/${logname}" ]]; then rm ${wdir}/log/${logname}; fi
+
+echo "************************************" >> ${wdir}/log/${logname}
+
+exec 3>&1 4>&2
+
+exec 1>${wdir}/log/${logname} 2>&1
+
+date
+echo "************************************"
+
 singularity exec -e --no-home -B /bcbl/home/public/PJMASK_2/preproc:/data \
 -B /bcbl/home/public/PJMASK_2/EuskalIBUR_dataproc:/scripts \
 -B /export/home/smoia/scratch:/tmp euskalibur.sif 06.third_level_analysis/01.cvr_post_icc_tests.sh

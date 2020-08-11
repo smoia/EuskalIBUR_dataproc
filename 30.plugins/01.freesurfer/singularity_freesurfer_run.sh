948327ed40eb7cfee4648caf55b1ecff47531b5d
vinferrer <v.ferrer@bcbl.eu>
2020-08-11 09:23:49 +0200

    add singularity freesurfer init script

---
+++ 30.plugins/01.freesurfer/singularity_freesurfer_run.sh
@@ -0,0 +1,20 @@
+#$ -S /bin/bash
+#$ -cwd
+#$ -m be
+#$ -M s.moia@bcbl.eu
+
+module load singularity/3.3.0
+
+##########################################################################################################################
+##---START OF SCRIPT----------------------------------------------------------------------------------------------------##
+##########################################################################################################################
+
+date
+PRJDIR=/bcbl/home/public/PJMASK_2
+wdir=/bcbl/home/public/PJMASK_2/EuskalIBUR_dataproc
+SUBJECTS_DIR=${PRJDIR}/tmp_freesurfer
+SUBJ=sub-001
+cd ${wdir}
+
+singularity exec -e --no-home -B /bcbl/home/public/PJMASK_2/preproc -B /bcbl/home/public/PJMASK_2/EuskalIBUR_dataproc freesurfer_img.simg\
+${wdir}/01_freesurfer.sh $PRJDIR $SUBJ $SUBJECTS_DIR

aecf878b398f9da5783da605d62fba7d0a8c7e25
vinferrer <v.ferrer@bcbl.eu>
2020-08-12 10:08:04 +0200

    add space

--- 30.plugins/01.freesurfer/singularity_freesurfer_run.sh
+++ 30.plugins/01.freesurfer/singularity_freesurfer_run.sh
@@ -17,4 +17,4 @@ SUBJ=sub-001
 cd ${wdir}
 
 singularity exec -e --no-home -B /bcbl/home/public/PJMASK_2/preproc -B ${wdir} ${wdir}/euskalibur_freesurfer_container/freesurfer_img.simg\
-${wdir}/EuskalIBUR_dataproc/30.plugins/01.freesurfer/01_freesurfer.sh $PRJDIR $SUBJ $SUBJECTS_DIR
+ ${wdir}/EuskalIBUR_dataproc/30.plugins/01.freesurfer/01_freesurfer.sh $PRJDIR $SUBJ $SUBJECTS_DIR

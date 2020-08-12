53c0c6a9fe6666f084f473bb1108316a0ebea82a
vinferrer <v.ferrer@bcbl.eu>
2020-08-12 10:13:40 +0200

    add home_n-z folder

--- 30.plugins/01.freesurfer/singularity_freesurfer_run.sh
+++ 30.plugins/01.freesurfer/singularity_freesurfer_run.sh
@@ -10,11 +10,11 @@ module load singularity/3.3.0
 ##########################################################################################################################
 
 date
-PRJDIR=/bcbl/home/home_n_z/public/PJMASK_2
-wdir=/bcbl/home/home_n_z/vferrer
+PRJDIR=/bcbl/home/home_n-z/public/PJMASK_2
+wdir=/bcbl/home/home_n-z/vferrer
 SUBJECTS_DIR=${PRJDIR}/tmp_freesurfer
 SUBJ=sub-001
 cd ${wdir}
 
-singularity exec -e --no-home -B /bcbl/home/home_n_z/public/PJMASK_2/preproc -B ${wdir} ${wdir}/euskalibur_freesurfer_container/freesurfer_img.simg\
+singularity exec -e --no-home -B /bcbl/home/home_n-z/public/PJMASK_2/preproc -B ${wdir} ${wdir}/euskalibur_freesurfer_container/freesurfer_img.simg\
  ${wdir}/EuskalIBUR_dataproc/30.plugins/01.freesurfer/01_freesurfer.sh $PRJDIR $SUBJ $SUBJECTS_DIR

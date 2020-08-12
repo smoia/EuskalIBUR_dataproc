e58d2270e0e6223f0cb624cfd93a91ac622301c7
vinferrer <v.ferrer@bcbl.eu>
2020-08-12 10:31:47 +0200

    bind PRJDIR

--- 30.plugins/01.freesurfer/singularity_freesurfer_run.sh
+++ 30.plugins/01.freesurfer/singularity_freesurfer_run.sh
@@ -16,5 +16,5 @@ SUBJECTS_DIR=${PRJDIR}/tmp_freesurfer
 SUBJ=sub-001
 cd ${wdir}
 
-singularity exec -e --no-home -B /bcbl/home/home_n-z/public/PJMASK_2/preproc -B ${wdir} ${wdir}/euskalibur_freesurfer_container/freesurfer_img.simg\
+singularity exec -e --no-home -B ${PRJDIR} -B ${wdir} ${wdir}/euskalibur_freesurfer_container/freesurfer_img.simg\
  ${wdir}/EuskalIBUR_dataproc/30.plugins/01.freesurfer/01_freesurfer.sh $PRJDIR $SUBJ $SUBJECTS_DIR

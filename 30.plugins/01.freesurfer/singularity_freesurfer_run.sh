b667247045ed0f5ad9489ee3929f9903bfcc1f8c
vinferrer <v.ferrer@bcbl.eu>
2020-08-12 10:38:26 +0200

    add the input of SUBJ so the script can be called in a loop

--- 30.plugins/01.freesurfer/singularity_freesurfer_run.sh
+++ 30.plugins/01.freesurfer/singularity_freesurfer_run.sh
@@ -3,6 +3,14 @@
 #$ -m be
 #$ -M v.ferrer@bcbl.eu
 
+if [[ -z "${SUBJ}" ]]; then
+  if [[ ! -z "$1" ]]; then
+     SUBJ=$1
+  else
+     echo "You need to input SUBJECT (SUBJ) as ENVIRONMENT VARIABLE or $1"
+     exit
+  fi
+fi
 module load singularity/3.3.0
 
 ##########################################################################################################################
@@ -13,7 +21,7 @@ date
 PRJDIR=/bcbl/home/public/PJMASK_2
 wdir=/bcbl/home/home_n-z/vferrer
 SUBJECTS_DIR=${PRJDIR}/tmp_freesurfer
-SUBJ=sub-001
+# SUBJ=sub-001
 cd ${wdir}
 
 singularity exec -e --no-home -B ${PRJDIR} -B ${wdir} ${wdir}/euskalibur_freesurfer_container/freesurfer_img.simg\

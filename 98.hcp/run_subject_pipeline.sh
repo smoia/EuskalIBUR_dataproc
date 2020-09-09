c5e5de884688cb717cc0c4a56d8b2ac87e77097d
Stefano Moia <s.moia@bcbl.eu>
2020-09-09 17:33:29 +0200

    Run real pipe

--- 98.hcp/run_subject_pipeline.sh
+++ 98.hcp/run_subject_pipeline.sh
@@ -15,6 +15,6 @@ wdir=/bcbl/home/public/PJMASK_2/EuskalIBUR_dataproc
 
 cd ${wdir}
 
-singularity exec -e --no-home -B /bcbl/home/public/PJMASK_2/preproc:/data -B /bcbl/home/public/PJMASK_2/EuskalIBUR_dataproc:/scripts -B /export/home/smoia/scratch:/tmp euskalibur.sif 00.pipelines/00.tensor_preproc.sh $1 $2
+# singularity exec -e --no-home -B /bcbl/home/public/PJMASK_2/preproc:/data -B /bcbl/home/public/PJMASK_2/EuskalIBUR_dataproc:/scripts -B /export/home/smoia/scratch:/tmp euskalibur.sif 00.pipelines/00.tensor_preproc.sh $1 $2
 # singularity exec -e --no-home -B /bcbl/home/public/PJMASK_2/preproc:/data -B /bcbl/home/public/PJMASK_2/EuskalIBUR_dataproc:/scripts -B /export/home/smoia/scratch:/tmp euskalibur.sif 00.pipelines/00.ISMRM_short_rest.sh $1 $2
-# singularity exec -e --no-home -B /bcbl/home/public/PJMASK_2/preproc:/data -B /bcbl/home/public/PJMASK_2/EuskalIBUR_dataproc:/scripts -B /export/home/smoia/scratch:/tmp euskalibur.sif 00.pipelines/pipeline_sim_cvr_motdenoise.sh $1 $2
+singularity exec -e --no-home -B /bcbl/home/public/PJMASK_2/preproc:/data -B /bcbl/home/public/PJMASK_2/EuskalIBUR_dataproc:/scripts -B /export/home/smoia/scratch:/tmp euskalibur.sif 00.pipelines/pipeline_sim_cvr_motdenoise.sh $1 $2

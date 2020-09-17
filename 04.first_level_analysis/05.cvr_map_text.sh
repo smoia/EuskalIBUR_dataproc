5140b1bd18ff0b7091db5a115cf9347dc3024053
Stefano Moia <s.moia@bcbl.eu>
2020-09-18 00:40:28 +0200

    Complete text cvr pipeline

--- 04.first_level_analysis/05.cvr_map_text.sh
+++ 04.first_level_analysis/05.cvr_map_text.sh
@@ -17,7 +17,6 @@ wdr=${5:-/data}
 scriptdir=${6:-/scripts}
 tmp=${7:-/tmp}
 
-ftype=optcom
 shiftdir=${flpr}_GM_optcom_avg_regr_shift
 step=12
 lag=9
@@ -74,125 +73,25 @@ do
 	fi
 done
 
-fslmerge -tr ${flpr}_${parc}_r2_time ${tmp}/tmp.${flpr}_${parc}_05cmt_res/${flpr}_${parc}_r2_* ${tr}
-
-fslmaths ${flpr}_${parc}_r2_time -Tmaxn ${flpr}_${parc}_cvr_idx
-fslmaths ${flpr}_${parc}_cvr_idx -mul ${step} -sub ${poslag} -mul 0.025 -mas ${mask} ${flpr}_${parc}_cvr_lag
-
-# prepare empty volumes
-fslmaths ${flpr}_${parc}_cvr_idx -mul 0 ${tmp}/${flpr}_${parc}_statsbuck
-fslmaths ${flpr}_${parc}_cvr_idx -mul 0 ${tmp}/${flpr}_${parc}_cbuck
-
-maxidx=( $( fslstats ${flpr}_${parc}_cvr_idx -R ) )
-
-for i in $( seq -f %g 0 ${maxidx[1]} )
-do
-	let v=i*step
-	v=$( printf %04d $v )
-	3dcalc -a ${tmp}/${flpr}_${parc}_statsbuck.1D -b ${tmp}/tmp.${flpr}_${parc}_05cmt_res/stats_${v}.1D -c ${flpr}_${parc}_cvr_idx.1D \
-		   -expr "a+b*equals(c,${i})" -prefix ${tmp}/${flpr}_${parc}_statsbuck.1D -overwrite
-	3dcalc -a ${tmp}/${flpr}_${parc}_cbuck.1D -b ${tmp}/tmp.${flpr}_${parc}_05cmt_res/c_stats_${v}.1D -c ${flpr}_${parc}_cvr_idx.1D \
-		   -expr "a+b*equals(c,${i})" -prefix ${tmp}/${flpr}_${parc}_cbuck.1D -overwrite
-done
-
-3dbucket -prefix ${tmp}/${flpr}_${parc}_spc_over_V.1D -abuc ${tmp}/${flpr}_${parc}_statsbuck.1D'[17]' -overwrite
-3dbucket -prefix ${tmp}/${flpr}_${parc}_tmap.1D -abuc ${tmp}/${flpr}_${parc}_statsbuck.1D'[18]' -overwrite
-
-mv ${tmp}/${flpr}_${parc}_spc_over_V.1D .
-mv ${tmp}/${flpr}_${parc}_tmap.1D .
-mv ${tmp}/${flpr}_${parc}_cbuck.1D .
-mv ${tmp}/${flpr}_${parc}_statsbuck.1D .
-
-# Obtain first CVR maps
-# the factor 71.2 is to take into account the pressure in mmHg:
-# CO2[mmHg] = ([pressure in Donosti]*[Lab altitude] - [Air expiration at body temperature])[mmHg]*(channel_trace[V]*10[V^(-1)]/100)
-# CO2[mmHg] = (768*0.988-47)[mmHg]*(channel_trace*10/100) ~ 71.2 mmHg
-# multiply by 100 cause it's not BOLD % yet!
-fslmaths ${flpr}_${parc}_spc_over_V.1D -div 71.2 -mul 100 ${flpr}_${parc}_cvr.1D
-# Obtain "simple" t-stats and CVR 
-medianvol=$( printf %04d ${poslag} )
-3dcalc -a ${tmp}/tmp.${flpr}_${parc}_05cmt_res/stats_${medianvol}.1D'[17]' -expr 'a / 71.2 * 100' \
-	   -prefix ${flpr}_${parc}_cvr_simple.1D -overwrite
-3dcalc -a ${tmp}/tmp.${flpr}_${parc}_05cmt_res/stats_${medianvol}.1D'[18]' -expr 'a' \
-	   -prefix ${flpr}_${parc}_tmap_simple.1D
-
-if [ ! -d ${flpr}_${parc}_map_cvr ]
-then
-	mkdir ${flpr}_${parc}_map_cvr
-fi
-
-mv ${flpr}_${parc}_cvr* ${flpr}_${parc}_map_cvr/.
-mv ${flpr}_${parc}_spc* ${flpr}_${parc}_map_cvr/.
-mv ${flpr}_${parc}_tmap* ${flpr}_${parc}_map_cvr/.
-
-##### -------------------------- #
-####                            ##
-###   Getting T and CVR maps   ###
-##                            ####
-# -------------------------- #####
-
-cd ${matdir}
-# Get T score to threshold
-
-# Read and process any mat there is in the "mat" folder
-if [ -e "mat_0000.1D" ]
-then
-	mat=mat_0000.1D
-elif [ -e "mat.1D" ]
-then
-	mat=mat.1D
-else
-	echo "Can't find any matrix. Abort."; exit
-fi
-
 # Extract degrees of freedom
-n=$( head -n 2 ${mat} | tail -n 1 - )
-n=${n#*\"}
-n=${n%\**}
-m=$( head -n 2 ${mat} | tail -n 1 - )
-m=${m#*\"}
-m=${m%\"*}
-let ndof=m-n-1
+
+mat=${matdir}/mat.1D
+
+nreg=$( cat ${mat} | grep ni_type )
+nreg=${nreg#*\"}
+nreg=${nreg%\**}
+ndim=$( cat ${mat} | grep ni_dimen )
+ndim=${ndim#*\"}
+ndim=${ndim%\"*}
+let ndof=ndim-nreg-1
 
 # Get equivalent in t value
 tscore=$( cdf -p2t fitt ${pval} ${ndof} )
 tscore=${tscore##* }
 
-cd ../${flpr}_${parc}_map_cvr
-# Applying threshold on positive and inverted negative t scores, then adding them together to have absolute tscores.
-fslmaths ${flpr}_${parc}_tmap -thr ${tscore} ${flpr}_${parc}_tmap_pos
-fslmaths ${flpr}_${parc}_tmap -mul -1 -thr ${tscore} ${flpr}_${parc}_tmap_neg
-fslmaths ${flpr}_${parc}_tmap_neg -add ${flpr}_${parc}_tmap_pos ${flpr}_${parc}_tmap_abs
-# Binarising all the above to obtain masks.
-fslmaths ${flpr}_${parc}_tmap_pos -bin ${flpr}_${parc}_tmap_pos_mask
-fslmaths ${flpr}_${parc}_tmap_neg -bin ${flpr}_${parc}_tmap_neg_mask
-fslmaths ${flpr}_${parc}_tmap_abs -bin ${flpr}_${parc}_tmap_abs_mask
-
-# Apply constriction by physiology (if a voxel didn't peak in range, might never peak)
-fslmaths ${flpr}_${parc}_cvr_idx -mul ${step} -thr 5 -uthr 705 -bin ${flpr}_${parc}_cvr_idx_physio_constrained
-
-# Obtaining the mask of good and the mask of bad voxels.
-fslmaths ${flpr}_${parc}_cvr_idx_physio_constrained -mas ${flpr}_${parc}_tmap_abs_mask -bin ${flpr}_${parc}_cvr_idx_mask
-fslmaths ${mask} -sub ${flpr}_${parc}_cvr_idx_mask ${flpr}_${parc}_cvr_idx_bad_vxs
-
-# Obtaining lag map (but check next line)
-fslmaths ${flpr}_${parc}_cvr_idx -sub 36 -mas ${flpr}_${parc}_cvr_idx_mask -add 36 -mas ${mask} ${flpr}_${parc}_cvr_idx_corrected
-fslmaths ${flpr}_${parc}_cvr_idx_corrected -mul ${step} -sub ${poslag} -mul 0.025 -mas ${mask} ${flpr}_${parc}_cvr_lag_corrected
-
-echo "Getting masked maps"
-# Mask Good CVR map, lags and tstats
-for map in cvr cvr_lag tmap
-do
-	fslmaths ${flpr}_${parc}_${map} -mas ${flpr}_${parc}_cvr_idx_mask ${flpr}_${parc}_${map}_masked
-done
-
-# # Momentarily retrieving tmap #30
-# fslroi ../${flpr}_${parc}_tstat_time ${flpr}_${parc}_tmap_simple 30 1
-
-echo "Getting corrected maps"
-# Assign the value of the "simple" CVR and tmap map to the bad voxels to have a complete brain.
-fslmaths ${flpr}_${parc}_cvr_idx_bad_vxs -mul ${flpr}_${parc}_cvr_simple -add ${flpr}_${parc}_cvr_masked ${flpr}_${parc}_cvr_corrected
-fslmaths ${flpr}_${parc}_cvr_idx_bad_vxs -mul ${flpr}_${parc}_tmap_simple -add ${flpr}_${parc}_tmap_masked ${flpr}_${parc}_tmap_corrected
+python3 ${scriptdir}/20.python_scripts/compute_cvr_text.py \
+		${tmp}/tmp.${flpr}_${parc}_05cmt_res ${step} ${lag} \
+		${freq} ${tscore} ${flpr}_${parc}
 
 rm -rf ${tmp}/tmp.${flpr}_${parc}_05cmt_*
 
--- 20.python_scripts/compute_cvr_text.py
+++ 20.python_scripts/compute_cvr_text.py
@@ -3,13 +3,59 @@
 import sys
 import os
 
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import seaborn as sns
 
 stats_dir = sys.argv[1]
 step = sys.argv[2]
-miter = sys.argv[3]
+lag = sys.argv[3]
+freq = sys.argv[4]
+t_thr = sys.argv[5]
+prefix = sys.argv[6]
+out_dir = sys.argv[7]
 
-for i in range(0, miter, step)
+lag_samples = lag * freq
+n_iter = lag_samples * 2
+
+dout = dict.fromkeys('cvr_idx', 'cvr_lag', 'cvr_masked_physio_only', 'tmap',
+                     'cvr_masked')
+
+data = dict.fromkeys(range(0, n_iter, step))
+for i in data.keys():
+    # Read last three columns of stats files
+    # This is possible only in optcom because first and last columns are the same
+    data[i] = np.genfromtxt(os.path.join(stats_dir, f'stats_{i}.1D'),
+                            skip_header=9)[:, 2:]  # check axis
+
+# Concatenate and find index of max value and max value
+# - data_3d[:, :, 0] = coefficient (spc over volts)
+# - data_3d[:, :, 1] = t stat
+# - data_3d[:, :, 2] = R^2
+data_3d = np.stack([data[i] for i in data.keys()], 0)
+
+# Get idx of max R^2, then transform them into lags
+dout['cvr_idx'] = data_3d[:, :, 2].argmax(axis=0)
+dout['cvr_lag'] = (dout['cvr_idx'] * step - lag_samples) * (1/freq)
+
+data_squeeze = np.empty_like(data_3d[0])
+
+for n, i in enumerate(dout['cvr_idx']):
+    data_squeeze[n, :] = data_3d[i, n, :]
+
+# Get cvr and tstats
+dout['cvr_masked_physio_only'] = data_squeeze[:, 0] / 71.2 * 100
+dout['tmap'] = data_squeeze[:, 1]
+
+# Create mask based on lag and on tstats and express it as integers (0 or 1)
+# #!# Check boolean operands
+mask_l = (dout['cvr_idx'] >= 5) * (dout['cvr_idx'] <= 705) * 1
+mask_t = (dout['tmap'] >= t_thr) * 1
+
+# mask maps
+for k in ['cvr_lag', 'cvr_masked_physio_only', 'tmap']:
+    dout[k] = dout[k]*mask_l
+
+dout['cvr_masked'] = dout['cvr_masked_physio_only']*mask_t
+
+# export everything
+for k in dout.keys():
+    np.savetxt(os.path.join(out_dir, f'{prefix}_{k}.1D'), dout[k])

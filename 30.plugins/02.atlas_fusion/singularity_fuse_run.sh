4e68c7cb9d3ab495201b1f5537d4f5f8f6ac8288
vinferrer <v.ferrer@bcbl.eu>
2020-09-21 14:08:39 +0200

    change scripts path

--- 30.plugins/02.atlas_fusion/singularity_fuse_run.sh
+++ 30.plugins/02.atlas_fusion/singularity_fuse_run.sh
@@ -40,5 +40,5 @@ date
 # SUBJ=sub-001
 cd ${wdir}
 
-singularity exec -e --no-home -B $PRJDIR/preproc:/Data -B ${PRJDIR}/EuskalIBUR_dataproc:/scripts ${PRJDIR}/EuskalIBUR_dataproc/euskalibur.sif\
+singularity exec -e --no-home -B $PRJDIR/preproc:/Data -B ${wdir}/EuskalIBUR_dataproc:/scripts ${PRJDIR}/EuskalIBUR_dataproc/euskalibur.sif\
  ${wdir}/EuskalIBUR_dataproc/30.plugins/02.atlas_fusion/mni_2_func.sh $SUBJ

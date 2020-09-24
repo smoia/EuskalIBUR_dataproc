2184d562119ef84e8157cbaf64831533ec9bef6e
Stefano Moia <s.moia@bcbl.eu>
2020-09-24 23:21:08 +0200

    Run all files

--- 20.python_scripts/biopac_decimate.py
+++ 20.python_scripts/biopac_decimate.py
@@ -20,26 +20,27 @@ for sub in range(1, 11):
         else:
             ch = 4
 
-        os.chdir(wdir)
-        path = f'sub-{sub:03g}/ses-{ses:02g}/func'
-        filename = f'sub-{sub:03g}_ses-{ses:02g}_task-breathhold_physio'
-        try:
-            os.chdir(path)
-            if not os.path.exists('../func_phys'):
-                os.mkdir('../func_phys')
+        for task in ['breathhold', 'motor', 'simon', 'pinel', 'rest']:
+            os.chdir(wdir)
+            path = f'sub-{sub:03g}/ses-{ses:02g}/func'
+            filename = f'sub-{sub:03g}_ses-{ses:02g}_task-breathhold_physio'
+            try:
+                os.chdir(path)
+                if not os.path.exists('../func_phys'):
+                    os.mkdir('../func_phys')
 
-            os.chdir('../func_phys')
-            print(f'Copying file {filename}')
-            copyfile(f'../func/{filename}.tsv.gz', f'./{filename}.tsv.gz')
-            print(f'Decimating {filename}')
-            co, pidx = bio.partone(filename, ch)
+                os.chdir('../func_phys')
+                print(f'Copying file {filename}')
+                copyfile(f'../func/{filename}.tsv.gz', f'./{filename}.tsv.gz')
+                print(f'Decimating {filename}')
+                co, pidx = bio.partone(filename, ch)
 
-            plt.figure(figsize=(18, 10), dpi=SET_DPI)
-            plt.plot(co)
-            plt.title(f'sub {sub:03g} ses {ses:02g}')
-            plt.savefig(f'{filename}.png', dpi=SET_DPI)
-            plt.close('all')
-        except Exception:
-            pass
+                plt.figure(figsize=(18, 10), dpi=SET_DPI)
+                plt.plot(co)
+                plt.title(f'sub {sub:03g} ses {ses:02g}')
+                plt.savefig(f'{filename}.png', dpi=SET_DPI)
+                plt.close('all')
+            except Exception:
+                pass
 
 os.chdir(cwd)

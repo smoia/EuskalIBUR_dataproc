ead1ab6789604cef42dbf92063136ddb985a25e2
Stefano Moia <s.moia@bcbl.eu>
2020-09-30 02:30:34 +0200

    Add plot ICC vs voxels

--- 20.python_scripts/plot_ICC_parcels.py
+++ 20.python_scripts/plot_ICC_parcels.py
@@ -15,7 +15,7 @@ COLOURS = ['#2ca02cff', '#d62728ff']  # , '#1f77b4ff']
 
 PARC_DICT = {'schaefer-100': 'Functional Networks',
              'flowterritories': 'Vascular Territories',
-             'rand': 'Random' }  #, 
+             'rand': 'Random'}  #, 
              # 'anatomical': 'Anatomical regions'}
 
 cwd = os.getcwd()
@@ -35,7 +35,13 @@ all_colours = all_colours + COLOURS
 # Prepare dictionaries
 icc = {'cvr': {}, 'lag': {}}
 parcel_nlabels = {'cvr': {}, 'lag': {}}
+parcel_nvoxels = {}
 
+# Read parcels voxels
+for i, parc in enumerate(all_parcs):
+    parcel_nvoxels[parc] = np.genfromtxt(f'/scripts/90.template/masked_atlases/{parc}_vx.1D')[:, 0]
+
+# Plot ICC against parcels
 for map in ['cvr']:  #, 'lag']:
     # Setup plot
     plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
@@ -47,6 +53,10 @@ for map in ['cvr']:  #, 'lag']:
     # Import ICC values and populate plot #!# Check axes
     for i, parc in enumerate(all_parcs):
         icc[map][parc] = np.genfromtxt(f'ICC2_{map}_{parc}.1D')[:, 0]
+        # Assuming that if the labels are 20 or less a sharp 0 was added just for processing data
+        if icc[map][parc].shape[0] <= 20:
+            icc[map][parc] = icc[map][parc][icc[map][parc] != 0]
+
         parcel_nlabels[map][parc] = np.asarray([icc[map][parc].shape[0]] *
                                                icc[map][parc].shape[0])
         plt.plot(parcel_nlabels[map][parc], icc[map][parc], '.', color=all_colours[i])
@@ -55,7 +65,26 @@ for map in ['cvr']:  #, 'lag']:
     plt.tight_layout()
 
     # Save plot
-    plt.savefig(f'{map}_ICC.png', dpi=SET_DPI)
+    plt.savefig(f'{map}_ICC_by_parcel.png', dpi=SET_DPI)
+    plt.close('all')
+
+# Plot ICC against parcels
+for map in ['cvr']:  #, 'lag']:
+    # Setup plot
+    plt.figure(figsize=FIGSIZE, dpi=SET_DPI)
+    plt.xlabel('Number of parcels')
+    # plt.xlim(0, 120)
+    plt.ylabel('ICC')
+    plt.ylim(0, 1)
+
+    for i, parc in enumerate(all_parcs):
+        plt.plot(parcel_nvoxels[parc], icc[map][parc], '.', color=all_colours[i])
+
+    plt.legend(PARC_DICT.values())
+    plt.tight_layout()
+
+    # Save plot
+    plt.savefig(f'{map}_ICC_by_voxel.png', dpi=SET_DPI)
     plt.close('all')
 
 os.chdir(cwd)

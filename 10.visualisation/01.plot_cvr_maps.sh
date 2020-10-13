c2d601fe1ca6edded34f5d4f68fd9be4e6f1807f
smoia <s.moia@bcbl.eu>
2020-10-13 23:31:11 +0200

    Add "transparent threshold" in images

--- 10.visualisation/01.plot_cvr_maps.sh
+++ 10.visualisation/01.plot_cvr_maps.sh
@@ -28,14 +28,23 @@ do
 			--name "CVR_(unmasked)" --overlayType volume --alpha 100.0 --cmap red-yellow --negativeCmap blue-lightblue \
 			--useNegativeCmap --displayRange 0.0 0.6 --clippingRange 0.0 10.0 --gamma 0.0 --cmapResolution 256 --interpolation none \
 			--numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
+			
 			fsleyes render -of ${wdr}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_masked \
 			--size 1400 500 --scene lightbox --sliceSpacing 18 --zrange 21 131 \
 			--ncols 6 --nrows 1 --hideCursor --bgColour 0.0 0.0 0.0 --fgColour 1.0 1.0 1.0 --showColourBar --colourBarLocation top \
 			--colourBarLabelSide top-left --colourBarSize 50 --labelSize 11 --performance 3 \
+			${wdr}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr.nii.gz \
+			--name "CVR" --overlayType volume --alpha 40.0 --cmap red-yellow --negativeCmap blue-lightblue \
+			--useNegativeCmap --displayRange 0.0 0.6 --clippingRange 0.0 10.0 --gamma 0.0 --cmapResolution 256 --interpolation none \
+			--numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0 \
 			${wdr}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_masked.nii.gz \
 			--name "CVR" --overlayType volume --alpha 100.0 --cmap red-yellow --negativeCmap blue-lightblue \
 			--useNegativeCmap --displayRange 0.0 0.6 --clippingRange 0.0 10.0 --gamma 0.0 --cmapResolution 256 --interpolation none \
-			--numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
+			--numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0 \
+			${wdr}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_masked.nii.gz \
+			--name "CVR" --overlayType mask --alpha 100.0 --brightness 50.0 --contrast 50.0 --maskColour 0.0 0.0 0.0 \
+			--threshold 0.0 0.6 --outline --outlineWidth 2 --interpolation none --volume 0
+			
 			fsleyes render -of ${wdr}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_corrected \
 			--size 1400 500 --scene lightbox --sliceSpacing 18 --zrange 21 131 \
 			--ncols 6 --nrows 1 --hideCursor --bgColour 0.0 0.0 0.0 --fgColour 1.0 1.0 1.0 --showColourBar --colourBarLocation top \
@@ -52,7 +61,9 @@ do
 			--name "tmap" --overlayType volume --alpha 100.0 --cmap red-yellow --negativeCmap blue-lightblue \
 			--useNegativeCmap --displayRange 0 50.0 --clippingRange 0 100.0 --gamma 0.0 --cmapResolution 256 --interpolation none \
 			--numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
+
 			echo "lag"
+
 			fsleyes render -of ${wdr}/CVR/sub-${sub}_ses-${ses}_${ftype}_map_cvr/sub-${sub}_ses-${ses}_${ftype}_cvr_lag \
 			--size 1400 500 --scene lightbox --sliceSpacing 18 --zrange 21 131 \
 			--ncols 6 --nrows 1 --hideCursor --bgColour 0.0 0.0 0.0 --fgColour 1.0 1.0 1.0 --showColourBar --colourBarLocation top \
--- 10.visualisation/06.plot_icc_maps.sh
+++ 10.visualisation/06.plot_icc_maps.sh
@@ -34,10 +34,18 @@ do
 					--performance 3 reg/MNI_T1_brain.nii.gz --name "MNI_T1_brain" --overlayType volume --alpha 100.0 \
 					--brightness 50.0 --contrast 50.0 --cmap greyscale --displayRange 0.0 8337.0 --clippingRange 0.0 8420.37 \
 					--gamma 0.0 --cmapResolution 256 --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 \
-					--numInnerSteps 10 --volume 0 ICC2_${map}_${ftype}.nii.gz --name "ICC(2,1)_${map}" --overlayType volume \
+					--numInnerSteps 10 --volume 0 \
+					ICC2_${map}_${ftype}.nii.gz --name "ICC(2,1)_${map}" --overlayType volume \
+					--alpha 40.0 --brightness 50.0 --contrast 50.0 --cmap brain_colours_1hot_iso --displayRange 0.4 1.0 \
+					--clippingRange 0 100 --gamma 0.0 --cmapResolution 256 --numSteps 100 --blendFactor 0.1 --smoothing 0 \
+					--resolution 100 --numInnerSteps 10 --volume 0 \
+					ICC2_${map}_${ftype}.nii.gz --name "ICC(2,1)_${map}" --overlayType volume \
 					--alpha 100.0 --brightness 50.0 --contrast 50.0 --cmap brain_colours_1hot_iso --displayRange 0.4 1.0 \
 					--clippingRange 0.4 100 --gamma 0.0 --cmapResolution 256 --numSteps 100 --blendFactor 0.1 --smoothing 0 \
-					--resolution 100 --numInnerSteps 10 --volume 0
+					--resolution 100 --numInnerSteps 10 --volume 0 \
+					ICC2_${map}_${ftype}.nii.gz --name "ICC(2,1)_${map}" --overlayType mask \
+					--alpha 100.0 --brightness 50.0 --contrast 50.0 --maskColour 0.0 0.0 0.0 \
+					--threshold 0.4 100 --outline --outlineWidth 2 --interpolation none --volume 0
 			fsleyes render -of ICC2_${map}_${ftype}_noclip --size 1400 500 --scene lightbox --displaySpace reg/MNI_T1_brain.nii.gz \
 					--zaxis 2 --sliceSpacing 21.4 --zrange 15.55 178.75 --ncols 6 --nrows 1 --hideCursor --showColourBar \
 					--colourBarLocation right --colourBarLabelSide bottom-right --colourBarSize 70.0 --labelSize 18 \

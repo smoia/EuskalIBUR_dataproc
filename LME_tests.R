4b3f8376eec0dd34e2d35a8e640173647fab4441
smoia <s.moia@bcbl.eu>
2020-10-21 15:32:42 +0200

    Update script after Gang's suggestions.

--- LME_tests.R
+++ LME_tests.R
@@ -4,13 +4,16 @@ ftypes_list <- c("pre", "echo-2", "optcom", "meica-cons", "meica-orth", "meica-a
 ftypes_list <- c("pre", "echo-2", "optcom", "meica-cons", "meica-orth", "meica-aggr")
 
 # Read data and make sure all the categorical data is a factor
-data <- read.csv('sub_long_table.csv')
+# Also check that FD and DVARS are centered
+data <- read.csv('sub_long_table.csv', header=T)
 data$sub <- as.factor(data$sub)
 data$ses <- as.factor(data$ses)
 data$ftype <- as.factor(data$ftype)
+data$fd <- scale(data$fd, scale=FALSE) # Centering
+data$dvars <- scale(data$dvars, scale=FALSE) # Centering
 
 # Run model
-model <- lmer(dvars ~ fd * ftype + ((1+fd)|ses) + ((1+fd)|sub), data)
+model <- lmer(dvars ~ fd * ftype + (1|ses) + (1|sub), data)
 summary(model)
 anova_table <- anova(model)
 
@@ -38,7 +41,7 @@ writeLines("\n------------------\n\n\n\n")
 for(i in 1:n_comb) {
 writeLines(sprintf("Pairwise comparison: %s vs %s\n", combinations[1, i], combinations[2, i]))
 subset_data <- subset(data, ftype == combinations[1, i] | ftype == combinations[2, i])
-model_subset[[i]] <- lmer(dvars ~ fd * ftype + ((1+fd)|ses) + ((1+fd)|sub), subset_data)
+model_subset[[i]] <- lmer(dvars ~ fd * ftype + (1|ses) + (1|sub), subset_data)
 anova_table_subset[[i]] <- anova(model_subset[[i]])
 writeLines("Summary\n")
 print(model_subset[[i]])

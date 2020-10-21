b33ae0186a3b4fc270df46eabefa43cbeb0156cc
smoia <s.moia@bcbl.eu>
2020-10-21 15:16:48 +0200

    New run

--- 98.hcp/batch_pipeline.sh
+++ 98.hcp/batch_pipeline.sh
@@ -77,32 +77,38 @@ fi
 # 	qsub -q long.q -N "m_${sub}_01_EuskalIBUR" -o ${wdr}/../LogFiles/${sub}_motor_pipe -e ${wdr}/../LogFiles/${sub}_motor_pipe ${wdr}/98.hcp/run_subject_pipeline.sh ${sub} 02
 # done
 
-# joblist=""
+joblist=""
 
-# for ses in $(seq -f %02g 1 10)
-# do
-# 	rm ${wdr}/../LogFiles/001_${ses}_pipe
-# 	qsub -q long.q -N "s_001_${ses}_EuskalIBUR" -o ${wdr}/../LogFiles/001_${ses}_pipe -e ${wdr}/../LogFiles/001_${ses}_pipe ${wdr}/98.hcp/run_subject_pipeline.sh 001 ${ses}
-# 	joblist=${joblist}s_001_${ses}_EuskalIBUR,
-# done
+for ses in $(seq -f %02g 1 10)
+do
+	rm ${wdr}/../LogFiles/001_${ses}_pipe
+	qsub -q long.q -N "s_001_${ses}_EuskalIBUR" \
+	-o ${wdr}/../LogFiles/001_${ses}_pipe \
+	-e ${wdr}/../LogFiles/001_${ses}_pipe \
+	${wdr}/98.hcp/run_subject_pipeline.sh 001 ${ses}
+	joblist=${joblist}s_001_${ses}_EuskalIBUR,
+done
 
-# joblist=${joblist::-1}
+joblist=${joblist::-1}
 
-# for sub in 002 003 004 007 008 009
-# do
-# 	for ses in $(seq -f %02g 1 10)
-# 	do
-# 		rm ${wdr}/../LogFiles/${sub}_${ses}_pipe
-# 		qsub -q long.q -hold_jid "${joblist}" -N "s_${sub}_${ses}_EuskalIBUR" -o ${wdr}/../LogFiles/${sub}_${ses}_pipe -e ${wdr}/../LogFiles/${sub}_${ses}_pipe ${wdr}/98.hcp/run_subject_pipeline.sh ${sub} ${ses}
-# 		# qsub -q long.q -N "s_${sub}_${ses}_EuskalIBUR" -o ${wdr}/../LogFiles/${sub}_${ses}_pipe -e ${wdr}/../LogFiles/${sub}_${ses}_pipe ${wdr}/98.hcp/run_subject_pipeline.sh ${sub} ${ses}
-# 	done
-# 	joblist=""
-# 	for ses in $(seq -f %02g 1 10)
-# 	do
-# 		joblist=${joblist}s_${sub}_${ses}_EuskalIBUR,
-# 	done
-# 	joblist=${joblist::-1}
-# done
+for sub in 002 003 004 007 008 009
+do
+	for ses in $(seq -f %02g 1 10)
+	do
+		rm ${wdr}/../LogFiles/${sub}_${ses}_pipe
+		qsub -q long.q -N "s_${sub}_${ses}_EuskalIBUR" \
+		-hold_jid "${joblist}" \
+		-o ${wdr}/../LogFiles/${sub}_${ses}_pipe \
+		-e ${wdr}/../LogFiles/${sub}_${ses}_pipe \
+		${wdr}/98.hcp/run_subject_pipeline.sh ${sub} ${ses}
+	done
+	joblist=""
+	for ses in $(seq -f %02g 1 10)
+	do
+		joblist=${joblist}s_${sub}_${ses}_EuskalIBUR,
+	done
+	joblist=${joblist::-1}
+done
 
 # ftype=optcom
 # rm ${wdr}/../LogFiles/${ftype}_pipe
@@ -129,5 +135,11 @@ fi
 # qsub -q short.q -N "s_010_11_prep" -o ${wdr}/../LogFiles/010_11_pipe -e ${wdr}/../LogFiles/010_11_pipe ${wdr}/tmp.preproc_10.sh
 
 rm ${wdr}/../LogFiles/third_level_pipe
-qsub -q long.q -N "third_level_EuskalIBUR" -o ${wdr}/../LogFiles/third_level_pipe -e ${wdr}/../LogFiles/third_level_pipe ${wdr}/98.hcp/run_third_level_pipe.sh
+qsub -q long.q -N "third_level_EuskalIBUR" \
+-hold_jid "${joblist}" \
+-o ${wdr}/../LogFiles/third_level_pipe \
+-e ${wdr}/../LogFiles/third_level_pipe \
+${wdr}/98.hcp/run_third_level_pipe.sh
+# -hold_jid "${old_ftype}_EuskalIBUR" \
+
 # qsub -q short.q -hold_jid "${old_ftype}_EuskalIBUR" -N "third_level_EuskalIBUR" -o ${wdr}/../LogFiles/third_level_pipe -e ${wdr}/../LogFiles/third_level_pipe ${wdr}/98.hcp/run_third_level_pipe.sh

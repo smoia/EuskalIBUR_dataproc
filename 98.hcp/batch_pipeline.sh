28161f866e6783a26c183abea0f1ba8958e15a94
smoia <s.moia@bcbl.eu>
2020-10-24 13:28:31 +0200

    New run

--- 98.hcp/batch_pipeline.sh
+++ 98.hcp/batch_pipeline.sh
@@ -91,24 +91,24 @@ fi
 
 # joblist=${joblist::-1}
 
-for sub in 004 007 008 009  # 002 003 004 007 008 009
-do
-	for ses in $(seq -f %02g 1 10)
-	do
-		rm ${wdr}/../LogFiles/${sub}_${ses}_pipe
-		qsub -q long.q -N "s_${sub}_${ses}_EuskalIBUR" \
-		-o ${wdr}/../LogFiles/${sub}_${ses}_pipe \
-		-e ${wdr}/../LogFiles/${sub}_${ses}_pipe \
-		${wdr}/98.hcp/run_subject_pipeline.sh ${sub} ${ses}
-		# -hold_jid "${joblist}" \
-	done
-	# joblist=""
-	# for ses in $(seq -f %02g 1 10)
-	# do
-	# 	joblist=${joblist}s_${sub}_${ses}_EuskalIBUR,
-	# done
-	# joblist=${joblist::-1}
-done
+# for sub in 004 007 008 009  # 002 003 004 007 008 009
+# do
+# 	for ses in $(seq -f %02g 1 10)
+# 	do
+# 		rm ${wdr}/../LogFiles/${sub}_${ses}_pipe
+# 		qsub -q long.q -N "s_${sub}_${ses}_EuskalIBUR" \
+# 		-o ${wdr}/../LogFiles/${sub}_${ses}_pipe \
+# 		-e ${wdr}/../LogFiles/${sub}_${ses}_pipe \
+# 		${wdr}/98.hcp/run_subject_pipeline.sh ${sub} ${ses}
+# 		# -hold_jid "${joblist}" \
+# 	done
+# 	joblist=""
+# 	for ses in $(seq -f %02g 1 10)
+# 	do
+# 		joblist=${joblist}s_${sub}_${ses}_EuskalIBUR,
+# 	done
+# 	joblist=${joblist::-1}
+# done
 
 # ftype=optcom
 # rm ${wdr}/../LogFiles/${ftype}_pipe
@@ -134,12 +134,12 @@ done
 
 # qsub -q short.q -N "s_010_11_prep" -o ${wdr}/../LogFiles/010_11_pipe -e ${wdr}/../LogFiles/010_11_pipe ${wdr}/tmp.preproc_10.sh
 
-# rm ${wdr}/../LogFiles/third_level_pipe
-# qsub -q long.q -N "third_level_EuskalIBUR" \
-# -hold_jid "${joblist}" \
-# -o ${wdr}/../LogFiles/third_level_pipe \
-# -e ${wdr}/../LogFiles/third_level_pipe \
-# ${wdr}/98.hcp/run_third_level_pipe.sh
-# -hold_jid "${old_ftype}_EuskalIBUR" \
 
-# qsub -q short.q -hold_jid "${old_ftype}_EuskalIBUR" -N "third_level_EuskalIBUR" -o ${wdr}/../LogFiles/third_level_pipe -e ${wdr}/../LogFiles/third_level_pipe ${wdr}/98.hcp/run_third_level_pipe.sh
+### Third level pipeline
+rm ${wdr}/../LogFiles/third_level_pipe
+qsub -q long.q -N "third_level_EuskalIBUR" \
+-o ${wdr}/../LogFiles/third_level_pipe \
+-e ${wdr}/../LogFiles/third_level_pipe \
+${wdr}/98.hcp/run_third_level_pipe.sh
+# -hold_jid "${joblist}" \
+# -hold_jid "${old_ftype}_EuskalIBUR" \

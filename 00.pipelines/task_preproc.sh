6d5bab33a926887ed1b836687cbc80d8f438e7f2
Stefano Moia <s.moia@bcbl.eu>
2021-01-06 14:32:05 +0100

    Cool looping of echoes AND optcom

--- 00.pipelines/task_preproc.sh
+++ 00.pipelines/task_preproc.sh
@@ -70,7 +70,7 @@ do
 	echo "************************************"
 
 	bold=${flpr}_task-${task}_echo-${e}_bold_cr
-	/scripts/02.func_preproc/04.func_realign.sh ${bold} ${fmat} ${mask} ${fdir} ${sbrf} none ${tmp}
+	${scriptdir}/02.func_preproc/04.func_realign.sh ${bold} ${fmat} ${mask} ${fdir} ${sbrf} none ${tmp}
 done
 
 echo "************************************"
@@ -78,80 +78,54 @@ echo "*** Func MEICA ${task} BOLD"
 echo "************************************"
 echo "************************************"
 
-/scripts/02.func_preproc/06.func_optcom.sh ${fmat}_bet ${fdir} "${TEs}" ${tmp}
+${scriptdir}/02.func_preproc/05.func_meica.sh ${fmat}_bet ${fdir} "${TEs}" check_bck ${tmp} ${scriptdir}
+
+# Since t2smap gives different results from tedana, prefer the former for optcom
+${scriptdir}/02.func_preproc/06.func_optcom.sh ${fmat}_bet ${fdir} "${TEs}" ${tmp}
 
 # As it's ${task}, don't skip smoothing and denoising!
-for e in $( seq 1 ${nTE} )
+for e in $( seq 1 ${nTE}; echo "optcom" )
 do
-	bold=${flpr}_task-${task}_echo-${e}_bold
+	if [ ${e} != "optcom" ]
+	then
+		e=echo-${e}
+	fi
+	bold=${flpr}_task-${task}_${e}_bold
 	
 	echo "************************************"
-	echo "*** Func Nuiscomp ${task} BOLD echo ${e}"
+	echo "*** Func Nuiscomp ${task} BOLD ${e}"
 	echo "************************************"
 	echo "************************************"
 
-	/scripts/02.func_preproc/07.func_nuiscomp.sh ${bold}_bet ${fmat} none none ${sbrf} ${fdir} none no 0.3 0.05 no no ${tmp}
+	${scriptdir}/02.func_preproc/07.func_nuiscomp.sh ${bold}_bet ${fmat} none none ${sbrf} ${fdir} none no 0.3 0.05 no no ${tmp}
 	
 	echo "************************************"
-	echo "*** Func Pepolar ${task} BOLD echo ${e}"
+	echo "*** Func Pepolar ${task} BOLD ${e}"
 	echo "************************************"
 	echo "************************************"
 
-	/scripts/02.func_preproc/02.func_pepolar.sh ${bold}_den ${fdir} ${sbrf}_topup none none ${tmp}
+	${scriptdir}/02.func_preproc/02.func_pepolar.sh ${bold}_den ${fdir} ${sbrf}_topup none none ${tmp}
 
 	echo "************************************"
-	echo "*** Func smoothing ${task} BOLD echo ${e}"
-	echo "************************************"
-	echo "************************************"
-
-	/scripts/02.func_preproc/08.func_smooth.sh ${bold}_tpp ${fdir} 5 ${mask} ${tmp}
-	immv ${fdir}/${bold}_SPC ${fdir}/01.${bold}_native_SPC_preprocessed
-
-	echo "************************************"
-	echo "*** Func SPC ${task} BOLD echo ${e}"
+	echo "*** Func smoothing ${task} BOLD ${e}"
 	echo "************************************"
 	echo "************************************"
 
-	/scripts/02.func_preproc/09.func_spc.sh ${bold}_tpp ${fdir} ${tmp}
+	${scriptdir}/02.func_preproc/08.func_smooth.sh ${bold}_tpp ${fdir} 5 ${mask} ${tmp}
+	imcp ${tmp}/${bold}_SPC ${fdir}/01.${bold}_native_SPC_preprocessed
+
+	echo "************************************"
+	echo "*** Func SPC ${task} BOLD ${e}"
+	echo "************************************"
+	echo "************************************"
+
+	${scriptdir}/02.func_preproc/09.func_spc.sh ${bold}_tpp ${fdir} ${tmp}
 
 	# Rename output
-	immv ${fdir}/${bold}_SPC ${fdir}/01.${bold}_native_SPC_preprocessed
+	immv ${tmp}/${bold}_SPC ${fdir}/01.${bold}_native_SPC_preprocessed
 
 done
 
-# Repeat part of it for optcom
-bold=${flpr}_task-${task}_optcom_bold
+rm -rf ${tmp}
 
-echo "************************************"
-echo "*** Func Nuiscomp ${task} BOLD optcom"
-echo "************************************"
-echo "************************************"
-
-/scripts/02.func_preproc/07.func_nuiscomp.sh ${bold}_bet ${fmat} none none ${sbrf} ${fdir} none yes 0.3 0.05 yes
-
-echo "************************************"
-echo "*** Func Pepolar ${task} BOLD optcom"
-echo "************************************"
-echo "************************************"
-
-/scripts/02.func_preproc/02.func_pepolar.sh ${bold}_den ${fdir} ${sbrf}_topup
-
-echo "************************************"
-echo "*** Func smoothing ${task} BOLD optcom"
-echo "************************************"
-echo "************************************"
-
-/scripts/02.func_preproc/08.func_smooth.sh ${bold}_tpp ${fdir} 3 ${mask}
-
-echo "************************************"
-echo "*** Func SPC ${task} BOLD optcom"
-echo "************************************"
-echo "************************************"
-
-/scripts/02.func_preproc/09.func_spc.sh ${bold}_tpp ${fdir}
-
-# Rename output
-immv ${fdir}/${bold}_SPC ${fdir}/01.${bold}_native_SPC_preprocessed
-
-
-/scripts/00.pipelines/clearspace.sh ${sub} ${ses} ${wdr} task
+${scriptdir}/00.pipelines/clearspace.sh ${sub} ${ses} ${wdr} task

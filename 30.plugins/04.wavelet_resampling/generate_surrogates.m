368b4e13ca6652114f6adaae30849689a006eb54
Stefano Moia <s.moia@bcbl.eu>
2021-01-29 16:58:47 +0100

    Try with classic nargin

--- 30.plugins/04.wavelet_resampling/generate_surrogates.m
+++ 30.plugins/04.wavelet_resampling/generate_surrogates.m
@@ -1,4 +1,4 @@
-function generate_surrogates(varargin)
+function generate_surrogates(infile,mask,Nsurr,outfolder,prefix)
 % This script generates surrogate volumes for a certain input. Enjoy a good sleep!
 %
 % Required arg:
@@ -26,6 +26,19 @@ end
 if nargin < 2
 	disp('Missing required infile and mask!')
 	disp()
+	disp('This script generates surrogate volumes for a certain input. Enjoy a good sleep!')
+	disp('')
+	disp('Required arg:')
+	disp('   - input: the input files')
+	disp('   - mask: the mask used to constrain the surrogate')
+	disp('Optional arg (in order):')
+	disp('   - Nsurr: Number of surrogates to output')
+	disp('       - Def: 1000')
+	disp('   - outfolder: folder where to place the surrogates')
+	disp('       - Def: same as file basename')
+	disp('   - prefix: the prefix to give to the surrogate')
+	disp('       - Def: same as file basename')
+	return
 end
 
 if infile == 'help'
@@ -44,20 +57,22 @@ if infile == 'help'
 	return
 end
 
+if nargin < 3
+	Nsurr = 1000
+end
+if nargin < 4
+    outfolder = sPath
+else
+    mkdir(outfolder)
+end
+if nargin < 5
+    prefix = sFilename
+end
+
 Nsurr = str2double(Nsurr);
 dimresamp = [1 1 0];
 adjust = 1;
 [sPath, sFilename, sExt] = fileparts(infile);
-
-if outfolder ~= ''
-    mkdir(outfolder)
-else
-    outfolder = sPath
-end
-
-if prefix == ''
-    prefix = sFilename
-end
 
 for i=0:(Nsurr-1)
     Vsurr = strcat(outfolder,'/',sprintf('%s_Surr_%03i.nii.gz',prefix,i));

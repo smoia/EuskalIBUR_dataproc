0c9b579eaf0621f078804b32c827d28377747193
smoia <s.moia@bcbl.eu>
2021-01-29 20:04:36 +0100

    new gensurr version

--- 30.plugins/04.wavelet_resampling/run_generate_surrogates.sh
+++ 30.plugins/04.wavelet_resampling/run_generate_surrogates.sh
@@ -19,7 +19,6 @@ else
   LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRROOT}/sys/os/glnxa64;
   LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRROOT}/sys/opengl/lib/glnxa64;
   export LD_LIBRARY_PATH;
-  export MCR_CACHE_ROOT=/tmp;
   echo LD_LIBRARY_PATH is ${LD_LIBRARY_PATH};
   shift 1
   args=

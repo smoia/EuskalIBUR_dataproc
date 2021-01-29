d078a6f0494036d8a381c83d626495fff50ffb10
Stefano Moia <s.moia@bcbl.eu>
2021-01-29 16:28:51 +0100

    Rework input

--- 30.plugins/04.wavelet_resampling/generate_surrogates.m
+++ 30.plugins/04.wavelet_resampling/generate_surrogates.m
@@ -1,5 +1,4 @@
-function generate_surrogates(input,mask,varoptin)
-
+function generate_surrogates(varargin)
 % This script generates surrogate volumes for a certain input. Enjoy a good sleep!
 %
 % Required arg:
@@ -14,23 +13,41 @@ function generate_surrogates(input,mask,varoptin)
 %        - Def: same as file basename
 
 % Setting defaults for the varoptin
-optvar = {1000, '', ''};
-
-% Reading the varoptin
-numvarin = length(varoptin);
+optvar = {'help', 'help', 1000, '', ''};
 
 % Reading optional arguments
-if numvarin >= 1
-    optvar(1:numvarin) = varoptin;
+if nargin >= 1
+    optvar(1:nargin) = varargin{1:nargin};
 end
 
 % Finally starting with variables
-[Nsurr,outfolder,prefix] = optvar{:};
+[infile,mask,Nsurr,outfolder,prefix] = optvar{:};
+
+if nargin < 2
+	disp('Missing required infile and mask!')
+	disp()
+end
+
+if infile == 'help'
+	disp('This script generates surrogate volumes for a certain input. Enjoy a good sleep!')
+	disp('')
+	disp('Required arg:')
+	disp('   - input: the input files')
+	disp('   - mask: the mask used to constrain the surrogate')
+	disp('Optional arg (in order):')
+	disp('   - Nsurr: Number of surrogates to output')
+	disp('       - Def: 1000')
+	disp('   - outfolder: folder where to place the surrogates')
+	disp('       - Def: same as file basename')
+	disp('   - prefix: the prefix to give to the surrogate')
+	disp('       - Def: same as file basename')
+	return
+end
 
 Nsurr = str2double(Nsurr);
 dimresamp = [1 1 0];
 adjust = 1;
-[sPath, sFilename, sExt] = fileparts(input);
+[sPath, sFilename, sExt] = fileparts(infile);
 
 if outfolder ~= ''
     mkdir(outfolder)

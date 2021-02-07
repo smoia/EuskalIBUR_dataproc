6550e3e743ad97cb49a91cc930c0b53bddd528be
Stefano Moia <s.moia@bcbl.eu>
2021-02-07 16:53:10 +0100

    New run

--- 98.hcp/batch_pipeline.sh
+++ 98.hcp/batch_pipeline.sh
@@ -54,18 +54,7 @@ fi
 # 	joblist=${joblist::-1}
 # done
 
-# # Run fALFF
-# for sub in 001 002 003 004 007 008 009
-# do
-# 	for ses in $(seq -f %02g 1 10)
-# 	do
-# 		rm ${wdr}/../LogFiles/${sub}_${ses}_falff_pipe
-# 		qsub -q short.q -N "falff_${sub}_${ses}_EuskalIBUR" \
-# 		-o ${wdr}/../LogFiles/${sub}_${ses}_falff_pipe \
-# 		-e ${wdr}/../LogFiles/${sub}_${ses}_falff_pipe \
-# 		${wdr}/98.hcp/run_falff.sh ${sub} ${ses}
-# 	done
-# done
+
 
 
 # joblist=""
@@ -85,61 +74,19 @@ fi
 
 # joblist=${joblist::-1}
 
-for map in cvr lag
-do
-	rm ${wdr}/../LogFiles/${map}_surr_pipe
-	qsub -q long.q -N "surr_${map}_EuskalIBUR" \
-	-o ${wdr}/../LogFiles/${map}_surr_pipe \
-	-e ${wdr}/../LogFiles/${map}_surr_pipe \
-	${wdr}/98.hcp/run_surrogate_icc.sh ${map}
-done
-	# -hold_jid "${joblist}" \
-
-
-
-# for ses in $(seq -f %02g 1 10)
+# for map in cvr lag
 # do
-# 	rm ${wdr}/../LogFiles/001_${ses}_pipe
-# 	qsub -q long.q -N "s_001_${ses}_EuskalIBUR" \
-# 	-o ${wdr}/../LogFiles/001_${ses}_pipe \
-# 	-e ${wdr}/../LogFiles/001_${ses}_pipe \
-# 	${wdr}/98.hcp/run_subject_pipeline.sh 001 ${ses}
-# 	joblist=${joblist}s_001_${ses}_EuskalIBUR,
+# 	rm ${wdr}/../LogFiles/${map}_surr_pipe
+# 	qsub -q long.q -N "surr_${map}_EuskalIBUR" \
+# 	-o ${wdr}/../LogFiles/${map}_surr_pipe \
+# 	-e ${wdr}/../LogFiles/${map}_surr_pipe \
+# 	${wdr}/98.hcp/run_surrogate_icc.sh ${map}
 # done
+# 	# -hold_jid "${joblist}" \
 
-# joblist=${joblist::-1}
 
-# for sub in 004 007 008 009  # 002 003 004 007 008 009
-# do
-# 	for ses in $(seq -f %02g 1 10)
-# 	do
-# 		rm ${wdr}/../LogFiles/${sub}_${ses}_pipe
-# 		qsub -q long.q -N "s_${sub}_${ses}_EuskalIBUR" \
-# 		-o ${wdr}/../LogFiles/${sub}_${ses}_pipe \
-# 		-e ${wdr}/../LogFiles/${sub}_${ses}_pipe \
-# 		${wdr}/98.hcp/run_subject_pipeline.sh ${sub} ${ses}
-# 		# -hold_jid "${joblist}" \
-# 	done
-# 	joblist=""
-# 	for ses in $(seq -f %02g 1 10)
-# 	do
-# 		joblist=${joblist}s_${sub}_${ses}_EuskalIBUR,
-# 	done
-# 	joblist=${joblist::-1}
-# done
-
-# ### Plot pipeline for subjects parallel
-# rm ${wdr}/../LogFiles/plot_cvrval_pipe
-# qsub -q short.q -N "plot_EuskalIBUR" \
-# -o ${wdr}/../LogFiles/plot_cvrval_pipe \
-# -e ${wdr}/../LogFiles/plot_cvrval_pipe \
-# ${wdr}/98.hcp/run_plot_pipeline.sh
-
-# ### Third level pipeline
-# rm ${wdr}/../LogFiles/third_level_pipe
-# qsub -q long.q -N "third_level_EuskalIBUR" \
-# -o ${wdr}/../LogFiles/third_level_pipe \
-# -e ${wdr}/../LogFiles/third_level_pipe \
-# ${wdr}/98.hcp/run_third_level_pipe.sh
-# # -hold_jid "${joblist}" \
-# # -hold_jid "${old_ftype}_EuskalIBUR" \
+rm ${wdr}/../LogFiles/simple_icc_t_pipe
+qsub -q long.q -N "simple_icc_t_EuskalIBUR" \
+-o ${wdr}/../LogFiles/simple_icc_t_pipe \
+-e ${wdr}/../LogFiles/simple_icc_t_pipe \
+${wdr}/98.hcp/run_cvr_reliability.sh optcom

5860c7e35e1c8667b9abe2648232333f34e518a9
Stefano Moia <s.moia@bcbl.eu>
2021-02-19 19:54:26 +0100

    Remove transposed check

--- 04.first_level_analysis/08.run_task_glm.sh
+++ 04.first_level_analysis/08.run_task_glm.sh
@@ -56,19 +56,19 @@ run3dDeconvolve="${run3dDeconvolve} -ortvec ${fdir}/${flpr}_echo-1_bold_mcf_deme
 run3dDeconvolve="${run3dDeconvolve} -ortvec ${fdir}/${flpr}_echo-1_bold_mcf_deriv1.par motdemean"
 run3dDeconvolve="${run3dDeconvolve} -ortvec ${fdir}/${flpr}_concat_bold_bet_rej_ort.1D meica_rej_ort"
 run3dDeconvolve="${run3dDeconvolve} -bucket ${cbuck}.nii.gz"
-run3dDeconvolve="${run3dDeconvolve} -x1D ${mat}_tpd.1D"
+run3dDeconvolve="${run3dDeconvolve} -x1D ${mat}.1D"
 run3dDeconvolve="${run3dDeconvolve} -x1D_stop"
 
 case ${task} in
 	motor )
 		# Motor has five "actions" and a star fixation equivalent to a sham
 		run3dDeconvolve="${run3dDeconvolve} -num_stimts 6"
-		run3dDeconvolve="${run3dDeconvolve} -stim_label 1 finger_left -stim_times 1 ${ospr}_finger_left_onset_tpd.1D 'SPMG1(15)'"
-		run3dDeconvolve="${run3dDeconvolve} -stim_label 2 finger_right -stim_times 2 ${ospr}_finger_right_onset_tpd.1D 'SPMG1(15)'"
-		run3dDeconvolve="${run3dDeconvolve} -stim_label 3 toe_left -stim_times 3 ${ospr}_toe_left_onset_tpd.1D 'SPMG1(15)'"
-		run3dDeconvolve="${run3dDeconvolve} -stim_label 4 toe_right -stim_times 4 ${ospr}_toe_right_onset_tpd.1D 'SPMG1(15)'"
-		run3dDeconvolve="${run3dDeconvolve} -stim_label 5 tongue -stim_times 5 ${ospr}_tongue_onset_tpd.1D 'SPMG1(15)'"
-		run3dDeconvolve="${run3dDeconvolve} -stim_label 6 sham -stim_times 6 ${ospr}_star_onset_tpd.1D 'SPMG1(15)'"
+		run3dDeconvolve="${run3dDeconvolve} -stim_label 1 finger_left -stim_times 1 ${ospr}_finger_left_onset.1D 'SPMG1(15)'"
+		run3dDeconvolve="${run3dDeconvolve} -stim_label 2 finger_right -stim_times 2 ${ospr}_finger_right_onset.1D 'SPMG1(15)'"
+		run3dDeconvolve="${run3dDeconvolve} -stim_label 3 toe_left -stim_times 3 ${ospr}_toe_left_onset.1D 'SPMG1(15)'"
+		run3dDeconvolve="${run3dDeconvolve} -stim_label 4 toe_right -stim_times 4 ${ospr}_toe_right_onset.1D 'SPMG1(15)'"
+		run3dDeconvolve="${run3dDeconvolve} -stim_label 5 tongue -stim_times 5 ${ospr}_tongue_onset.1D 'SPMG1(15)'"
+		run3dDeconvolve="${run3dDeconvolve} -stim_label 6 sham -stim_times 6 ${ospr}_star_onset.1D 'SPMG1(15)'"
 		# Two GLT are coded: all motor activations, and all motor activations against the sham to remove visual stimuli"
 		run3dDeconvolve="${run3dDeconvolve} -gltsym 'SYM: +finger_left +finger_right +toe_left +toe_right +tongue"
 		run3dDeconvolve="${run3dDeconvolve} \\ +finger_left +finger_right +toe_left +toe_right +tongue -sham'"
@@ -138,13 +138,13 @@ esac
 echo "${run3dDeconvolve}"
 eval ${run3dDeconvolve}
 
-3dREMLfit -overwrite -matrix ${mat}_tpd.1D \
+3dREMLfit -overwrite -matrix ${mat}.1D \
 		  -mask ${mask}.nii.gz         \
 		  -input ${tmp}/${flpr}_spc.nii.gz        \
 		  -tout -verb -GOFORIT         \
-		  -Rfitts ${fitts}_tpd.nii.gz      \
-		  -Rbuck  ${rbuck}_tpd.nii.gz      \
-		  -Rerrts ${ertts}_tpd.nii.gz
+		  -Rfitts ${fitts}.nii.gz      \
+		  -Rbuck  ${rbuck}.nii.gz      \
+		  -Rerrts ${ertts}.nii.gz
 
 # Run 3dDeconvolve on a trial-by-trial basis with the stim_IM option
 echo "Compute statistical maps of activation per individual event ocurrence"
@@ -156,18 +156,18 @@ run3dDeconvolve="${run3dDeconvolve} -ortvec ${fdir}/${flpr}_echo-1_bold_mcf_deme
 run3dDeconvolve="${run3dDeconvolve} -ortvec ${fdir}/${flpr}_echo-1_bold_mcf_deriv1.par motdemean"
 run3dDeconvolve="${run3dDeconvolve} -ortvec ${fdir}/${flpr}_concat_bold_bet_rej_ort.1D meica_rej_ort"
 run3dDeconvolve="${run3dDeconvolve} -bucket ${cbuck}-IM.nii.gz"
-run3dDeconvolve="${run3dDeconvolve} -x1D ${mat}-IM_tpd.1D"
+run3dDeconvolve="${run3dDeconvolve} -x1D ${mat}-IM.1D"
 run3dDeconvolve="${run3dDeconvolve} -x1D_stop"
 
 
 case ${task} in
 	motor )
 		run3dDeconvolve="${run3dDeconvolve} -num_stimts 5"
-		run3dDeconvolve="${run3dDeconvolve} -stim_label 1 finger_left -stim_times_IM 1 ${ospr}_finger_left_onset_tpd.1D 'SPMG1(15)'"
-		run3dDeconvolve="${run3dDeconvolve} -stim_label 2 finger_right -stim_times_IM 2 ${ospr}_finger_right_onset_tpd.1D 'SPMG1(15)'"
-		run3dDeconvolve="${run3dDeconvolve} -stim_label 3 toe_left -stim_times_IM 3 ${ospr}_toe_left_onset_tpd.1D 'SPMG1(15)'"
-		run3dDeconvolve="${run3dDeconvolve} -stim_label 4 toe_right -stim_times_IM 4 ${ospr}_toe_right_onset_tpd.1D 'SPMG1(15)'"
-		run3dDeconvolve="${run3dDeconvolve} -stim_label 5 tongue -stim_times_IM 5 ${ospr}_tongue_onset_tpd.1D 'SPMG1(15)'"
+		run3dDeconvolve="${run3dDeconvolve} -stim_label 1 finger_left -stim_times_IM 1 ${ospr}_finger_left_onset.1D 'SPMG1(15)'"
+		run3dDeconvolve="${run3dDeconvolve} -stim_label 2 finger_right -stim_times_IM 2 ${ospr}_finger_right_onset.1D 'SPMG1(15)'"
+		run3dDeconvolve="${run3dDeconvolve} -stim_label 3 toe_left -stim_times_IM 3 ${ospr}_toe_left_onset.1D 'SPMG1(15)'"
+		run3dDeconvolve="${run3dDeconvolve} -stim_label 4 toe_right -stim_times_IM 4 ${ospr}_toe_right_onset.1D 'SPMG1(15)'"
+		run3dDeconvolve="${run3dDeconvolve} -stim_label 5 tongue -stim_times_IM 5 ${ospr}_tongue_onset.1D 'SPMG1(15)'"
 	;;
 	simon )
 		run3dDeconvolve="${run3dDeconvolve} -num_stimts 8"
@@ -199,12 +199,12 @@ esac
 
 # Run 3dDeconvolve
 eval ${run3dDeconvolve}
-3dREMLfit -overwrite -matrix ${mat}_tpd.1D	\
+3dREMLfit -overwrite -matrix ${mat}.1D	\
 		  -mask	 ${mask}.nii.gz \
 		  -input ${tmp}/${flpr}_spc.nii.gz	\
 		  -tout -verb -GOFORIT				  \
-		  -Rfitts ${fitts}-IM_tpd.nii.gz		 \
-		  -Rbuck  ${rbuck}-IM_tpd.nii.gz
+		  -Rfitts ${fitts}-IM.nii.gz		 \
+		  -Rbuck  ${rbuck}-IM.nii.gz
 
 # rm ${tmp}/${flpr}_spc.nii.gz ${tmp}/${flpr}_mean.nii.gz
 

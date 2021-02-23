b6a42531258aa9a0eb8ee4987a617f098df4dc51
Stefano Moia <s.moia@bcbl.eu>
2021-02-23 21:46:04 +0100

    Complete correlation for all maps

--- 05.second_level_analysis/06.quick_correlation_Mennes.sh
+++ 05.second_level_analysis/06.quick_correlation_Mennes.sh
@@ -53,12 +53,13 @@ if_missing_do mkdir ${tmp}/CVR
 for ses in $( seq -f %02g 1 10; echo "allses" )
 do
 	# Consider only contrasts of interest
+	rbuck=GLM/${task}/output/${sub}_${ses}_task-${task}_spm.nii.gz
 	case ${task} in
 		motor )
 			# Two GLT are coded: all motor activations, and all motor activations against the sham to remove visual stimuli"
 			3dbucket -prefix ${tmp}/${sub}_allmotors_${ses}.nii.gz -abuc ${rbuck}'[13]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_allmotors_vs_sham_${ses}.nii.gz -abuc ${rbuck}'[15]' -overwrite
-			fslmerge 
+			3dbucket -prefix ${tmp}/${sub}_motors_vs_sham_${ses}.nii.gz -abuc ${rbuck}'[15]' -overwrite
+			bricks=$( allmotors motors_vs_sham )
 		;;
 		simon )
 			# Four GLTs are coded, good congruents, good incongruents, good congruents vs good incongruents and good congruents + good incongruents
@@ -66,56 +67,58 @@ do
 			3dbucket -prefix ${tmp}/${sub}_all_incongruent_${ses}.nii.gz -abuc ${rbuck}'[19]' -overwrite
 			3dbucket -prefix ${tmp}/${sub}_congruent_vs_incongruent_${ses}.nii.gz -abuc ${rbuck}'[21]' -overwrite
 			3dbucket -prefix ${tmp}/${sub}_congruent_and_incongruent_${ses}.nii.gz -abuc ${rbuck}'[23]' -overwrite
+			bricks=$( all_congruent all_incongruent congruent_vs_incongruent congruent_and_incongruent )
 		;;
 		* ) echo "    !!! Warning !!! Invalid task: ${task}"; exit ;;
 	esac
-done
 
-mask=${wdr}/sub-${sub}/ses-01/reg/sub-${sub}_sbref_brain_mask
-if_missing_do copy ${mask}.nii.gz ${tmp}/${sub}_${ses}_mask.nii.gz
-for ses in $(seq -f %02g 1 10)
-do
 	# Copy CVR maps
 	cvrmap=${wdr}/CVR/sub-${sub}_ses-${ses}_optcom_map_cvr/sub-${sub}_ses-${ses}_optcom_cvr.nii.gz
 	if_missing_do copy ${cvrmap} CVR/${sub}_${ses}_cvr.nii.gz
 	if_missing_do copy CVR/${sub}_${ses}_cvr.nii.gz ${tmp}/CVR/${sub}_${ses}.nii.gz
 
 	# Copy RSFA & FALFF maps
-	if_missing_do copy fALFF/sub-${sub}_ses-${ses}_task-rest_run-01_fALFF.nii.gz ${tmp}/fALFF_${sub}_${ses}.nii.gz
-	if_missing_do copy RSFA/sub-${sub}_ses-${ses}_task-rest_run-01_RSFA.nii.gz ${tmp}/RSFA_${sub}_${ses}.nii.gz
-
-	rbuck=GLM/${task}/output/${sub}_${ses}_task-${task}_spm.nii.gz
-	for brick in $(seq 0 ${lastbrick})
+	for run in $( seq -f %02g 1 4 )
 	do
-		# Break bricks
-		brickname=$( 3dinfo -verb ${rbuck} | grep 'brick #${brick} ' | awk -F "'" '{ print $2 }' )
-		3dbucket -prefix ${tmp}/${brickname}_${sub}_${ses}.nii.gz -abuc ${rbuck}'[${brick}]' -overwrite
+		if_missing_do copy fALFF/sub-${sub}_ses-${ses}_task-rest_run-${run}_fALFF.nii.gz ${tmp}/fALFF_${sub}_r${run}_${ses}.nii.gz
+		if_missing_do copy RSFA/sub-${sub}_ses-${ses}_task-rest_run-${run}_RSFA.nii.gz ${tmp}/RSFA_${sub}_r${run}_${ses}.nii.gz
 	done
 done
+
+mask=${wdr}/sub-${sub}/ses-01/reg/sub-${sub}_sbref_brain_mask
+if_missing_do copy ${mask}.nii.gz ${tmp}/${sub}_${ses}_mask.nii.gz
 # Merge CVR
-fslmerge -t ${tmp}/${sub}_cvr ${tmp}/${task}/CVR/${sub}*
+fslmerge -t ${tmp}/${sub}_cvr ${tmp}/CVR/${sub}*
 # Merge RSFA
-fslmerge -t ${tmp}/${task}/${sub}_fALFF ${tmp}/${task}/fALFF_${sub}*
-fslmerge -t ${tmp}/${task}/${sub}_RSFA ${tmp}/${task}/RSFA_${sub}*
-for brick in $(seq 0 ${lastbrick})
+for run in $( seq -f %02g 1 4 )
+do
+	fslmerge -t ${tmp}/${sub}_r${run}_fALFF ${tmp}/fALFF_${sub}_r${run}*
+	fslmerge -t ${tmp}/${sub}_r${run}_RSFA ${tmp}/RSFA_${sub}_r${run}*
+done
+
+for brick in "${bricks[@]}"
 do
 	# Merge bricks
-	brickname=$( 3dinfo -verb ${rbuck} | grep 'brick #${brick} ' | awk -F "'" '{ print $2 }' )
-	fslmerge -t ${tmp}/${task}/${sub}_${brickname} ${tmp}/${task}/${brickname}_${sub}*
+	fslmerge -t ${tmp}/${sub}_${brick} ${tmp}/${sub}_${brick}_*
+	
+	3dTcorrelate -pearson -polort -1 -prefix corr/${sub}_${task}_${brick}_cvr.nii.gz \
+				 ${tmp}/${sub}_${brick}.nii.gz ${tmp}/${sub}_cvr.nii.gz -overwrite
+
 	# Compute correlations
-	for map in cvr fALFF RSFA
+	for map in fALFF RSFA
 	do
-		3dTcorrelate -pearson -polort -1 -prefix corr/${sub}_${map}_${task}_${brickname}.nii.gz \
-					 ${tmp}/${task}/${sub}_${brickname}.nii.gz ${tmp}/${task}/${sub}_${map}.nii.gz -overwrite
-		if [ ${map} != "cvr" ]
-		then
-			3dTcorrelate -pearson -polort -1 -partial ${tmp}/${task}/${sub}_cvr.nii.gz\
-						 -prefix parcorr/${sub}_${map}_${task}_${brickname}.nii.gz \
-						 ${tmp}/${task}/${sub}_${brickname}.nii.gz ${tmp}/${task}/${sub}_${map}.nii.gz -overwrite
-		fi
+		for run in $( seq -f %02g 1 4 )
+		do
+			3dTcorrelate -pearson -polort -1 -prefix corr/${sub}_${task}_${brick}_${map}_r${run}.nii.gz \
+						 ${tmp}/${sub}_${brick}.nii.gz ${tmp}/${sub}_r${run}_${map}.nii.gz -overwrite
+
+			3dTcorrelate -pearson -polort -1 -partial ${tmp}/${sub}_cvr.nii.gz\
+						 -prefix parcorr/${sub}_${task}_${brick}_${map}_r${run}.nii.gz \
+						 ${tmp}/${sub}_${brick}.nii.gz ${tmp}/${sub}_r${run}_${map}.nii.gz -overwrite
+		done
 	done
 done
 
-rm -rf ${tmp}/${task}
+rm -rf ${tmp}
 
 cd ${cwd}

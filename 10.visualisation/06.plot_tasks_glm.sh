69caa6b75a47d088fbc4d714e00fde9cfedabb8e
Stefano Moia <s.moia@bcbl.eu>
2021-02-26 11:32:51 +0100

    Try one index more

--- 10.visualisation/06.plot_tasks_glm.sh
+++ 10.visualisation/06.plot_tasks_glm.sh
@@ -52,89 +52,89 @@ replace_and mkdir ${tmp}
 # Set the T1 in native space
 bckimg=${wdr}/sub-${sub}/ses-01/reg/sub-${sub}_ses-01_T2w2sub-${sub}_sbref
 
-for sfx in spm spm-IM
-do
-	# Check number of bricks and remove one cause 0-index
-	lastbrick=$( fslval ${sub}_01_task-${task}_${sfx} dim5 )
-	let lastbrick--
-	for ses in $( seq -f %02g 1 ${lastses}; echo "allses" )
-	do
-		rbuck=${sub}_${ses}_task-${task}_${sfx}.nii.gz
-		if [ ! -e ${rbuck} ]; then continue; else echo "Exploding ${rbuck}"; fi
-		for brick in $(seq 0 ${lastbrick})
-		do
-			# Break bricks
-			brickname=$( 3dinfo -verb ${rbuck} | grep 'brick #${brick} ' | awk -F "'" '{ print $2 }' )
-			3dbucket -prefix ${tmp}/${sub}_${ses}_${sfx}_${brickname}.nii.gz -abuc "${rbuck}[${brick}]" -overwrite
-		done
-	done
-done
+# for sfx in spm spm-IM
+# do
+# 	# Check number of bricks and remove one cause 0-index
+# 	lastbrick=$( fslval ${sub}_01_task-${task}_${sfx} dim5 )
+# 	let lastbrick--
+# 	for ses in $( seq -f %02g 1 ${lastses}; echo "allses" )
+# 	do
+# 		rbuck=${sub}_${ses}_task-${task}_${sfx}.nii.gz
+# 		if [ ! -e ${rbuck} ]; then continue; else echo "Exploding ${rbuck}"; fi
+# 		for brick in $(seq 0 ${lastbrick})
+# 		do
+# 			# Break bricks
+# 			brickname=$( 3dinfo -verb ${rbuck} | grep 'brick #${brick} ' | awk -F "'" '{ print $2 }' )
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_${sfx}_${brickname}.nii.gz -abuc "${rbuck}[${brick}]" -overwrite
+# 		done
+# 	done
+# done
 
-# Until it is not fixed, correctly rename GLT contrasts bricks
-for ses in $( seq -f %02g 1 ${lastses}; echo "allses" )
-do
-	rbuck=${sub}_${ses}_task-${task}_${sfx}.nii.gz
-	if [ ! -e ${rbuck} ]; then continue; else echo "Exploding ${rbuck}"; fi
-	case ${task} in
-		motor )
-			rm ${tmp}/${sub}_${ses}_spm_*#1*.nii.gz
-			# Two GLT are coded: all motor activations, and all motor activations against the sham to remove visual stimuli"
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_allmotors#0_Coef.nii.gz -abuc ${rbuck}'[13]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_allmotors#0_Tstat.nii.gz -abuc ${rbuck}'[14]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_allmotors_vs_sham#0_Coef.nii.gz -abuc ${rbuck}'[15]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_allmotors_vs_sham#0_Tstat.nii.gz -abuc ${rbuck}'[16]' -overwrite
-		;;
-		simon )
-			rm ${tmp}/${sub}_${ses}_spm_*#1*.nii.gz ${tmp}/${sub}_${ses}_spm_*#2*.nii.gz ${tmp}/${sub}_${ses}_spm_*#3*.nii.gz
-			# Four GLTs are coded, good congruents, good incongruents, good congruents vs good incongruents and good congruents + good incongruents
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_congruent#0_Coef.nii.gz -abuc ${rbuck}'[17]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_congruent#0_Tstat.nii.gz -abuc ${rbuck}'[18]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_incongruent#0_Coef.nii.gz -abuc ${rbuck}'[19]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_incongruent#0_Tstat.nii.gz -abuc ${rbuck}'[20]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_congruent_vs_incongruent#0_Coef.nii.gz -abuc ${rbuck}'[21]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_congruent_vs_incongruent#0_Tstat.nii.gz -abuc ${rbuck}'[22]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_congruent_and_incongruent#0_Coef.nii.gz -abuc ${rbuck}'[23]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_congruent_and_incongruent#0_Tstat.nii.gz -abuc ${rbuck}'[24]' -overwrite
-		;;
-		pinel )
-			rm ${tmp}/${sub}_${ses}_spm_*#1*.nii.gz ${tmp}/${sub}_${ses}_spm_*#2*.nii.gz ${tmp}/${sub}_${ses}_spm_*#3*.nii.gz
-			rm ${tmp}/${sub}_${ses}_spm_*#4*.nii.gz ${tmp}/${sub}_${ses}_spm_*#5*.nii.gz ${tmp}/${sub}_${ses}_spm_*#6*.nii.gz
-			rm ${tmp}/${sub}_${ses}_spm_*#7*.nii.gz ${tmp}/${sub}_${ses}_spm_*#8*.nii.gz ${tmp}/${sub}_${ses}_spm_*#9*.nii.gz
-			rm ${tmp}/${sub}_${ses}_spm_*#10*.nii.gz ${tmp}/${sub}_${ses}_spm_*#11*.nii.gz ${tmp}/${sub}_${ses}_spm_*#12*.nii.gz
-			rm ${tmp}/${sub}_${ses}_spm_*#13*.nii.gz
-			# Pinel models 8 contrasts: right vs left hand, vertical vs horizontal checkers, auditory stims, visual stims, 
-			# auditory calc vs auditory noncalc, visual calc vs visual noncalc, auditory vs visual, visual vs checkerboards.
-			# Added all sentences, all motor, all calculus, all motor vs sentences, all calculus vs sentences.
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_right_vs_left#0_Coef.nii.gz -abuc ${rbuck}'[21]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_right_vs_left#0_Tstat.nii.gz -abuc ${rbuck}'[22]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_vertical_vs_horizontal_cb#0_Coef.nii.gz -abuc ${rbuck}'[23]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_vertical_vs_horizontal_cb#0_Tstat.nii.gz -abuc ${rbuck}'[24]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_auditory#0_Coef.nii.gz -abuc ${rbuck}'[25]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_auditory#0_Tstat.nii.gz -abuc ${rbuck}'[26]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_visual#0_Coef.nii.gz -abuc ${rbuck}'[27]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_visual#0_Tstat.nii.gz -abuc ${rbuck}'[28]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_auditory_calc_vs_noncalc#0_Coef.nii.gz -abuc ${rbuck}'[29]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_auditory_calc_vs_noncalc#0_Tstat.nii.gz -abuc ${rbuck}'[30]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_visual_calc_vs_noncalc#0_Coef.nii.gz -abuc ${rbuck}'[31]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_visual_calc_vs_noncalc#0_Tstat.nii.gz -abuc ${rbuck}'[32]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_auditory_vs_visual#0_Coef.nii.gz -abuc ${rbuck}'[33]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_auditory_vs_visual#0_Tstat.nii.gz -abuc ${rbuck}'[34]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_visual_vs_cb#0_Coef.nii.gz -abuc ${rbuck}'[35]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_visual_vs_cb#0_Tstat.nii.gz -abuc ${rbuck}'[36]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_sentences#0_Coef.nii.gz -abuc ${rbuck}'[37]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_sentences#0_Tstat.nii.gz -abuc ${rbuck}'[38]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_motor#0_Coef.nii.gz -abuc ${rbuck}'[39]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_motor#0_Tstat.nii.gz -abuc ${rbuck}'[40]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_calc#0_Coef.nii.gz -abuc ${rbuck}'[41]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_calc#0_Tstat.nii.gz -abuc ${rbuck}'[42]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_motor_vs_sentences#0_Coef.nii.gz -abuc ${rbuck}'[43]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_motor_vs_sentences#0_Tstat.nii.gz -abuc ${rbuck}'[44]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_calc_vs_sentences#0_Coef.nii.gz -abuc ${rbuck}'[45]' -overwrite
-			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_calc_vs_sentences#0_Tstat.nii.gz -abuc ${rbuck}'[46]' -overwrite
-		;;
-		* ) echo "    !!! Warning !!! Invalid task: ${task}"; exit ;;
-	esac
-done
+# # Until it is not fixed, correctly rename GLT contrasts bricks
+# for ses in $( seq -f %02g 1 ${lastses}; echo "allses" )
+# do
+# 	rbuck=${sub}_${ses}_task-${task}_${sfx}.nii.gz
+# 	if [ ! -e ${rbuck} ]; then continue; else echo "Exploding ${rbuck}"; fi
+# 	case ${task} in
+# 		motor )
+# 			rm ${tmp}/${sub}_${ses}_spm_*#1*.nii.gz
+# 			# Two GLT are coded: all motor activations, and all motor activations against the sham to remove visual stimuli"
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_allmotors#0_Coef.nii.gz -abuc ${rbuck}'[13]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_allmotors#0_Tstat.nii.gz -abuc ${rbuck}'[14]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_allmotors_vs_sham#0_Coef.nii.gz -abuc ${rbuck}'[15]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_allmotors_vs_sham#0_Tstat.nii.gz -abuc ${rbuck}'[16]' -overwrite
+# 		;;
+# 		simon )
+# 			rm ${tmp}/${sub}_${ses}_spm_*#1*.nii.gz ${tmp}/${sub}_${ses}_spm_*#2*.nii.gz ${tmp}/${sub}_${ses}_spm_*#3*.nii.gz
+# 			# Four GLTs are coded, good congruents, good incongruents, good congruents vs good incongruents and good congruents + good incongruents
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_congruent#0_Coef.nii.gz -abuc ${rbuck}'[17]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_congruent#0_Tstat.nii.gz -abuc ${rbuck}'[18]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_incongruent#0_Coef.nii.gz -abuc ${rbuck}'[19]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_incongruent#0_Tstat.nii.gz -abuc ${rbuck}'[20]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_congruent_vs_incongruent#0_Coef.nii.gz -abuc ${rbuck}'[21]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_congruent_vs_incongruent#0_Tstat.nii.gz -abuc ${rbuck}'[22]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_congruent_and_incongruent#0_Coef.nii.gz -abuc ${rbuck}'[23]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_congruent_and_incongruent#0_Tstat.nii.gz -abuc ${rbuck}'[24]' -overwrite
+# 		;;
+# 		pinel )
+# 			rm ${tmp}/${sub}_${ses}_spm_*#1*.nii.gz ${tmp}/${sub}_${ses}_spm_*#2*.nii.gz ${tmp}/${sub}_${ses}_spm_*#3*.nii.gz
+# 			rm ${tmp}/${sub}_${ses}_spm_*#4*.nii.gz ${tmp}/${sub}_${ses}_spm_*#5*.nii.gz ${tmp}/${sub}_${ses}_spm_*#6*.nii.gz
+# 			rm ${tmp}/${sub}_${ses}_spm_*#7*.nii.gz ${tmp}/${sub}_${ses}_spm_*#8*.nii.gz ${tmp}/${sub}_${ses}_spm_*#9*.nii.gz
+# 			rm ${tmp}/${sub}_${ses}_spm_*#10*.nii.gz ${tmp}/${sub}_${ses}_spm_*#11*.nii.gz ${tmp}/${sub}_${ses}_spm_*#12*.nii.gz
+# 			rm ${tmp}/${sub}_${ses}_spm_*#13*.nii.gz
+# 			# Pinel models 8 contrasts: right vs left hand, vertical vs horizontal checkers, auditory stims, visual stims, 
+# 			# auditory calc vs auditory noncalc, visual calc vs visual noncalc, auditory vs visual, visual vs checkerboards.
+# 			# Added all sentences, all motor, all calculus, all motor vs sentences, all calculus vs sentences.
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_right_vs_left#0_Coef.nii.gz -abuc ${rbuck}'[21]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_right_vs_left#0_Tstat.nii.gz -abuc ${rbuck}'[22]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_vertical_vs_horizontal_cb#0_Coef.nii.gz -abuc ${rbuck}'[23]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_vertical_vs_horizontal_cb#0_Tstat.nii.gz -abuc ${rbuck}'[24]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_auditory#0_Coef.nii.gz -abuc ${rbuck}'[25]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_auditory#0_Tstat.nii.gz -abuc ${rbuck}'[26]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_visual#0_Coef.nii.gz -abuc ${rbuck}'[27]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_visual#0_Tstat.nii.gz -abuc ${rbuck}'[28]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_auditory_calc_vs_noncalc#0_Coef.nii.gz -abuc ${rbuck}'[29]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_auditory_calc_vs_noncalc#0_Tstat.nii.gz -abuc ${rbuck}'[30]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_visual_calc_vs_noncalc#0_Coef.nii.gz -abuc ${rbuck}'[31]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_visual_calc_vs_noncalc#0_Tstat.nii.gz -abuc ${rbuck}'[32]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_auditory_vs_visual#0_Coef.nii.gz -abuc ${rbuck}'[33]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_auditory_vs_visual#0_Tstat.nii.gz -abuc ${rbuck}'[34]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_visual_vs_cb#0_Coef.nii.gz -abuc ${rbuck}'[35]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_visual_vs_cb#0_Tstat.nii.gz -abuc ${rbuck}'[36]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_sentences#0_Coef.nii.gz -abuc ${rbuck}'[37]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_sentences#0_Tstat.nii.gz -abuc ${rbuck}'[38]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_motor#0_Coef.nii.gz -abuc ${rbuck}'[39]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_motor#0_Tstat.nii.gz -abuc ${rbuck}'[40]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_calc#0_Coef.nii.gz -abuc ${rbuck}'[41]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_calc#0_Tstat.nii.gz -abuc ${rbuck}'[42]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_motor_vs_sentences#0_Coef.nii.gz -abuc ${rbuck}'[43]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_motor_vs_sentences#0_Tstat.nii.gz -abuc ${rbuck}'[44]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_calc_vs_sentences#0_Coef.nii.gz -abuc ${rbuck}'[45]' -overwrite
+# 			3dbucket -prefix ${tmp}/${sub}_${ses}_spm_all_calc_vs_sentences#0_Tstat.nii.gz -abuc ${rbuck}'[46]' -overwrite
+# 		;;
+# 		* ) echo "    !!! Warning !!! Invalid task: ${task}"; exit ;;
+# 	esac
+# done
 
 for sfx in spm spm-IM
 do
@@ -146,7 +146,7 @@ do
 		ndof=$( 3dinfo -verb ${rbuck} | grep statpar | awk -F " = " '{ print $3 }' )
 		printf "%s " "DoF found:" "${ndof[@]}"
 		echo ""
-		dof="${ndof[2]}"
+		dof="${ndof[3]}"
 		echo "DoF selected: ${dof}"
 		thr=$( cdf -p2t fitt ${pval} ${dof} )
 		echo "thr: ${thr}"
@@ -178,6 +178,6 @@ done
 
 mv ${tmp}/*.png ${picdir}/.
 
-rm -rf ${tmp}/${task}
+# rm -rf ${tmp}/${task}
 
 cd ${cwd}

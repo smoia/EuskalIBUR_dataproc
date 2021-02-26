63f7315891f962dc2d6d5ee26149ba5757369953
Stefano Moia <s.moia@bcbl.eu>
2021-02-26 20:34:34 +0100

    Revert "VERBOSE"
    This reverts commit b6f011fd219163d41505d84a4d9b9fd2ebc1ddc7.

--- 10.visualisation/06.plot_tasks_glm.sh
+++ 10.visualisation/06.plot_tasks_glm.sh
@@ -27,16 +27,15 @@ esac
 
 slice_coeffs() {
 # $1:brickname $2:bckimg $3:[p/q] $4:pval $5:picdir
-set -x
-fsleyes render --verbose --verbose --verbose -of ${1}_tmp_axial.png --size 1900 200 \
+fsleyes render -of ${1}_tmp_axial.png --size 1900 200 \
 --scene lightbox --zaxis 2 --sliceSpacing 12 --zrange 19.3 139.9 --ncols 10 --nrows 1 --hideCursor --showColourBar --colourBarLocation right --colourBarLabelSide bottom-right --colourBarSize 80.0 --labelSize 12 --performance 3 --movieSync \
 ${2}.nii.gz --name "anat" --overlayType volume --alpha 100.0 --brightness 49.75000000000001 --contrast 49.90029860765409 --cmap greyscale --negativeCmap greyscale --displayRange 0.0 631.9035656738281 --clippingRange 0.0 631.9035656738281 --modulateRange 0.0 625.6470947265625 --gamma 0.0 --cmapResolution 256 --interpolation none --invert --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0 \
 ${1}_fmkd.nii.gz --name "beta" --overlayType volume --alpha 100.0 --cmap brain_colours_1hot --negativeCmap cool --useNegativeCmap --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
-fsleyes render --verbose --verbose --verbose -of ${1}_tmp_sagittal.png --size 1900 200 \
+fsleyes render -of ${1}_tmp_sagittal.png --size 1900 200 \
 --scene lightbox --zaxis 0 --sliceSpacing 13 --zrange 39.5 169 --ncols 10 --nrows 1 --hideCursor --showColourBar --colourBarLocation right --colourBarLabelSide bottom-right --colourBarSize 80.0 --labelSize 12 --performance 3 --movieSync \
 ${2}.nii.gz --name "anat" --overlayType volume --alpha 100.0 --brightness 49.75000000000001 --contrast 49.90029860765409 --cmap greyscale --negativeCmap greyscale --displayRange 0.0 631.9035656738281 --clippingRange 0.0 631.9035656738281 --modulateRange 0.0 625.6470947265625 --gamma 0.0 --cmapResolution 256 --interpolation none --invert --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0 \
 ${1}_fmkd.nii.gz --name "beta" --overlayType volume --alpha 100.0 --cmap brain_colours_1hot --negativeCmap cool --useNegativeCmap --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
-fsleyes render --verbose --verbose --verbose -of ${1}_tmp_coronal.png --size 1900 200 \
+fsleyes render -of ${1}_tmp_coronal.png --size 1900 200 \
 --scene lightbox --zaxis 1 --sliceSpacing 15 --zrange 21 169 --ncols 10 --nrows 1 --hideCursor --showColourBar --colourBarLocation right --colourBarLabelSide bottom-right --colourBarSize 80.0 --labelSize 12 --performance 3 --movieSync \
 ${2}.nii.gz --name "anat" --overlayType volume --alpha 100.0 --brightness 49.75000000000001 --contrast 49.90029860765409 --cmap greyscale --negativeCmap greyscale --displayRange 0.0 631.9035656738281 --clippingRange 0.0 631.9035656738281 --modulateRange 0.0 625.6470947265625 --gamma 0.0 --cmapResolution 256 --interpolation none --invert --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0 \
 ${1}_fmkd.nii.gz --name "beta" --overlayType volume --alpha 100.0 --cmap brain_colours_1hot --negativeCmap cool --useNegativeCmap --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
@@ -81,12 +80,12 @@ replace_and mkdir ${tmp}
 # Set the T1 in native space
 bckimg=${wdr}/sub-${sub}/ses-01/reg/sub-${sub}_ses-01_T2w2sub-${sub}_sbref
 
-for sfx in spm #spm-IM
+for sfx in spm spm-IM
 do
 	# Check number of bricks and remove one cause 0-index
 	lastbrick=$( fslval ${sub}_01_task-${task}_${sfx} dim5 )
 	let lastbrick--
-	for ses in $( seq -f %02g 1 ${lastses}) #; echo "allses" )
+	for ses in $( seq -f %02g 1 ${lastses}; echo "allses" )
 	do
 		rbuck=${sub}_${ses}_task-${task}_${sfx}.nii.gz
 		if [ ! -e ${rbuck} ]; then continue; else echo "Exploding ${rbuck}"; fi
@@ -100,7 +99,7 @@ do
 done
 
 # Until it is not fixed, correctly rename GLT contrasts bricks
-for ses in $( seq -f %02g 1 ${lastses}) #; echo "allses" )
+for ses in $( seq -f %02g 1 ${lastses}; echo "allses" )
 do
 	rbuck=${sub}_${ses}_task-${task}_${sfx}.nii.gz
 	if [ ! -e ${rbuck} ]; then continue; else echo "Exploding ${rbuck}"; fi
@@ -165,9 +164,9 @@ do
 	esac
 done
 
-for sfx in spm #spm-IM
+for sfx in spm spm-IM
 do
-	for ses in $( seq -f %02g 1 ${lastses}) #; echo "allses" )
+	for ses in $( seq -f %02g 1 ${lastses}; echo "allses" )
 	do
 		rbuck=${sub}_${ses}_task-${task}_${sfx}.nii.gz
 		[ ! -e ${rbuck} ] && continue || echo "Plot ${rbuck}"

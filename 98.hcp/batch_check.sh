4cea4bff61ee9999cc24675f1c2733ff5cd7a98f
Stefano Moia <s.moia@bcbl.eu>
2021-03-03 20:31:44 +0100

    New run

--- 98.hcp/batch_check.sh
+++ 98.hcp/batch_check.sh
@@ -24,7 +24,7 @@ do
 	for ses in $(seq -f %02g 1 10)
 	do
 		rm ${wdr}/../LogFiles/${sub}_${ses}_check
-		qsub -q long.q -N "s_${sub}_${ses}_check" \
+		qsub -q veryshort.q -N "s_${sub}_${ses}_check" \
 		-o ${wdr}/../LogFiles/${sub}_${ses}_check \
 		-e ${wdr}/../LogFiles/${sub}_${ses}_check \
 		${wdr}/98.hcp/run_check.sh ${sub} ${ses} /bcbl/home/public/PJMASK_2

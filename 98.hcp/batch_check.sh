5b6210b7359f6b0532cadc33bb951604518a254c
Stefano Moia <s.moia@bcbl.eu>
2021-03-03 20:22:01 +0100

    Checks

---
+++ 98.hcp/batch_check.sh
@@ -0,0 +1,32 @@
+#$ -S /bin/bash
+#$ -cwd
+#$ -m be
+#$ -M s.moia@bcbl.eu
+
+
+##########################################################################################################################
+##---START OF SCRIPT----------------------------------------------------------------------------------------------------##
+##########################################################################################################################
+
+date
+
+wdr=/bcbl/home/public/PJMASK_2/EuskalIBUR_dataproc
+
+cd ${wdr}
+
+if [[ ! -d ../LogFiles ]]
+then
+	mkdir ../LogFiles
+fi
+
+for sub in 001 002 003 004 007 008 009
+do
+	for ses in $(seq -f %02g 1 10)
+	do
+		rm ${wdr}/../LogFiles/${sub}_${ses}_check
+		qsub -q long.q -N "s_${sub}_${ses}_check" \
+		-o ${wdr}/../LogFiles/${sub}_${ses}_check \
+		-e ${wdr}/../LogFiles/${sub}_${ses}_check \
+		${wdr}/98.hcp/run_check.sh ${sub} ${ses} /bcbl/home/public/PJMASK_2
+	done
+done

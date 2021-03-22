17c942ac2f195a0dd62735fa58556b16fa27264c
Stefano Moia <s.moia@bcbl.eu>
2021-03-22 15:27:28 +0100

    New run

--- 98.hcp/batch_pipeline.sh
+++ 98.hcp/batch_pipeline.sh
@@ -68,38 +68,38 @@ fi
 # 	done
 # done
 
-# Run GLMs
-for sub in 001 002 003 004 007 008 009
-do
-	# for ses in $(seq -f %02g 1 10)
-	# do
-	# 	rm ${wdr}/../LogFiles/${sub}_${ses}_glm_pipe
-	# 	qsub -q long.q -N "glm_${sub}_${ses}_EuskalIBUR" \
-	# 	-o ${wdr}/../LogFiles/${sub}_${ses}_glm_pipe \
-	# 	-e ${wdr}/../LogFiles/${sub}_${ses}_glm_pipe \
-	# 	${wdr}/98.hcp/run_ses_glm.sh ${sub} ${ses}
-	# 	# -hold_jid "${joblist}" \
-	# done
-
-	rm ${wdr}/../LogFiles/${sub}_allses_glm_pipe
-	qsub -q long.q -N "glm_${sub}_allses_EuskalIBUR" \
-	-o ${wdr}/../LogFiles/${sub}_allses_glm_pipe \
-	-e ${wdr}/../LogFiles/${sub}_allses_glm_pipe \
-	${wdr}/98.hcp/run_Mennes.sh ${sub}
-	# -hold_jid "${joblist}" \
-done
-
-# # Run LME for CVR
-# for task in motor simon
+# # Run GLMs
+# for sub in 001 002 003 004 007 008 009
 # do
-# 	qsub -q long.q -N "lme_${task}_cvr_EuskalIBUR" \
-# 	-o ${wdr}/../LogFiles/lme_${task}_cvr_pipe \
-# 	-e ${wdr}/../LogFiles/lme_${task}_cvr_pipe \
-# 	${wdr}/98.hcp/run_lme_glm_cvr.sh ${task}
+# 	# for ses in $(seq -f %02g 1 10)
+# 	# do
+# 	# 	rm ${wdr}/../LogFiles/${sub}_${ses}_glm_pipe
+# 	# 	qsub -q long.q -N "glm_${sub}_${ses}_EuskalIBUR" \
+# 	# 	-o ${wdr}/../LogFiles/${sub}_${ses}_glm_pipe \
+# 	# 	-e ${wdr}/../LogFiles/${sub}_${ses}_glm_pipe \
+# 	# 	${wdr}/98.hcp/run_ses_glm.sh ${sub} ${ses}
+# 	# 	# -hold_jid "${joblist}" \
+# 	# done
+
+# 	rm ${wdr}/../LogFiles/${sub}_allses_glm_pipe
+# 	qsub -q long.q -N "glm_${sub}_allses_EuskalIBUR" \
+# 	-o ${wdr}/../LogFiles/${sub}_allses_glm_pipe \
+# 	-e ${wdr}/../LogFiles/${sub}_allses_glm_pipe \
+# 	${wdr}/98.hcp/run_Mennes.sh ${sub}
+# 	# -hold_jid "${joblist}" \
 # done
 
-# # Run LME for questionnaire
-# qsub -q long.q -N "lme_cvr_questionnaire_EuskalIBUR" \
-# -o ${wdr}/../LogFiles/lme_cvr_questionnaire_pipe \
-# -e ${wdr}/../LogFiles/lme_cvr_questionnaire_pipe \
-# ${wdr}/98.hcp/run_lme_cvr_questionnaire.sh
+# Run LME for CVR
+for task in motor simon
+do
+	qsub -q long.q -N "lme_${task}_cvr_EuskalIBUR" \
+	-o ${wdr}/../LogFiles/lme_${task}_cvr_pipe \
+	-e ${wdr}/../LogFiles/lme_${task}_cvr_pipe \
+	${wdr}/98.hcp/run_lme_glm_cvr.sh ${task}
+done
+
+# Run LME for questionnaire
+qsub -q long.q -N "lme_cvr_questionnaire_EuskalIBUR" \
+-o ${wdr}/../LogFiles/lme_cvr_questionnaire_pipe \
+-e ${wdr}/../LogFiles/lme_cvr_questionnaire_pipe \
+${wdr}/98.hcp/run_lme_cvr_questionnaire.sh

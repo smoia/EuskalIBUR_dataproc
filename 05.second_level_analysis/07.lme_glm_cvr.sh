fe48889afc69845457d10e87077af86ff1bf26b9
Stefano Moia <s.moia@bcbl.eu>
2021-03-29 18:51:49 +0200

    Fix extension

--- 05.second_level_analysis/07.lme_glm_cvr.sh
+++ 05.second_level_analysis/07.lme_glm_cvr.sh
@@ -83,17 +83,17 @@ do
 			motor )
 				# Two GLT are coded: all motor activations, and all motor activations against the sham to remove visual stimuli"
 				extract_and_average 34 42 ${tmp}/${sub}_${ses}_allmotors ${rbuck}
-                3dbucket -prefix ${tmp}/${sub}_${ses}_finger_left ${rbuck} -abuc ${rbuck}'[1]' -overwrite
-                3dbucket -prefix ${tmp}/${sub}_${ses}_finger_right ${rbuck} -abuc ${rbuck}'[4]' -overwrite
-                3dbucket -prefix ${tmp}/${sub}_${ses}_toe_left ${rbuck} -abuc ${rbuck}'[7]' -overwrite
-                3dbucket -prefix ${tmp}/${sub}_${ses}_toe_right ${rbuck} -abuc ${rbuck}'[10]' -overwrite
-                3dbucket -prefix ${tmp}/${sub}_${ses}_tongue ${rbuck} -abuc ${rbuck}'[13]' -overwrite
+                3dbucket -prefix ${tmp}/${sub}_${ses}_finger_left.nii.gz ${rbuck} -abuc ${rbuck}'[1]' -overwrite
+                3dbucket -prefix ${tmp}/${sub}_${ses}_finger_right.nii.gz ${rbuck} -abuc ${rbuck}'[4]' -overwrite
+                3dbucket -prefix ${tmp}/${sub}_${ses}_toe_left.nii.gz ${rbuck} -abuc ${rbuck}'[7]' -overwrite
+                3dbucket -prefix ${tmp}/${sub}_${ses}_toe_right.nii.gz ${rbuck} -abuc ${rbuck}'[10]' -overwrite
+                3dbucket -prefix ${tmp}/${sub}_${ses}_tongue.nii.gz ${rbuck} -abuc ${rbuck}'[13]' -overwrite
 				extract_and_average 45 53 ${tmp}/${sub}_${ses}_motors_vs_sham ${rbuck}
-                3dbucket -prefix ${tmp}/${sub}_${ses}_finger_left_vs_sham ${rbuck} -abuc ${rbuck}'[19]' -overwrite
-                3dbucket -prefix ${tmp}/${sub}_${ses}_finger_right_vs_sham ${rbuck} -abuc ${rbuck}'[22]' -overwrite
-                3dbucket -prefix ${tmp}/${sub}_${ses}_toe_left_vs_sham ${rbuck} -abuc ${rbuck}'[25]' -overwrite
-                3dbucket -prefix ${tmp}/${sub}_${ses}_toe_right_vs_sham ${rbuck} -abuc ${rbuck}'[28]' -overwrite
-                3dbucket -prefix ${tmp}/${sub}_${ses}_tongue_vs_sham ${rbuck} -abuc ${rbuck}'[31]' -overwrite
+                3dbucket -prefix ${tmp}/${sub}_${ses}_finger_left_vs_sham.nii.gz ${rbuck} -abuc ${rbuck}'[19]' -overwrite
+                3dbucket -prefix ${tmp}/${sub}_${ses}_finger_right_vs_sham.nii.gz ${rbuck} -abuc ${rbuck}'[22]' -overwrite
+                3dbucket -prefix ${tmp}/${sub}_${ses}_toe_left_vs_sham.nii.gz ${rbuck} -abuc ${rbuck}'[25]' -overwrite
+                3dbucket -prefix ${tmp}/${sub}_${ses}_toe_right_vs_sham.nii.gz ${rbuck} -abuc ${rbuck}'[28]' -overwrite
+                3dbucket -prefix ${tmp}/${sub}_${ses}_tongue_vs_sham.nii.gz ${rbuck} -abuc ${rbuck}'[31]' -overwrite
 				bricks=( allmotors motors_vs_sham finger_left finger_right toe_left toe_right tongue finger_left_vs_sham finger_right_vs_sham toe_left_vs_sham toe_right_vs_sham tongue_vs_sham )
 			;;
 			simon )

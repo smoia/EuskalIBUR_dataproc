993053892e3ea8adcc33b6746bbde2700853cadb
Stefano Moia <s.moia@bcbl.eu>
2021-04-19 16:44:48 +0200

    Revert "Fancier image (colorbar not working)"
    This reverts commit eb7eb4c1a359664ed951e913f04f44891c41c6fa.

--- 10.visualisation/07.plot_lme_questionnaire.sh
+++ 10.visualisation/07.plot_lme_questionnaire.sh
@@ -26,30 +26,24 @@ esac
 }
 
 slice_coeffs() {
-declare -A zvals
-zvals=( [0.1]=1.64 [0.05]=1.96 [0.02]=2.33 [0.01]=2.58 [0.005]=2.81 [0.002]=3.09 [0.001]=3.29 )
-thr=${zvals[${6}]}
-# $1:brickname $2:bckimg $3:lthr $4:uthr $5:[p/q] $6:pval $7:picdir
-echo "plot ${1##*/} $5 $6 display between ${3::4} and ${4::4}"
+# $1:brickname $2:bckimg $3:[p/q] $4:pval $5:picdir
+echo "plot ${1##*/} $3 $4"
 fsleyes render -of ${1}_tmp_axial.png --size 1900 200 \
 --scene lightbox --zaxis 2 --sliceSpacing 12 --zrange 19.3 139.9 --ncols 10 --nrows 1 --hideCursor --showColourBar --colourBarLocation right --colourBarLabelSide bottom-right --colourBarSize 80.0 --labelSize 12 --performance 3 --movieSync \
 ${2}.nii.gz --name "anat" --overlayType volume --alpha 100.0 --brightness 49.75000000000001 --contrast 49.90029860765409 --cmap greyscale --negativeCmap greyscale --displayRange 0.0 10000 --clippingRange 0.0 10000 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0 \
-${1}_Z.nii.gz --name "zeta" --overlayType volume --alpha 100.0 --disabled --cmap greyscale --negativeCmap greyscale --displayRange 0 10000 --clippingRange 0.0 10000 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0 \
-${1}.nii.gz --name "beta" --overlayType volume --alpha 100.0 --cmap brain_colours_1hot --negativeCmap cool --useNegativeCmap --clipImage ${1}_Z.nii.gz --displayRange ${3} ${4} --clippingRange ${thr} 10000 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
+${1}_fmkd.nii.gz --name "beta" --overlayType volume --alpha 100.0 --cmap brain_colours_1hot --negativeCmap cool --useNegativeCmap --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
 fsleyes render -of ${1}_tmp_sagittal.png --size 1900 200 \
 --scene lightbox --zaxis 0 --sliceSpacing 13 --zrange 39.5 169 --ncols 10 --nrows 1 --hideCursor --showColourBar --colourBarLocation right --colourBarLabelSide bottom-right --colourBarSize 80.0 --labelSize 12 --performance 3 --movieSync \
 ${2}.nii.gz --name "anat" --overlayType volume --alpha 100.0 --brightness 49.75000000000001 --contrast 49.90029860765409 --cmap greyscale --negativeCmap greyscale --displayRange 0.0 10000 --clippingRange 0.0 10000 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0 \
-${1}_Z.nii.gz --name "zeta" --overlayType volume --alpha 100.0 --disabled --cmap greyscale --negativeCmap greyscale --displayRange 0 10000 --clippingRange 0.0 10000 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0 \
-${1}.nii.gz --name "beta" --overlayType volume --alpha 100.0 --cmap brain_colours_1hot --negativeCmap cool --useNegativeCmap --clipImage ${1}_Z.nii.gz --displayRange ${3} ${4} --clippingRange ${thr} 10000 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
+${1}_fmkd.nii.gz --name "beta" --overlayType volume --alpha 100.0 --cmap brain_colours_1hot --negativeCmap cool --useNegativeCmap --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
 fsleyes render -of ${1}_tmp_coronal.png --size 1900 200 \
 --scene lightbox --zaxis 1 --sliceSpacing 15 --zrange 21 169 --ncols 10 --nrows 1 --hideCursor --showColourBar --colourBarLocation right --colourBarLabelSide bottom-right --colourBarSize 80.0 --labelSize 12 --performance 3 --movieSync \
 ${2}.nii.gz --name "anat" --overlayType volume --alpha 100.0 --brightness 49.75000000000001 --contrast 49.90029860765409 --cmap greyscale --negativeCmap greyscale --displayRange 0.0 10000 --clippingRange 0.0 10000 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0 \
-${1}_Z.nii.gz --name "zeta" --overlayType volume --alpha 100.0 --disabled --cmap greyscale --negativeCmap greyscale --displayRange 0 10000 --clippingRange 0.0 10000 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0 \
-${1}.nii.gz --name "beta" --overlayType volume --alpha 100.0 --cmap brain_colours_1hot --negativeCmap cool --useNegativeCmap --clipImage ${1}_Z.nii.gz --displayRange ${3} ${4} --clippingRange ${thr} 10000 --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
+${1}_fmkd.nii.gz --name "beta" --overlayType volume --alpha 100.0 --cmap brain_colours_1hot --negativeCmap cool --useNegativeCmap --gamma 0.0 --cmapResolution 256 --interpolation none --numSteps 100 --blendFactor 0.1 --smoothing 0 --resolution 100 --numInnerSteps 10 --clipMode intersection --volume 0
 # Mount visions
-convert -append ${1}_tmp_axial.png ${1}_tmp_sagittal.png ${1}_tmp_coronal.png +repage ${1}_${5}-${6}.png
+convert -append ${1}_tmp_axial.png ${1}_tmp_sagittal.png ${1}_tmp_coronal.png +repage ${1}_${3}-${4}.png
 rm ${1}_tmp*
-mv ${1}_${5}-${6}.png ${7}/.
+mv ${1}_${3}-${4}.png ${5}/.
 }
 
 # Declare z-value
@@ -107,21 +101,17 @@ do
 	echo ${brick##*/}
 	brickname=${brick%_Z.nii.gz}
 	# mask the functional brick with the right tstat
-	fslmaths ${brickname}_Z -abs -thr ${thr} -bin -mul ${brick} -abs ${brickname}_fmkd
-	uthr=$( fslstats ${brickname}_fmkd -P 90 )
-	lthr=$( fslstats ${brickname}_fmkd -P 0 )
+	fslmaths ${brickname}_Z -abs -thr ${thr} -bin -mul ${brick} ${brickname}_fmkd
 	# fsleyes all the way
-	slice_coeffs ${brickname} ${bckimg} ${lthr} ${uthr} p ${pval} ${picdir}/${analysis}
+	slice_coeffs ${brickname} ${bckimg} p ${pval} ${picdir}/${analysis}
 
 	# Repeat with FDR
 	echo "Computing FDR with z=${thr}"
 	3dFDR -input ${brickname}_Z.nii.gz -prefix ${brickname}_FDR.nii.gz
-	fslmaths ${brickname}_FDR -thr ${thr} -bin -mul ${brick} -abs ${brickname}_fmkd
-	uthr=$( fslstats ${brickname}_fmkd -P 90 )
-	lthr=$( fslstats ${brickname}_fmkd -P 0 )
-	slice_coeffs ${brickname} ${bckimg} ${lthr} ${uthr} q ${pval} ${picdir}/${analysis}
+	fslmaths ${brickname}_FDR -thr ${thr} -bin -mul ${brick} ${brickname}_fmkd
+	slice_coeffs ${brickname} ${bckimg} q ${pval} ${picdir}/${analysis}
 done
 
-# rm -rf ${tmp}/${task}
+rm -rf ${tmp}/${task}
 
 cd ${cwd}

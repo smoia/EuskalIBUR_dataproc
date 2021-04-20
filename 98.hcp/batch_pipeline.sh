0c96a5aaf4e6392f512364e47c43adeb85c4ba69
Stefano Moia <s.moia@bcbl.eu>
2021-04-20 19:32:12 +0200

    New run

--- 98.hcp/batch_pipeline.sh
+++ 98.hcp/batch_pipeline.sh
@@ -89,17 +89,17 @@ fi
 # 	# -hold_jid "${joblist}" \
 # done
 
-# Run LME for CVR
-for task in motor #motor simon
-do
-	qsub -q long.q -N "lme_${task}_cvr_EuskalIBUR" \
-	-o ${wdr}/../LogFiles/lme_${task}_cvr_pipe \
-	-e ${wdr}/../LogFiles/lme_${task}_cvr_pipe \
-	${wdr}/98.hcp/run_lme_glm_cvr.sh ${task}
-done
+# # Run LME for CVR
+# for task in motor #motor simon
+# do
+# 	qsub -q long.q -N "lme_${task}_cvr_EuskalIBUR" \
+# 	-o ${wdr}/../LogFiles/lme_${task}_cvr_pipe \
+# 	-e ${wdr}/../LogFiles/lme_${task}_cvr_pipe \
+# 	${wdr}/98.hcp/run_lme_glm_cvr.sh ${task}
+# done
 
-# # Run LME for questionnaire
-# qsub -q long.q -N "lme_cvr_questionnaire_EuskalIBUR" \
-# -o ${wdr}/../LogFiles/lme_cvr_questionnaire_pipe \
-# -e ${wdr}/../LogFiles/lme_cvr_questionnaire_pipe \
-# ${wdr}/98.hcp/run_lme_cvr_questionnaire.sh
+# Run LME for questionnaire
+qsub -q long.q -N "lme_cvr_questionnaire_EuskalIBUR" \
+-o ${wdr}/../LogFiles/lme_cvr_questionnaire_pipe \
+-e ${wdr}/../LogFiles/lme_cvr_questionnaire_pipe \
+${wdr}/98.hcp/run_lme_cvr_questionnaire.sh

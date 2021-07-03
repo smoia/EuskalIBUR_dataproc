a5033b8759ec5fbecc38c2b6b25cdb3bce1c2a47
smoia <s.moia@bcbl.eu>
2021-07-03 13:35:43 +0200

    Add range

--- 02.func_preproc/12.func_grayplot.sh
+++ 02.func_preproc/12.func_grayplot.sh
@@ -84,12 +84,12 @@ fi
 #Plot some grayplots!
 3dGrayplot -input ${tmp}/${func}.nii.gz -mask ${seg}.nii.gz \
 		   -prefix ${func}_gp_PVO.png -dimen 1800 1200 \
-		   -polort ${pol} -pvorder -percent
+		   -polort ${pol} -pvorder -percent -range 3
 3dGrayplot -input ${tmp}/${func}.nii.gz -mask ${seg}.nii.gz \
 		   -prefix ${func}_gp_IJK.png -dimen 1800 1200 \
-		   -polort ${pol} -ijkorder -percent
+		   -polort ${pol} -ijkorder -percent -range 3
 3dGrayplot -input ${tmp}/${func}.nii.gz -mask ${seg}.nii.gz \
 		   -prefix ${func}_gp_peel.png -dimen 1800 1200 \
-		   -polort ${pol} -peelorder -percent
+		   -polort ${pol} -peelorder -percent -range 3
 
 cd ${cwd}
